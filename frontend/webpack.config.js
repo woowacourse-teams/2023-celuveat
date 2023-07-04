59559e9a12dd5f5a48bd4a19f71057096eee543f
Jeremy <102432453+shackstack@users.noreply.github.com>
2023-07-04 18:36:53 +0900

    chore: 초기설정
    Co-authored-by: Minjae Kim <smallkdb@gmail.com>
    Co-authored-by: 황준승 <turtle601@users.noreply.github.com>

---
+++ frontend/webpack.config.js
@@ -0,0 +1,69 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+
+const { EnvironmentPlugin, DefinePlugin } = require('webpack');
+
+module.exports = {
+  mode: process.env.NODE_ENV,
+  entry: ['./src/index.tsx'],
+  output: {
+    path: path.resolve(__dirname, 'dist/'),
+    publicPath: '/',
+    clean: true,
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.jsx', '.js', 'json'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        use: [
+          'babel-loader',
+          {
+            loader: 'ts-loader',
+            options: {
+              transpileOnly: true,
+            },
+          },
+        ],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(jpg|jpeg|gif|png|svg|ico)?$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 10000,
+              fallback: 'file-loader',
+              name: 'images/[name].[ext]',
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: 'index.html',
+    }),
+    new ForkTsCheckerWebpackPlugin(),
+    new DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+    }),
+    new EnvironmentPlugin(['NODE_ENV']),
+  ],
+  devtool: 'inline-source-map',
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'public'),
+    },
+    hot: true,
+    open: true,
+    historyApiFallback: true,
+  },
+};
