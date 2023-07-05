b3ea11f803509ee464d10c8d7962dfb58b84153c
odo27 <odo27@naver.com>
2023-07-05 14:23:58 +0900

    feat: DB 컬럼에 created_date 추가 (#9)

--- backend/src/test/resources/schema.sql
+++ backend/src/test/resources/schema.sql
@@ -1,9 +1,11 @@
-CREATE TABLE celeb (
-    id BIGINT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(255) NOT NULL,
-    youtube_id VARCHAR(255) NOT NULL,
-    subscriber_count INT NOT NULL,
-    link VARCHAR(511) NOT NULL,
+CREATE TABLE celeb
+(
+    id                   BIGINT PRIMARY KEY AUTO_INCREMENT,
+    name                 VARCHAR(255) NOT NULL,
+    youtube_id           VARCHAR(255) NOT NULL,
+    subscriber_count     INT          NOT NULL,
+    link                 VARCHAR(511) NOT NULL,
     background_image_url VARCHAR(511) NOT NULL,
-    profile_image_url VARCHAR(511) NOT NULL
+    profile_image_url    VARCHAR(511) NOT NULL,
+    created_date         TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
