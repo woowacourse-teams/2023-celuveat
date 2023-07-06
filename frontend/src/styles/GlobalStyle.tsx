f754823139c84195d087c333a277df172310bb3c
Jeremy <102432453+shackstack@users.noreply.github.com>
2023-07-06 14:50:41 +0900

    chore: global style 설정  (#33)
    reset css, 메인 색상 수정

--- frontend/src/styles/GlobalStyle.tsx
+++ frontend/src/styles/GlobalStyle.tsx
@@ -1,38 +1,10 @@
 import { createGlobalStyle } from 'styled-components';
+import resetCSS from './reset';
+import baseCSS from './base';
 
 const GlobalStyle = createGlobalStyle`
-  * {
-    padding: 0;
-    border: 0;
-    box-sizing: border-box;
-    color: #333333
-  }
-  
-  body {
-    margin: 0;
-    padding: 0;
-  }
-  
-  ul,li {
-  list-style: none;
-  }
-
-  button {
-  all: unset;
-  cursor: pointer;
-  }
-
-  a {
-    text-decoration: none;
-  }
-
-  span {
-    font-weight: 200;
-  }
-
-  :root {
-    --theme-primary: #FF7B54;
-  }
+  ${resetCSS}
+  ${baseCSS}
 `;
 
 export default GlobalStyle;
