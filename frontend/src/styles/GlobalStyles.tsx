1bfd93573785cf87e9c0328a87b889908a7629dc
Jeremy <102432453+shackstack@users.noreply.github.com>
2023-07-06 15:10:47 +0900

    style: GlobalStyles로 파일명 수정 (#33)

--- frontend/src/styles/GlobalStyles.tsx
+++ frontend/src/styles/GlobalStyles.tsx
@@ -2,9 +2,9 @@ import { createGlobalStyle } from 'styled-components';
 import resetCSS from './reset';
 import baseCSS from './base';
 
-const GlobalStyle = createGlobalStyle`
+const GlobalStyles = createGlobalStyle`
   ${resetCSS}
   ${baseCSS}
 `;
 
-export default GlobalStyle;
+export default GlobalStyles;
