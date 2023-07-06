c6dac15d5630ab473dc84d96aef9c19f47dafd2e
Jeremy <102432453+shackstack@users.noreply.github.com>
2023-07-06 15:17:39 +0900

    chore: 스토리북 글로벌 스타일 적용 (#35)
    * chore: global style 설정  (#33)
    
    reset css, 메인 색상 수정
    
    * chore: 스토리북 addon styling 설치  (#33)
    
    * chore: 스토리북 global style 적용 (#33)
    
    * style: GlobalStyles로 파일명 수정 (#33)

--- frontend/.storybook/preview.ts
+++ frontend/.storybook/preview.ts
@@ -1,4 +1,6 @@
 import type { Preview } from '@storybook/react';
+import { withThemeFromJSXProvider } from '@storybook/addon-styling';
+import GlobalStyles from '../src/styles/GlobalStyles';
 
 const preview: Preview = {
   parameters: {
@@ -11,5 +13,7 @@ const preview: Preview = {
     },
   },
 };
+
+export const decorators = [withThemeFromJSXProvider({ GlobalStyles })];
 
 export default preview;
