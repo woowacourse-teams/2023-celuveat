c72d72479852bc58846966d4416696fdc4d40a61
d0dam <smallkdb@gmail.com>
2023-07-07 11:33:12 +0900

    feat: Header 컴포넌트 구현(#28)

---
+++ frontend/src/components/@common/Header/Header.tsx
@@ -0,0 +1,22 @@
+import { styled } from 'styled-components';
+import Logo from '~/assets/logo.png';
+
+function Header() {
+  return (
+    <StyledHeader>
+      <StyledLogo alt="셀럽잇 로고" src={Logo} />
+    </StyledHeader>
+  );
+}
+
+export default Header;
+
+const StyledHeader = styled.header`
+  width: 100%;
+  padding: 12px 24px;
+  border-bottom: 1px solid var(--gray-1);
+`;
+
+const StyledLogo = styled.img`
+  width: 136px;
+`;
