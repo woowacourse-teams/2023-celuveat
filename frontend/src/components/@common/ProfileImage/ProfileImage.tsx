31f21c24832c1caf4fb8ccb5976bfa8723a84282
d0dam <smallkdb@gmail.com>
2023-07-07 11:33:50 +0900

    feat: ProfileImage 컴포넌트 구현(#28)

---
+++ frontend/src/components/@common/ProfileImage/ProfileImage.tsx
@@ -0,0 +1,21 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import { styled } from 'styled-components';
+
+interface ProfileImageProps extends React.HTMLAttributes<HTMLImageElement> {
+  name: string;
+  imageUrl: string;
+  size: number;
+}
+
+function ProfileImage({ name = '셀럽', imageUrl, size, ...props }: ProfileImageProps) {
+  return <StyledProfile alt={`${name} 프로필`} src={imageUrl} size={size} {...props} />;
+}
+
+export default ProfileImage;
+
+const StyledProfile = styled.img<{ size: number }>`
+  width: ${props => props.size}px;
+  height: ${props => props.size}px;
+  background: var(--red-5);
+  border-radius: 50%;
+`;
