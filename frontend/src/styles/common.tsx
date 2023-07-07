6fed2039d6b58c8904dfe77030bd27c8763935e8
Jeremy <102432453+shackstack@users.noreply.github.com>
2023-07-07 11:39:35 +0900

    feat: Label 컴포넌트 구현 (#41)
    * style: 폰트 사이즈 상수 컨벤션 적용 (#40)
    
    * feat: Label 컴포넌트 구현
    
    * style: 오탈자 수정
    
    * feat: border-radius 상수화 (#32)
    
    * feat: Label 컴포넌트 추상화 (#32)
    
    * style: Label 컴포넌트 파일 이동(#32)
    
    * feat: Label 컴포넌트 index 파일 생성 (#32)

--- frontend/src/styles/common.tsx
+++ frontend/src/styles/common.tsx
@@ -1,8 +1,15 @@
 /* eslint-disable import/prefer-default-export */
 
-export const fontSize = {
+export const FONT_SIZE = {
   xs: '0.8rem',
   sm: '1.2rem',
   md: '1.6rem',
   lg: '2.0rem',
+};
+
+export const BORDER_RADIUS = {
+  xs: '4px',
+  sm: '8px',
+  md: '12px',
+  lg: '16px',
 };
