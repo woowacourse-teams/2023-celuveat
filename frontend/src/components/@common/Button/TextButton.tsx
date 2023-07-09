6a63c211c1a8e3e479df3d52b434963ad398efcf
d0dam <smallkdb@gmail.com>
2023-07-09 16:36:37 +0900

    refactor: figma 시안에 맞추어 TextButton 컴포넌트의 추상화 개선 (#44)

--- frontend/src/components/@common/Button/TextButton.tsx
+++ frontend/src/components/@common/Button/TextButton.tsx
@@ -3,27 +3,43 @@ import { styled } from 'styled-components';
 import { BORDER_RADIUS, FONT_SIZE } from '~/styles/common';
 
 interface TextButtonProps extends React.HTMLAttributes<HTMLButtonElement> {
+  type: 'button' | 'submit';
   text: string;
   onClick: React.MouseEventHandler<HTMLButtonElement>;
-  additionalStyle: string;
+  colorType: 'dark' | 'light';
+  disabled?: boolean;
 }
 
-function TextButton({ text, onClick, additionalStyle, ...props }: TextButtonProps) {
+function TextButton({ text, colorType, disabled = false, ...props }: TextButtonProps) {
   return (
-    <StyledButton {...props} onClick={onClick} additionalStyle={additionalStyle}>
+    <StyledButton {...props} colorType={colorType} disabled={disabled}>
       {text}
     </StyledButton>
   );
 }
 
-const StyledButton = styled.button<{ additionalStyle: string }>`
-  ${props => props.additionalStyle}
+export default TextButton;
+
+const StyledButton = styled.button<{ colorType: 'dark' | 'light' }>`
   font-size: ${FONT_SIZE.md};
   padding: 12px 24px;
+
   border: none;
   border-radius: ${BORDER_RADIUS.sm};
-  background-color: var(--yellow);
+  background-color: ${({ colorType }) => (colorType === 'dark' ? 'var(--primary-5)' : 'var(--primary-1)')};
+  color: ${({ colorType }) => (colorType === 'dark' ? 'var(--white)' : 'var(--primary-5)')};
   cursor: pointer;
+
+  &:disabled {
+    background-color: ${({ colorType }) => (colorType === 'dark' ? 'var(--primary-3)' : 'var(--gray-1)')};
+    color: var(--white);
+  }
+
+  &:not(:disabled):hover {
+    background-color: ${({ colorType }) => (colorType === 'dark' ? 'var(--primary-6);' : 'var(--primary-2)')};
+  }
 `;
 
-export default TextButton;
+TextButton.defaultProps = {
+  disabled: false,
+};
