de5bc4277f9076b46419b7a5faf82e1478f21d34
Minjae Kim <smallkdb@gmail.com>
2023-07-10 17:01:14 +0900

    feat: button 추상화 컴포넌트 제작 (#52)
    * feat: Button 컴포넌트 구현 (#44)
    
    * chore: storybook 관련 라이브러리 위치 dependencies로 변경 (#44)
    
    * refactor: figma 시안에 맞추어 TextButton 컴포넌트의 추상화 개선 (#44)
    
    * refactor: defaultProps 삭제 (#44)
    
    * chore: storybook dependency 변경 (#44)
    
    * fix: 오타 수정 (#44)

---
+++ frontend/src/components/@common/Button/TextButton.stories.tsx
@@ -0,0 +1,58 @@
+/* eslint-disable no-alert */
+import type { Meta, StoryObj } from '@storybook/react';
+import { styled } from 'styled-components';
+import TextButton from './TextButton';
+
+function TextButtons() {
+  return (
+    <StyledTextButtons>
+      <section>
+        <h2>Dark Button</h2>
+        <div>
+          <h5>disabled</h5>
+          <TextButton type="button" text="dark button" colorType="dark" onClick={() => alert('clicked')} disabled />
+          <h5>default</h5>
+          <TextButton type="button" text="dark button" colorType="dark" onClick={() => alert('clicked')} />
+        </div>
+      </section>
+      <section>
+        <h2>light Button</h2>
+        <div>
+          <h5>disabled</h5>
+          <TextButton type="button" text="light button" colorType="light" onClick={() => alert('clicked')} disabled />
+          <h5>default</h5>
+          <TextButton type="button" text="light button" colorType="light" onClick={() => alert('clicked')} />
+        </div>
+      </section>
+    </StyledTextButtons>
+  );
+}
+
+const meta: Meta<typeof TextButtons> = {
+  title: 'TextButton',
+  component: TextButtons,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof TextButtons>;
+
+export const Buttons: Story = {};
+
+const StyledTextButtons = styled.div`
+  display: flex;
+  column-gap: 124px;
+
+  & > section > div {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-template-rows: 1fr 1fr;
+
+    & > * {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      margin: 8px;
+    }
+  }
+`;
