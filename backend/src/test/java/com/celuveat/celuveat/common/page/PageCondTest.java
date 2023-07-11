e971be1bb9e385c0f603ce0a43c78a637f2df1dc
shin-mallang <huipulci1@naver.com>
2023-07-11 17:35:03 +0900

    feat: 페이징 공통 응답 정의 (#14)

---
+++ backend/src/test/java/com/celuveat/celuveat/common/page/PageCondTest.java
@@ -0,0 +1,41 @@
+package com.celuveat.celuveat.common.page;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("PageCond 은(는)")
+@SuppressWarnings("NonAsciiCharacters")
+@DisplayNameGeneration(ReplaceUnderscores.class)
+class PageCondTest {
+
+    @Test
+    void 페이지는_1페이지가_처음이며_이보다_작은_페이지는_1페이지로_설정된다() {
+        // when
+        PageCond pageCond = new PageCond(0, 20);
+
+        // then
+        assertThat(pageCond.page()).isEqualTo(1);
+    }
+
+    @Test
+    void 사이즈는_50이_최대이다() {
+        // when
+        PageCond pageCond = new PageCond(1, 51);
+
+        // then
+        assertThat(pageCond.size()).isEqualTo(50);
+    }
+
+    @Test
+    void 사이즈가_5보다_작으면_20으로_설정된다() {
+        // when
+        PageCond pageCond = new PageCond(1, 4);
+
+        // then
+        assertThat(pageCond.size()).isEqualTo(20);
+    }
+}
