c0592209b454c334f0a97c4f61701a9663b149bc
Jeremy <102432453+shackstack@users.noreply.github.com>
2023-07-11 14:13:58 +0900

    refactor: 주석제거 (#51)

--- frontend/src/components/@common/Map/Map.tsx
+++ frontend/src/components/@common/Map/Map.tsx
@@ -20,15 +20,13 @@ interface MapProps {
 
 function Map({ width, height, level, searchWord }: MapProps) {
   useEffect(() => {
-    const container = document.getElementById('map'); // 지도를 담을 영역의 DOM 레퍼런스
+    const container = document.getElementById('map');
 
     if ('latitude' in searchWord && 'longitude' in searchWord) {
       const options = {
-        // 지도를 생성할 때 필요한 기본 옵션
-        center: new window.kakao.maps.LatLng(searchWord.longitude, searchWord.latitude), // 지도의 중심좌표. 디폴트 선릉캠퍼스
-        level, // 지도의 레벨(확대, 축소 정도)
+        center: new window.kakao.maps.LatLng(searchWord.longitude, searchWord.latitude),
+        level,
       };
-
       const map = new window.kakao.maps.Map(container, options);
       const position = new window.kakao.maps.LatLng(searchWord.longitude, searchWord.latitude);
 
@@ -37,22 +35,17 @@ function Map({ width, height, level, searchWord }: MapProps) {
 
     if ('address' in searchWord) {
       const options = {
-        // 지도를 생성할 때 필요한 기본 옵션
-        center: new window.kakao.maps.LatLng(127.050727, 37.5057482), // 지도의 중심좌표. 디폴트 선릉캠퍼스
-        level, // 지도의 레벨(확대, 축소 정도)
+        center: new window.kakao.maps.LatLng(127.050727, 37.5057482),
+        level,
       };
       const map = new window.kakao.maps.Map(container, options);
       const geocoder = new window.kakao.maps.services.Geocoder();
 
       geocoder.addressSearch(searchWord.address, (result: any, status: any) => {
-        // 정상적으로 검색이 완료됐으면
         if (status === window.kakao.maps.services.Status.OK) {
           const position = new window.kakao.maps.LatLng(result[0].y, result[0].x);
 
-          // 결과값으로 받은 위치를 마커로 표시합니다
           new window.kakao.maps.Marker({ map, position });
-
-          // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
           map.setCenter(position);
         }
       });
