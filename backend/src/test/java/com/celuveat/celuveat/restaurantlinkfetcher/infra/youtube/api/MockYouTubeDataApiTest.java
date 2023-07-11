627a5b20d7973cb7adb8e32d52356f7549f5bc7f
odo27 <odo27@naver.com>
2023-07-11 16:17:52 +0900

    style: 어노테이션 컨벤션 반영 (#16)

--- backend/src/test/java/com/celuveat/celuveat/restaurantlinkfetcher/infra/youtube/api/MockYouTubeDataApiTest.java
+++ backend/src/test/java/com/celuveat/celuveat/restaurantlinkfetcher/infra/youtube/api/MockYouTubeDataApiTest.java
@@ -18,8 +18,8 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-@DisplayName("MockYouTubeDataApi 은(는)")
 @SuppressWarnings("NonAsciiCharacters")
+@DisplayName("MockYouTubeDataApi 은(는)")
 @DisplayNameGeneration(ReplaceUnderscores.class)
 class MockYouTubeDataApiTest {
 
