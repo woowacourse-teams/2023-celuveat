85db813815b6123426d5ddba8b84c56431184e5f
shin-mallang <huipulci1@naver.com>
2023-07-11 17:42:40 +0900

    test: 페이징 공통 응답 테스트 추가 (#14)

--- backend/src/test/java/com/celuveat/celuveat/common/page/PageCondTest.java
+++ backend/src/test/java/com/celuveat/celuveat/common/page/PageCondTest.java
@@ -38,4 +38,25 @@ class PageCondTest {
         // then
         assertThat(pageCond.size()).isEqualTo(20);
     }
+
+    @Test
+    void limit_는_size_와_같다() {
+        // when
+        PageCond pageCond = new PageCond(1, 14);
+
+        // then
+        assertThat(pageCond.limit()).isEqualTo(14);
+    }
+
+    @Test
+    @DisplayName("offset = (page - 1) * size")
+    void offset_test() {
+        // when
+        PageCond pageCond1 = new PageCond(1, 14);
+        PageCond pageCond2 = new PageCond(2, 14);
+
+        // then
+        assertThat(pageCond1.offset()).isEqualTo(0);
+        assertThat(pageCond2.offset()).isEqualTo(14);
+    }
 }
