18a02fe2c6a4c64957902f9c332fded72abef560
shin-mallang <huipulci1@naver.com>
2023-07-11 16:16:52 +0900

    feat: 단일 레스토랑 조회 API 구현 (#13)

---
+++ backend/src/main/java/com/celuveat/celuveat/restaurant/presentation/RestaurantController.java
@@ -0,0 +1,26 @@
+package com.celuveat.celuveat.restaurant.presentation;
+
+import com.celuveat.celuveat.restaurant.application.RestaurantQueryService;
+import com.celuveat.celuveat.restaurant.domain.Restaurant;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/restaurants")
+public class RestaurantController {
+
+    private final RestaurantQueryService restaurantQueryService;
+
+    @GetMapping("/{id}")
+    ResponseEntity<Restaurant> findById(
+            @PathVariable Long id
+    ) {
+        Restaurant result = restaurantQueryService.findById(id);
+        return ResponseEntity.ok(result);
+    }
+}
---
+++ backend/src/test/java/com/celuveat/celuveat/acceptance/restaurant/RestaurantAcceptanceSteps.java
@@ -0,0 +1,27 @@
+package com.celuveat.celuveat.acceptance.restaurant;
+
+import static com.celuveat.celuveat.acceptance.common.CommonAcceptanceSteps.given;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.celuveat.celuveat.restaurant.domain.Restaurant;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+
+@SuppressWarnings("NonAsciiCharacters")
+public class RestaurantAcceptanceSteps {
+
+    public static ExtractableResponse<Response> 단일_음식점_조회_요청(Long 미스터피자_ID) {
+        return given()
+                .when()
+                .get("/restaurants/{id}", 미스터피자_ID)
+                .then().log().all()
+                .extract();
+    }
+
+    public static void 단일_음식점_조회_결과를_검증한다(Restaurant 예상_응답, ExtractableResponse<Response> 응답) {
+        assertThat(응답.as(Restaurant.class))
+                .usingRecursiveComparison()
+                .ignoringFields("id")
+                .isEqualTo(예상_응답);
+    }
+}
