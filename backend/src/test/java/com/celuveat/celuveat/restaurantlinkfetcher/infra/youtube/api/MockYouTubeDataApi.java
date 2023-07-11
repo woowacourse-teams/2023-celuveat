cf043b20d4c6e2f9a2f169f8b744eb120db84f12
odo27 <odo27@naver.com>
2023-07-11 15:38:04 +0900

    feat: 채널 아이디에 따라 다르게 응답하는 기능 추가 (#16)

--- backend/src/test/java/com/celuveat/celuveat/restaurantlinkfetcher/infra/youtube/api/MockYouTubeDataApi.java
+++ backend/src/test/java/com/celuveat/celuveat/restaurantlinkfetcher/infra/youtube/api/MockYouTubeDataApi.java
@@ -1,31 +1,20 @@
 package com.celuveat.celuveat.restaurantlinkfetcher.infra.youtube.api;
 
+import static com.celuveat.celuveat.restaurantlinkfetcher.exception.RestaurantLinkFetcherExceptionType.NOT_FOUND_RESTAURANT_LINK;
+
 import com.celuveat.celuveat.restaurantlinkfetcher.exception.RestaurantLinkFetcherException;
-import com.celuveat.celuveat.restaurantlinkfetcher.exception.RestaurantLinkFetcherExceptionType;
 import com.celuveat.celuveat.restaurantlinkfetcher.infra.youtube.dto.search.SearchListResponse;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import org.springframework.core.io.ClassPathResource;
 
 public class MockYouTubeDataApi extends YouTubeDataApi {
 
-    private static final String FILE_PATH = "youtube/search/tzuyang/tzuyang%s.json";
-    private static final Map<String, String> TOKEN_BY_FILENAME = Map.of(
-            "01", "CDIQAQ",
-            "02", "CDIQAA",
-            "03", "CGQQAA",
-            "04", "CJYBEAA",
-            "05", "CMgBEAA",
-            "06", "CPoBEAA",
-            "07", "CKwCEAA",
-            "08", "CN4CEAA",
-            "09", "CJADEAA"
-    );
-
-    private final Map<String, SearchListResponse> responses = new HashMap<>();
+    private final Map<String, Map<String, SearchListResponse>> responsesByChannelId = new HashMap<>();
 
     public MockYouTubeDataApi() {
         super(null, null);
@@ -33,26 +22,38 @@ public class MockYouTubeDataApi extends YouTubeDataApi {
     }
 
     private void loadData() {
-        TOKEN_BY_FILENAME.keySet()
-                .forEach(filename -> responses.put(TOKEN_BY_FILENAME.get(filename), loadData(filename)));
+        Arrays.stream(Channel.values()).forEach(this::loadData);
     }
 
-    private SearchListResponse loadData(String filename) {
+    private void loadData(Channel channel) {
+        String filepath = channel.filepath();
+        Map<String, SearchListResponse> responses = new HashMap<>();
+        channel.tokenByFilename().forEach((filename, token) -> responses.put(token, loadData(filepath, filename)));
+        responsesByChannelId.put(channel.channelId(), responses);
+    }
+
+    private SearchListResponse loadData(String filepath, String filename) {
         try {
-            File file = new ClassPathResource(String.format(FILE_PATH, filename)).getFile();
+            File file = new ClassPathResource(String.format(filepath, filename)).getFile();
             return new ObjectMapper().readValue(file, SearchListResponse.class);
         } catch (IOException e) {
-            throw new RestaurantLinkFetcherException(RestaurantLinkFetcherExceptionType.NOT_FOUND_RESTAURANT_LINK);
+            throw new RestaurantLinkFetcherException(NOT_FOUND_RESTAURANT_LINK);
         }
     }
 
     @Override
     public SearchListResponse searchList(String channelId) {
-        return responses.get("CDIQAQ");
+        if (responsesByChannelId.containsKey(channelId)) {
+            return responsesByChannelId.get(channelId).get("CDIQAQ");
+        }
+        throw new RestaurantLinkFetcherException(NOT_FOUND_RESTAURANT_LINK);
     }
 
     @Override
     public SearchListResponse searchList(String channelId, String pageToken) {
-        return responses.get(pageToken);
+        if (responsesByChannelId.containsKey(channelId)) {
+            return responsesByChannelId.get(channelId).get(pageToken);
+        }
+        throw new RestaurantLinkFetcherException(NOT_FOUND_RESTAURANT_LINK);
     }
 }
--- backend/src/test/java/com/celuveat/celuveat/restaurantlinkfetcher/infra/youtube/api/MockYouTubeDataApiTest.java
+++ backend/src/test/java/com/celuveat/celuveat/restaurantlinkfetcher/infra/youtube/api/MockYouTubeDataApiTest.java
@@ -1,41 +1,84 @@
 package com.celuveat.celuveat.restaurantlinkfetcher.infra.youtube.api;
 
+import static com.celuveat.celuveat.restaurantlinkfetcher.exception.RestaurantLinkFetcherExceptionType.NOT_FOUND_RESTAURANT_LINK;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import com.celuveat.celuveat.common.exception.BaseExceptionType;
+import com.celuveat.celuveat.restaurantlinkfetcher.exception.RestaurantLinkFetcherException;
 import com.celuveat.celuveat.restaurantlinkfetcher.infra.youtube.dto.search.SearchListResponse;
+import java.time.LocalDateTime;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 @DisplayName("MockYouTubeDataApi 은(는)")
 @SuppressWarnings("NonAsciiCharacters")
 @DisplayNameGeneration(ReplaceUnderscores.class)
 class MockYouTubeDataApiTest {
 
-    @Test
-    void 미리_설정된_데이터를_반환한다() {
-        // given
-        YouTubeDataApi youTubeDataApi = new MockYouTubeDataApi();
+    private final YouTubeDataApi youTubeDataApi = new MockYouTubeDataApi();
 
-        // when
-        SearchListResponse response = youTubeDataApi.searchList("a");
-
-        // then
-        assertThat(response.items()).hasSize(50);
+    private static Stream<Arguments> getChannels() {
+        return Stream.of(
+                Arguments.of(Channel.TZUYANG, "tzuyang쯔양"),
+                Arguments.of(Channel.HEEBAB, "히밥heebab")
+        );
     }
 
     @Test
-    void 페이지_토큰으로_다음_응답을_가져온다() {
-        // given
-        YouTubeDataApi youTubeDataApi = new MockYouTubeDataApi();
-        SearchListResponse response = youTubeDataApi.searchList("a");
-        String nextPageToken = response.nextPageToken();
-
+    void 존재하지_않는_채널_아이디로_조회시_예외가_발생한다() {
         // when
-        SearchListResponse result = youTubeDataApi.searchList("a", nextPageToken);
+        BaseExceptionType exceptionType = assertThrows(RestaurantLinkFetcherException.class, () ->
+                youTubeDataApi.searchList("a")
+        ).exceptionType();
 
         // then
-        assertThat(result.items()).hasSize(50);
+        assertThat(exceptionType).isEqualTo(NOT_FOUND_RESTAURANT_LINK);
+    }
+
+    @ParameterizedTest
+    @MethodSource("getChannels")
+    void 채널_아이디로_조회시_미리_설정된_데이터를_반환한다(Channel channel, String name) {
+        // given
+        String channelId = channel.channelId();
+
+        // when
+        SearchListResponse response = youTubeDataApi.searchList(channelId);
+
+        // then
+        assertAll(
+                () -> assertThat(response.items()).hasSize(50),
+                () -> assertThat(response.items().get(0).snippet().channelId()).isEqualTo(channelId),
+                () -> assertThat(response.items().get(0).snippet().channelTitle()).isEqualTo(name)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("getChannels")
+    void 채널_아이디와_페이지_토큰으로_조회시_다음_응답을_반환한다(Channel channel, String name) {
+        // given
+        String channelId = channel.channelId();
+        SearchListResponse response = youTubeDataApi.searchList(channelId);
+        String nextPageToken = response.nextPageToken();
+        LocalDateTime pageOnePublishedAt = response.items().get(0).publishedAt();
+
+        // when
+        SearchListResponse result = youTubeDataApi.searchList(channelId, nextPageToken);
+        LocalDateTime pageTwoPublishedAt = result.items().get(0).publishedAt();
+
+        // then
+        assertAll(
+                () -> assertThat(result.items()).hasSize(50),
+                () -> assertThat(result.items().get(0).snippet().channelId()).isEqualTo(channelId),
+                () -> assertThat(result.items().get(0).snippet().channelTitle()).isEqualTo(name),
+                () -> assertThat(pageTwoPublishedAt.isBefore(pageOnePublishedAt)).isTrue()
+        );
     }
 }
