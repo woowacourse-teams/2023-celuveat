95aa76958412a5012bb0bef175aafa95f19b00eb
odo27 <odo27@naver.com>
2023-07-12 15:40:45 +0900

    feat: 비디오 응답을 영상 이력으로 변환하는 메서드 구현 (#76)

--- backend/src/main/java/com/celuveat/celuveat/restaurantlinkfetcher/infra/youtube/dto/video/VideoListResponse.java
+++ backend/src/main/java/com/celuveat/celuveat/restaurantlinkfetcher/infra/youtube/dto/video/VideoListResponse.java
@@ -1,5 +1,7 @@
 package com.celuveat.celuveat.restaurantlinkfetcher.infra.youtube.dto.video;
 
+import com.celuveat.celuveat.celeb.domain.Celeb;
+import com.celuveat.celuveat.video.domain.VideoHistory;
 import java.util.List;
 
 public record VideoListResponse(
@@ -8,4 +10,16 @@ public record VideoListResponse(
         List<Item> items,
         PageInfo pageInfo
 ) {
+
+    public VideoHistory toVideoHistory(Celeb celeb) {
+        Item item = items.get(0);
+        return VideoHistory.builder()
+                .celebId(celeb.id())
+                .title(item.snippet().title())
+                .videoId(item.id())
+                .viewCount(item.viewCount())
+                .uploadDate(item.publishedAt())
+                .ads(false)
+                .build();
+    }
 }
