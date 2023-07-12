ec21e49cc0fc20ae9518ef83d7c102679c992d12
kdkdhoho <hkim4410@naver.com>
2023-07-12 15:39:29 +0900

    feat: AdminService 구현 (#18)

---
+++ backend/src/test/java/com/celuveat/celuveat/admin/application/AdminServiceTest.java
@@ -0,0 +1,70 @@
+package com.celuveat.celuveat.admin.application;
+
+import static com.celuveat.celuveat.admin.exception.AdminExceptionType.NOT_FOUND_ADMIN;
+import static com.celuveat.celuveat.admin.exception.AdminExceptionType.NOT_MATCH_PASSWORD;
+import static com.celuveat.celuveat.admin.fixture.AdminFixture.관리자_도기;
+import static com.celuveat.celuveat.admin.fixture.AdminFixture.관리자_오도;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatNoException;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import com.celuveat.celuveat.admin.domain.Admin;
+import com.celuveat.celuveat.admin.domain.PasswordEncoder;
+import com.celuveat.celuveat.admin.exception.AdminException;
+import com.celuveat.celuveat.admin.infra.encoder.FakePasswordEncoder;
+import com.celuveat.celuveat.admin.infra.persistence.FakeAdminDao;
+import com.celuveat.celuveat.common.exception.BaseExceptionType;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("AdminService 은(는)")
+@SuppressWarnings("NonAsciiCharacters")
+@DisplayNameGeneration(ReplaceUnderscores.class)
+class AdminServiceTest {
+
+    private final FakeAdminDao fakeAdminDao = new FakeAdminDao();
+    private final PasswordEncoder encoder = new FakePasswordEncoder();
+    private final AdminService adminService = new AdminService(fakeAdminDao, encoder);
+
+    @BeforeEach
+    void setUp() {
+        fakeAdminDao.save(관리자_오도());
+        fakeAdminDao.save(관리자_도기());
+    }
+
+    @Test
+    void 입력된_아이디가_존재하지_않으면_예외를_발생한다() {
+        // when
+        BaseExceptionType exceptionType = assertThrows(AdminException.class, () ->
+                adminService.login("Empty User Name", "Empty Password")
+        ).exceptionType();
+
+        // then
+        assertThat(exceptionType).isEqualTo(NOT_FOUND_ADMIN);
+    }
+
+    @Test
+    void 입력된_비밀번호가_일치하지_않으면_예외를_발생한다() {
+        // when
+        BaseExceptionType exceptionType = assertThrows(AdminException.class, () ->
+                adminService.login(관리자_도기().username(), "Wrong Password")
+        ).exceptionType();
+
+        // then
+        assertThat(exceptionType).isEqualTo(NOT_MATCH_PASSWORD);
+    }
+
+    @Test
+    void 로그인_성공() {
+        // given
+        Admin 도기 = 관리자_도기();
+
+        // then
+        assertThatNoException().isThrownBy(
+                () -> adminService.login(도기.username(), 도기.password())
+        );
+    }
+}
