99a63568e42c10d8b4e470f08b12f1906a0aab3f
odo27 <odo27@naver.com>
2023-07-12 12:57:06 +0900

    feat: 비디오 아이디로 비디오 상세 조회 기능 구현 (#76)

--- backend/src/main/java/com/celuveat/celuveat/restaurantlinkfetcher/infra/youtube/api/YouTubeDataApi.java
+++ backend/src/main/java/com/celuveat/celuveat/restaurantlinkfetcher/infra/youtube/api/YouTubeDataApi.java
@@ -1,7 +1,9 @@
 package com.celuveat.celuveat.restaurantlinkfetcher.infra.youtube.api;
 
 import com.celuveat.celuveat.restaurantlinkfetcher.infra.youtube.dto.search.SearchListResponse;
+import com.celuveat.celuveat.restaurantlinkfetcher.infra.youtube.dto.video.VideoListResponse;
 import com.celuveat.celuveat.restaurantlinkfetcher.infra.youtube.uri.SearchURI;
+import com.celuveat.celuveat.restaurantlinkfetcher.infra.youtube.uri.VideosURI;
 import java.net.URI;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.client.RestTemplate;
@@ -23,5 +25,11 @@ public class YouTubeDataApi {
                 .pageToken(pageToken)
                 .build();
         return restTemplate.getForObject(uri, SearchListResponse.class);
+    }
+
+    public VideoListResponse searchVideoById(String videoId) {
+        URI uri = VideosURI.builder(apiKey, videoId)
+                .build();
+        return restTemplate.getForObject(uri, VideoListResponse.class);
     }
 }
