353a2b6a367a850b220d78a2a0a2f21cf4d2b82d
Donghun Shin <huipulci1@naver.com>
2023-07-12 17:44:48 +0900

    feat: CORS 설정 추가 (#99)
    * feat: CORS 설정 추가 (#97)

--- backend/src/main/java/com/celuveat/celuveat/common/config/WebConfig.java
+++ backend/src/main/java/com/celuveat/celuveat/common/config/WebConfig.java
@@ -3,8 +3,11 @@ package com.celuveat.celuveat.common.config;
 import com.celuveat.celuveat.common.page.PageCondArgumentResolver;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -13,8 +16,26 @@ public class WebConfig implements WebMvcConfigurer {
 
     private final PageCondArgumentResolver argumentResolver;
 
+    @Value("${cors.allowedOrigins}")
+    private String[] allowedOrigins;
+
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(argumentResolver);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(allowedOrigins)
+                .allowedMethods(
+                        HttpMethod.GET.name(),
+                        HttpMethod.POST.name(),
+                        HttpMethod.PUT.name(),
+                        HttpMethod.DELETE.name(),
+                        HttpMethod.PATCH.name()
+                )
+                .allowCredentials(true)
+                .exposedHeaders("*");
     }
 }
