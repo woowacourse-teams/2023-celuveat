bb045b24b03d94155bb3b53b9dd725807b97509a
Jeremy <102432453+shackstack@users.noreply.github.com>
2023-07-13 15:04:49 +0900

    chore: msw 초기환경 세팅 (#92)
    * chore: msw 초기환경 세팅
    
    * style: 불필요한 코드 및 주석 제거 (#88)
    
    * refactor: develop환경에서 worker 설정 (#88)
    
    * fix: mockServiceWorker 경로 못읽는 오류 해결 (#88)

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,6 +2,11 @@ import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from '~/App';
 import GlobalStyles from './styles/GlobalStyles';
+import { worker } from './mocks/browser';
+
+if (process.env.NODE_ENV === 'development') {
+  worker.start();
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
---
+++ frontend/src/mocks/handlers.ts
@@ -0,0 +1,4 @@
+// src/mocks/handlers.js
+import { rest } from 'msw';
+
+export const handlers = [];
