3af9953361e313b84cb3ae933738b2c24fbffd88
kdkdhoho <hkim4410@naver.com>
2023-07-13 13:41:52 +0900

    feat: 음식점 카테고리 추가 (#63)

--- backend/src/main/java/com/celuveat/celuveat/restaurant/domain/Category.java
+++ backend/src/main/java/com/celuveat/celuveat/restaurant/domain/Category.java
@@ -2,6 +2,21 @@ package com.celuveat.celuveat.restaurant.domain;
 
 public enum Category {
 
+    족발_보쌈,
+    찜_탕_찌개,
+    돈까스_회_일식,
+    피자,
+    고기_구이,
+    야식,
+    양식,
+    치킨,
+    중식,
+    아시안,
+    백반_죽_국수,
+    도시락,
+    분식,
+    카페_디저트,
+    패스트푸드,
     ETC,
     ;
 }
