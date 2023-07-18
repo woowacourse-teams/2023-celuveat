753dbb1ba589d82c939375c07007c4ea0a210237
shin-mallang <huipulci1@naver.com>
2023-07-18 12:14:53 +0900

    init: 프로젝트 초기 설정

---
+++ backend/src/main/java/com/celuveat/CeluveatApplication.java
@@ -0,0 +1,13 @@
+package com.celuveat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CeluveatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CeluveatApplication.class, args);
+	}
+
+}
