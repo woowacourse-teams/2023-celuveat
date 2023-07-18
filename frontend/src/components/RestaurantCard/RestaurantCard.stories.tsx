530f5af409018938bc9cafc0179610bacc06bf79
Jeremy <102432453+shackstack@users.noreply.github.com>
2023-07-18 18:40:36 +0900

    refactor: 음식점 카드 컴포넌트 API명세에 맞게 수정 (#134)

--- frontend/src/components/RestaurantCard/RestaurantCard.stories.tsx
+++ frontend/src/components/RestaurantCard/RestaurantCard.stories.tsx
@@ -12,15 +12,40 @@ type Story = StoryObj<typeof RestaurantCard>;
 
 export const Default: Story = {
   args: {
-    imageUrl:
-      'https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMzAzMjhfMjY1%2FMDAxNjc5OTk0NjYxMDI5.Mo-i3h1Q8kR4yi0hOL2lQZdA6t6uiQ599aBNnnJ83q8g._NGlnMeHtVCiJVWenUbbtICefoddkW1Wg0g3PCxn9Q4g.JPEG.twinkle_paul%2F100V7467-2.jpg',
-    name: '카와카츠',
-    address: '서울시 마포구',
-    category: '일식',
-    celeb: {
-      name: '도담',
-      imageUrl: 'https://avatars.githubusercontent.com/u/51052049?v=4',
-      size: 42,
+    restaurant: {
+      id: 1,
+      name: '스시렌',
+      category: '일식당',
+      roadAddress: '서울 강남구 선릉로146길 27-8 2F',
+      latitude: 37.5222779,
+      longitude: 127.0423149,
+      phoneNumber: '010-8072-2032',
+      naverMapUrl: 'https://naver.me/58HxhMsl',
+      images: [
+        {
+          id: 1,
+          name: 'https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMzAzMjhfMjY1%2FMDAxNjc5OTk0NjYxMDI5.Mo-i3h1Q8kR4yi0hOL2lQZdA6t6uiQ599aBNnnJ83q8g._NGlnMeHtVCiJVWenUbbtICefoddkW1Wg0g3PCxn9Q4g.JPEG.twinkle_paul%2F100V7467-2.jpg',
+          author: '@mheebab',
+          sns: 'YOUTUBE',
+        },
+      ],
     },
+    celebs: [
+      {
+        id: 1,
+        name: '히밥',
+        youtubeChannelName: '@heebab',
+        profileImageUrl:
+          'https://yt3.googleusercontent.com/sL5ugPfl9vvwRwhf6l5APY__BZBw8qWiwgHs-uVsMPFoD5-a4opTJIcRSyrY8aY5LEESOMWJ=s176-c-k-c0x00ffffff-no-rj',
+      },
+      {
+        id: 2,
+        name: '히밥',
+        youtubeChannelName: '@heebab',
+        profileImageUrl:
+          'https://yt3.googleusercontent.com/sL5ugPfl9vvwRwhf6l5APY__BZBw8qWiwgHs-uVsMPFoD5-a4opTJIcRSyrY8aY5LEESOMWJ=s176-c-k-c0x00ffffff-no-rj',
+      },
+    ],
+    size: 42,
   },
 };
