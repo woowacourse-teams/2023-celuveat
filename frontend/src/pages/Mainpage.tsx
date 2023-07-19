8ea194666d82222de35b6138cc97f44843e5e850
Jeremy <102432453+shackstack@users.noreply.github.com>
2023-07-19 10:56:51 +0900

    feat: 메인페이지 UI구현 (#134)
    Co-authored-by: Minjae Kim <smallkdb@gmail.com>
    Co-authored-by: 황준승 <turtle601@users.noreply.github.com>

---
+++ frontend/src/pages/Mainpage.tsx
@@ -0,0 +1,77 @@
+import { styled } from 'styled-components';
+import { RestaurantData } from '~/@types/api.types';
+import Header from '~/components/@common/Header';
+import Map from '~/components/@common/Map';
+import RestaurantCard from '~/components/RestaurantCard';
+import { data } from '~/mocks/data';
+import { FONT_SIZE } from '~/styles/common';
+
+const mapArgs = {
+  width: '100%',
+  height: '100%',
+  level: 7,
+  mainPosition: { latitude: 37.5057482, longitude: 127.050727 },
+  markers: data.map(({ latitude, longitude }) => ({ latitude, longitude })),
+};
+
+function MainPage() {
+  return (
+    <>
+      <Header />
+      <StyledLayout>
+        <StyledLeftSide>
+          <StyledCardListHeader>음식점 수 20 개</StyledCardListHeader>
+          <StyledRestaurantCardList>
+            {data.map(({ celebs, ...restaurant }: RestaurantData) => (
+              <RestaurantCard restaurant={restaurant} celebs={celebs} size={42} />
+            ))}
+          </StyledRestaurantCardList>
+        </StyledLeftSide>
+        <StyledRightSide>
+          <Map {...mapArgs} />
+        </StyledRightSide>
+      </StyledLayout>
+    </>
+  );
+}
+
+export default MainPage;
+
+const StyledLayout = styled.div`
+  display: grid;
+  grid-template-columns: 3fr 2fr;
+
+  width: 100%;
+  height: 100%;
+`;
+
+const StyledLeftSide = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 2.4rem;
+
+  height: calc(100vh - 128px);
+
+  margin: 3.2rem 2.4rem;
+`;
+
+const StyledCardListHeader = styled.p`
+  font-size: ${FONT_SIZE.md};
+`;
+
+const StyledRestaurantCardList = styled.div`
+  display: grid;
+  gap: 4rem 2.4rem;
+
+  height: 100%;
+  grid-template-columns: 1fr 1fr 1fr;
+
+  overflow-y: auto;
+  grid-auto-flow: row;
+  grid-auto-rows: 360px;
+`;
+
+const StyledRightSide = styled.div`
+  width: 100%;
+  height: 100%;
+`;
