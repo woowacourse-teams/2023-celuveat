8bd2db39677d5cc3c668e78bdc8d75604219c39e
Jeremy <102432453+shackstack@users.noreply.github.com>
2023-07-20 12:48:15 +0900

    refactor: 지도 2중 생성 오류 처리 및 훅 분리 (#134)
    지도 생성 로직 분리

--- frontend/src/components/@common/Map/Map.tsx
+++ frontend/src/components/@common/Map/Map.tsx
@@ -1,7 +1,8 @@
 /* eslint-disable no-new */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { useEffect } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { Coordinate } from '~/@types/map.types';
+import useCreateMap from './hooks/useCreateMap';
 
 interface MapProps {
   width: string;
@@ -12,13 +13,13 @@ interface MapProps {
 }
 
 function Map({ width, height, level, mainPosition, markers }: MapProps) {
+  const container = useRef();
+  const { kakaoMap } = useCreateMap({ mainPosition, level, container });
+  const { latitude, longitude } = mainPosition;
+  const [currentMarker, setCurrentMarker] = useState(null);
+
   useEffect(() => {
-    const container = document.getElementById('map');
-    const options = {
-      center: new window.kakao.maps.LatLng(mainPosition.latitude, mainPosition.longitude),
-      level,
-    };
-    const map = new window.kakao.maps.Map(container, options);
+    if (!kakaoMap) return;
 
     markers.forEach(marker => {
       const position = new window.kakao.maps.LatLng(marker.latitude, marker.longitude);
@@ -26,24 +27,37 @@ function Map({ width, height, level, mainPosition, markers }: MapProps) {
       const imageSrc = 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png';
       const markerImage = new window.kakao.maps.MarkerImage(imageSrc, imageSize);
 
-      if (JSON.stringify(mainPosition) === JSON.stringify(marker)) return;
-
       new window.kakao.maps.Marker({
-        map,
+        map: kakaoMap,
         position,
         image: markerImage,
       });
     });
+  }, [kakaoMap]);
 
-    const position = new window.kakao.maps.LatLng(mainPosition.latitude, mainPosition.longitude);
+  useEffect(() => {
+    if (!kakaoMap) return;
+    if (currentMarker) currentMarker.setMap(null);
+    // 이동할 위도 경도 위치를 생성합니다
+    const moveLatLon = new window.kakao.maps.LatLng(latitude, longitude);
 
-    new window.kakao.maps.Marker({
-      map,
+    // 지도 중심을 이동 시킵니다
+    kakaoMap.setCenter(moveLatLon);
+
+    const position = new window.kakao.maps.LatLng(latitude, longitude);
+
+    // 마커를 생성합니다
+    const newMarker = new window.kakao.maps.Marker({
+      map: kakaoMap,
       position,
     });
-  }, [level, mainPosition, markers]);
 
-  return <div id="map" style={{ width, height }} />;
+    newMarker.setMap(kakaoMap);
+
+    setCurrentMarker(newMarker);
+  }, [mainPosition]);
+
+  return <div ref={container} style={{ width, height }} />;
 }
 
 export default Map;
