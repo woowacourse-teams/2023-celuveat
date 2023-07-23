487e48f9af602a1cf3cf35722691525a9b57c48f
turtle601 <turtle601@naver.com>
2023-07-23 17:51:21 +0900

    fix: jest svg, png 관련 컴포넌트 발생 에러 해결 (#154)

---
+++ frontend/fileTransformer.js
@@ -0,0 +1,10 @@
+// fileTransformer.js
+const path = require('path');
+
+module.exports = {
+  process(sourceText, sourcePath, options) {
+    return {
+      code: `module.exports = ${JSON.stringify(path.basename(sourcePath))};`,
+    };
+  },
+};
