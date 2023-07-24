1f49433372992db42ca91977dcf6052aa9f1204b
Donghun Shin <huipulci1@naver.com>
2023-07-24 19:53:24 +0900

    fix: 이미지를 base64로 인코딩하여 반환한다 (#166)

--- backend/src/main/java/com/celuveat/restaurant/application/dto/RestaurantImageQueryResponse.java
+++ backend/src/main/java/com/celuveat/restaurant/application/dto/RestaurantImageQueryResponse.java
@@ -1,5 +1,6 @@
 package com.celuveat.restaurant.application.dto;
 
+import com.celuveat.common.util.Base64Util;
 import com.celuveat.restaurant.domain.RestaurantImage;
 
 public record RestaurantImageQueryResponse(
@@ -12,7 +13,7 @@ public record RestaurantImageQueryResponse(
     public static RestaurantImageQueryResponse of(RestaurantImage restaurantImage) {
         return new RestaurantImageQueryResponse(
                 restaurantImage.id(),
-                restaurantImage.name(),
+                Base64Util.encode(restaurantImage.name().replace(".png", "")) + ".png",
                 restaurantImage.author(),
                 restaurantImage.author()
         );
