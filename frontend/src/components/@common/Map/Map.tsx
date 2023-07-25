3fe99b1c61f86d29351a31d950cab53f525f500a
Minjae Kim <smallkdb@gmail.com>
2023-07-25 11:26:31 +0900

    feat: google map 보강 (#168)
    * feat: 레스토랑 카드를 반응형으로 스타일링 (#167)
    
    * feat: Overlay 추가, Map에 추가 메서드 추가 및 훅 분리, 그에 따른 변경사항 적용 (#167)
    
    * refactor: 네이밍 변경, Overlay 구조 변경 (#167)

--- frontend/src/components/@common/Map/Map.tsx
+++ frontend/src/components/@common/Map/Map.tsx
@@ -1,29 +1,48 @@
-import { useRef } from 'react';
-import { styled } from 'styled-components';
-import { RestaurantData } from '~/@types/api.types';
-import useDrawMap from './hooks/useDrawMap';
-import useMarker from './hooks/useMarker';
-import { Coordinate } from '~/@types/map.types';
+import { useState } from 'react';
+import { Wrapper, Status } from '@googlemaps/react-wrapper';
+import OverlayMarker from './OverlayMarker';
+import type { Coordinate } from '~/@types/map.types';
+import type { Celebs } from '~/@types/celeb.types';
+import MapContent from './MapContent';
 
 interface MapProps {
-  center: google.maps.LatLngLiteral;
-  zoom: number;
-  size: { width: string; height: string };
-  restaurants: RestaurantData[];
+  center: Coordinate;
   clickMarker: ({ lat, lng }: Coordinate) => void;
+  markers: { position: Coordinate; celebs: Celebs }[];
 }
 
-function Map({ center, zoom, size, restaurants, clickMarker }: MapProps) {
-  const googleMapRef = useRef();
-  const { googleMap } = useDrawMap({ mapRef: googleMapRef, center, zoom });
-  useMarker({ map: googleMap, restaurants, clickMarker });
+const render = (status: Status) => {
+  if (status === Status.FAILURE) return <div>에러</div>;
+  return <div>로딩중</div>;
+};
 
-  return <StyledMap ref={googleMapRef} size={size} id="map" />;
+function Map({ center, clickMarker, markers }: MapProps) {
+  const [clicks, setClicks] = useState<google.maps.LatLng[]>([]);
+  const [zoom, setZoom] = useState(16);
+
+  const onClick = (e: google.maps.MapMouseEvent) => {
+    setClicks([...clicks, e.latLng!]);
+  };
+
+  const onIdle = (m: google.maps.Map) => {
+    setZoom(m.getZoom()!);
+  };
+
+  return (
+    <Wrapper apiKey={process.env.GOOGLE_MAP_API_KEY} render={render} language="ko">
+      <MapContent
+        onClick={onClick}
+        onIdle={onIdle}
+        style={{ width: '100%', height: '100%' }}
+        zoom={zoom}
+        center={center}
+      >
+        {markers.map(({ position, celebs }) => (
+          <OverlayMarker position={position} onClick={clickMarker} celeb={celebs[0]} />
+        ))}
+      </MapContent>
+    </Wrapper>
+  );
 }
 
 export default Map;
-
-const StyledMap = styled.div<{ size: { width: string; height: string } }>`
-  width: ${({ size }) => size.width};
-  height: ${({ size }) => size.height};
-`;
