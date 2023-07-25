b435791ec16af7a4626496c5fa0cd3ff354a5faa
kdkdhoho <hkim4410@naver.com>
2023-07-25 17:53:41 +0900

    feat: AdminException 구현 (#148)

---
+++ backend/src/main/java/com/celuveat/admin/exception/AdminExceptionType.java
@@ -0,0 +1,26 @@
+package com.celuveat.admin.exception;
+
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+
+import com.celuveat.common.exception.BaseExceptionType;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@RequiredArgsConstructor
+public enum AdminExceptionType implements BaseExceptionType {
+    NOT_EXISTS_CELEB(BAD_REQUEST, "셀럽이 저장되어 있지 않습니다. 셀럽 먼저 저장해주세요."),
+    ;
+
+    private final HttpStatus httpStatus;
+    private final String errorMessage;
+
+    @Override
+    public HttpStatus httpStatus() {
+        return httpStatus;
+    }
+
+    @Override
+    public String errorMessage() {
+        return errorMessage;
+    }
+}
