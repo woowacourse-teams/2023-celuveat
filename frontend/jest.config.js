013b31594bd80f58a2ed3353420d878a16d72808
황준승 <78203399+turtle601@users.noreply.github.com>
2023-07-26 14:30:44 +0900

    fix: react testing library 사용 시 에러 이미지 import 시 에러 발생 (#155)
    * infra: 이슈 템플릿 & PR 템플릿 작성 (#86)
    
    * infra: 이슈 템플릿 작성 (#84)
    
    * infra: PR 템플릿 작성 (#84)
    
    * fix: jest svg, png 관련 컴포넌트 발생 에러 해결 (#154)
    
    ---------
    
    Co-authored-by: Donghun Shin <huipulci1@naver.com>

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -5,5 +5,10 @@ module.exports = {
   setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
   transform: {
     '^.+\\.[tj]sx?$': 'babel-jest',
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/fileTransformer.js',
+  },
+  moduleNameMapper: {
+    '^\\~/(.*)$': '<rootDir>/src/$1',
   },
 };
