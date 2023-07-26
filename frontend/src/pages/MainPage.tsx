36f92cae098383d9fbf6c90dfcad605396ce1414
turtle601 <turtle601@naver.com>
2023-07-27 02:31:02 +0900

    fix: 필터링 클릭 시 렌더링이 한 박자 늦게되는 오류 해결 (#184)

--- frontend/src/pages/MainPage.tsx
+++ frontend/src/pages/MainPage.tsx
@@ -29,7 +29,7 @@ function MainPage() {
 
   useEffect(() => {
     fetchRestaurants();
-  }, [boundary]);
+  }, [boundary, data]);
 
   const clickCard = (restaurant: Restaurant) => {
     const { lat, lng, ...restaurantModalInfo } = restaurant;
