29bfd15c4a80476a6b7568fe06f47c11ee1367d8
kdkdhoho <hkim4410@naver.com>
2023-07-26 17:57:49 +0900

    feat: 데이터 저장 시 날짜 형식이 달라 발생하는 예외 추가 (#181)

--- backend/src/main/java/com/celuveat/admin/exception/AdminExceptionType.java
+++ backend/src/main/java/com/celuveat/admin/exception/AdminExceptionType.java
@@ -8,8 +8,9 @@ import org.springframework.http.HttpStatus;
 
 @RequiredArgsConstructor
 public enum AdminExceptionType implements BaseExceptionType {
-    
+
     NOT_EXISTS_CELEB(BAD_REQUEST, "셀럽이 저장되어 있지 않습니다. 셀럽 먼저 저장해주세요."),
+    ILLEGAL_DATE_FORMAT(BAD_REQUEST, "영상 업로드 날짜 형식이 잘못됐습니다. \'yyyy. M. d\' 형식으로 입력해주세요."),
     ;
 
     private final HttpStatus httpStatus;
--- backend/src/test/java/com/celuveat/admin/application/AdminServiceTest.java
+++ backend/src/test/java/com/celuveat/admin/application/AdminServiceTest.java
@@ -2,15 +2,18 @@ package com.celuveat.admin.application;
 
 import static com.celuveat.acceptance.admin.AdminAcceptanceSteps.요청_생성;
 import static com.celuveat.acceptance.admin.AdminAcceptanceSteps.입력_생성;
+import static com.celuveat.admin.exception.AdminExceptionType.ILLEGAL_DATE_FORMAT;
 import static com.celuveat.celeb.fixture.CelebFixture.셀럽;
 import static com.celuveat.restaurant.fixture.RestaurantFixture.국민연금_구내식당;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import com.celuveat.admin.exception.AdminException;
 import com.celuveat.admin.presentation.dto.SaveDataRequest;
 import com.celuveat.celeb.domain.Celeb;
 import com.celuveat.celeb.domain.CelebRepository;
+import com.celuveat.common.exception.BaseExceptionType;
 import com.celuveat.restaurant.domain.Restaurant;
 import com.celuveat.restaurant.domain.RestaurantImageRepository;
 import com.celuveat.restaurant.domain.RestaurantRepository;
@@ -20,6 +23,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.jdbc.Sql;
@@ -56,6 +61,22 @@ class AdminServiceTest {
         // then
         assertThatThrownBy(() -> adminService.saveData(요청))
                 .isInstanceOf(AdminException.class);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"2023. 7. 23. ", "23. 7. 23.", "2023.7.23", "2023. 7.8."})
+    void 날짜_형식이_다른_경우_예외가_발생한다(String 영상_업로드_날짜) {
+        // given
+        셀럽_저장(셀럽("도기"));
+
+        String input = 잘못된_입력_생성("도기", "농민백암순대", 영상_업로드_날짜);
+        List<SaveDataRequest> 요청 = 요청_생성(input);
+
+        // expect
+        BaseExceptionType exceptionType = assertThrows(AdminException.class, () ->
+                adminService.saveData(요청)
+        ).exceptionType();
+        assertThat(exceptionType).isEqualTo(ILLEGAL_DATE_FORMAT);
     }
 
     @Test
@@ -125,5 +146,19 @@ class AdminServiceTest {
 
     private Restaurant 음식점_저장(Restaurant 음식점) {
         return restaurantRepository.save(음식점);
+    }
+
+    private String 잘못된_입력_생성(String 셀럽_이름, String 음식점_이름, String 영상_업로드_날짜) {
+        return "@" + 셀럽_이름 +
+                "\t" + 음식점_이름 + "png" +
+                "\t유튜브링크" +
+                "\t" + 영상_업로드_날짜 +
+                "\t" + 음식점_이름 +
+                "\t" + 음식점_이름 + " 주소" +
+                "\t전화번호" +
+                "\t카테고리" +
+                "\t음식점네이버링크" +
+                "\t12.3456" +
+                "\t12.3456";
     }
 }
