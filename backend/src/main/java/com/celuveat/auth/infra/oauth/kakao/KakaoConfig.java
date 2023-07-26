2d29392c8e2cf577ce583005e98d03c1751ae6ea
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2023-07-26 09:45:57 +0900

    refactor: Properties binding 방식 record로 변경 (#148)

--- backend/src/main/java/com/celuveat/auth/infra/oauth/kakao/KakaoConfig.java
+++ backend/src/main/java/com/celuveat/auth/infra/oauth/kakao/KakaoConfig.java
@@ -1,30 +1,13 @@
 package com.celuveat.auth.infra.oauth.kakao;
 
-import lombok.AllArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-@AllArgsConstructor
 @ConfigurationProperties(prefix = "oauth.kakao")
-public class KakaoConfig {
-
-    private final String redirectUri;
-    private final String clientId;
-    private final String clientSecret;
-    private final String[] scope;
-
-    public String redirectUri() {
-        return redirectUri;
-    }
-
-    public String clientId() {
-        return clientId;
-    }
-
-    public String clientSecret() {
-        return clientSecret;
-    }
-
-    public String[] scope() {
-        return scope;
-    }
+public record KakaoConfig(
+        String redirectUri,
+        String clientId,
+        String clientSecret,
+        String[] scope
+) {
 }
+
