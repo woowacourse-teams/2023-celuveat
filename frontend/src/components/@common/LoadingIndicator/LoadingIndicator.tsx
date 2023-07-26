cb57fefb1a487f286600dd5b5340a04afc023124
Minjae Kim <smallkdb@gmail.com>
2023-07-26 11:03:16 +0900

    feat: 로딩 컴포넌트 및 스켈레톤 구현 (#140)
    * feat: 로딩 애니메이션 컴포넌트 구현 (#90)
    
    * refactor: loadingAnimation 도형 간 간격 조정 (#90)
    
    * feat: skeleton css 및 컴포넌트 구현 (#90)
    
    * feat: LoadingDots 추가
    
    * refactor: Loading 관련 컴포넌트를 한 폴더로 분리 (#90)
    
    * design: celuveat 아이콘 픽셀 단위 조정 (#90)
    
    * design: celuveat 아이콘 크기를 조절할 수 있게 svg 코드 수정 (#90)
    
    * refactor: 폴더 구조 변경, 네이밍 변경 (#90)
    
    * refactor: svg width props 제거, 일부 스타일 수정 (#90)

---
+++ frontend/src/components/@common/LoadingIndicator/LoadingIndicator.tsx
@@ -0,0 +1,76 @@
+import styled, { keyframes } from 'styled-components';
+import Cel from '~/assets/icons/celuveat_cel.svg';
+import Luv from '~/assets/icons/celuveat_luv.svg';
+import Eat from '~/assets/icons/celuveat_eat.svg';
+
+interface LoadingIndicatorProps {
+  size: number;
+}
+
+function LoadingIndicator({ size }: LoadingIndicatorProps) {
+  return (
+    <StyledLoadingAnimation>
+      <StyledBouncing size={size}>
+        <Cel />
+      </StyledBouncing>
+      <StyledBouncing size={size}>
+        <Luv />
+      </StyledBouncing>
+      <StyledBouncing size={size}>
+        <Eat />
+      </StyledBouncing>
+    </StyledLoadingAnimation>
+  );
+}
+
+export default LoadingIndicator;
+
+const StyledLoadingAnimation = styled.div`
+  display: flex;
+  gap: 0.6rem;
+
+  & > div:nth-child(2) {
+    animation-delay: 0.2s;
+  }
+
+  & > div:nth-child(3) {
+    animation-delay: 0.4s;
+  }
+`;
+
+const bounceAnimation = keyframes`
+  0% {
+    top: 0px;
+  }
+  25% {
+    top: 10px;
+  }
+  30% {
+    transform: rotate(10deg);
+  }
+  50% {
+    top: 0px;
+  }
+  75% {
+    top: 10px;
+  }
+  80% {
+    transform: rotate(-10deg);
+  }
+  100% {
+    top: 0px;
+  }
+`;
+
+const StyledBouncing = styled.div<{ size: number }>`
+  position: relative;
+  top: 0;
+  left: ${({ size }) => `${size}px`};
+
+  width: ${({ size }) => `${size}px`};
+  height: ${({ size }) => `${size}px`};
+
+  border-radius: 50%;
+  background: none;
+  animation: ${bounceAnimation} 1.6s ease-in-out infinite;
+`;
