59187a6f087ce3745eef474e1a2daf43e05e6335
turtle601 <turtle601@naver.com>
2023-07-29 21:52:55 +0900

    feat: Modal 컴포넌트 구현 (#193)

---
+++ frontend/src/components/@common/Modal/ModalContent.stories.tsx
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import ModalContent from './ModalContent';
+
+const meta: Meta<typeof ModalContent> = {
+  title: 'ModalContent',
+  component: ModalContent,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof ModalContent>;
+
+export const Default: Story = {
+  args: {
+    isShow: true,
+    title: '로그인 또는 회원가입',
+    handleModalShow: () => {},
+    children: '모달 내용',
+  },
+};
---
+++ frontend/src/components/@common/Modal/index.tsx
@@ -0,0 +1,4 @@
+import Modal from '~/components/@common/Modal/Modal';
+import ModalContent from '~/components/@common/Modal/ModalContent';
+
+export { Modal, ModalContent };
