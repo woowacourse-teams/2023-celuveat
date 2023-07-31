104c31051fe26cf7806c6846ae765b241b8332ae
odo27 <odo27@naver.com>
2023-07-31 14:35:59 +0900

    feat: RestaurantLikeRepository 구현(#206)

---
+++ backend/src/main/java/com/celuveat/restaurant/domain/RestaurantLikeRepository.java
@@ -0,0 +1,6 @@
+package com.celuveat.restaurant.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RestaurantLikeRepository extends JpaRepository<RestaurantLike, Long> {
+}
