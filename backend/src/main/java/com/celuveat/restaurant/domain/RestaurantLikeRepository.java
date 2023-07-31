4a9eb59fe2de04324e22a678623a6393746a90e8
odo27 <odo27@naver.com>
2023-07-31 17:28:06 +0900

    feat: findByRestaurantAndMember 추가 (#206)

--- backend/src/main/java/com/celuveat/restaurant/domain/RestaurantLikeRepository.java
+++ backend/src/main/java/com/celuveat/restaurant/domain/RestaurantLikeRepository.java
@@ -1,6 +1,10 @@
 package com.celuveat.restaurant.domain;
 
+import com.celuveat.auth.domain.OauthMember;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RestaurantLikeRepository extends JpaRepository<RestaurantLike, Long> {
+
+    Optional<RestaurantLike> findByRestaurantAndMember(Restaurant restaurant, OauthMember member);
 }
