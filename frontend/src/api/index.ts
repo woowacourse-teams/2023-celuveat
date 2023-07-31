db60289c3bb942166144988d5190d0c81a62605d
Jeremy <102432453+shackstack@users.noreply.github.com>
2023-07-31 18:00:50 +0900

    refactor: react query, zustand, axios 설치 (#212)
    * chore: 전역상태관리 및 비동기 라이브러리 설치 (#210)
    
    * refactor: 리액트 쿼리 및 axios 적용 (#210)

---
+++ frontend/src/api/index.ts
@@ -0,0 +1,26 @@
+/* eslint-disable import/prefer-default-export */
+import axios from 'axios';
+
+import type { RestaurantListData } from '~/@types/api.types';
+import { CoordinateBoundary } from '~/@types/map.types';
+import { RestaurantCategory } from '~/@types/restaurant.types';
+import getQueryString from '~/utils/getQueryString';
+
+export interface GetRestaurantsQueryParams {
+  boundary: CoordinateBoundary;
+  celebId: number;
+  category: RestaurantCategory;
+}
+
+const apiClient = axios.create({
+  baseURL: `${process.env.BASE_URL}/api`,
+  headers: {
+    'Content-type': 'application/json',
+  },
+});
+
+export const getRestaurants = async (queryParams: GetRestaurantsQueryParams) => {
+  const queryString = getQueryString(queryParams);
+  const response = await apiClient.get<RestaurantListData>(`/restaurants?${queryString}`);
+  return response.data;
+};
