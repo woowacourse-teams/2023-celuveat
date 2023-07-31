e82c34e03c5dbbb04feac1a06d692b7ce7c64e45
Jeremy <102432453+shackstack@users.noreply.github.com>
2023-07-31 17:24:00 +0900

    chore: 전역상태관리 및 비동기 라이브러리 설치 (#210)

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,4 +1,6 @@
 import ReactDOM from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import App from '~/App';
 import GlobalStyles from './styles/GlobalStyles';
 import { worker } from './mocks/browser';
@@ -9,9 +11,14 @@ if (process.env.NODE_ENV === 'development') {
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
+const queryClient = new QueryClient();
+
 root.render(
   <>
     <GlobalStyles />
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   </>,
 );
