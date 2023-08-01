f99d5b217f55a4c46098da52f726431a8a12f2ec
turtle601 <turtle601@naver.com>
2023-08-01 18:32:37 +0900

    fix: 도메인 설정을 위한 webpack 설정 수정(#231)

--- frontend/.webpack/webpack.dev.js
+++ frontend/.webpack/webpack.dev.js
@@ -6,6 +6,7 @@ module.exports = {
   mode: 'development',
   devtool: 'inline-source-map',
   devServer: {
+    disableHostCheck: true,
     static: {
       directory: path.resolve(__dirname, '../public'),
     },
