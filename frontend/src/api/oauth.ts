63ffe0e24d4ddf8a99c4324ace2f62c6e71932e2
turtle601 <turtle601@naver.com>
2023-08-01 14:35:02 +0900

    refactor: 중복된 코드 제거 (#215)

--- frontend/src/api/oauth.ts
+++ frontend/src/api/oauth.ts
@@ -1,17 +1,10 @@
-import axios from 'axios';
 import { BASE_URL } from '../constants/api';
 
 import type { Oauth } from '~/@types/oauth.types';
-
-const userApiClient = axios.create({
-  baseURL: `${process.env.BASE_URL}/api`,
-  headers: {
-    'Content-type': 'application/json',
-  },
-});
+import { apiClient } from '~/api';
 
 const getAccessToken = async (type: Oauth, code: string) => {
-  const response = await userApiClient.get(`${BASE_URL}/api/oauth/login/${type}?code=${code}`);
+  const response = await apiClient.get(`${BASE_URL}/api/oauth/login/${type}?code=${code}`);
   // 통신 에러 되었을 때 로직 추가
   return response.data;
 };
