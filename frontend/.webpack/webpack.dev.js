325d0d6a506105943b9491013fa99eae86795958
turtle601 <turtle601@naver.com>
2023-08-01 18:36:13 +0900

    fix: 도메인 설정을 위한 webpack5 설정 수정(#231)

--- frontend/.webpack/webpack.dev.js
+++ frontend/.webpack/webpack.dev.js
@@ -6,13 +6,13 @@ module.exports = {
   mode: 'development',
   devtool: 'inline-source-map',
   devServer: {
-    disableHostCheck: true,
     static: {
       directory: path.resolve(__dirname, '../public'),
     },
     hot: true,
     open: true,
     historyApiFallback: true,
+    allowedHosts: 'all',
   },
   plugins: [
     new CopyWebpackPlugin({
