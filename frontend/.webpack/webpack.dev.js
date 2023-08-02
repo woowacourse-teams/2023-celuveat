89ad1b44d06b5edf52602c848c8c0b6f998e85ef
황준승 <78203399+turtle601@users.noreply.github.com>
2023-08-02 10:38:47 +0900

    chore: 도메인 설정을 위한 webpack 수정 (#234)
    * chore: chore-도메인-설정을-위한-webpack-수정 (#231)
    
    * fix: 서버가 안 뜨는 에러 해결 (#231)
    
    Changed:
    disableHostCheck 삭제
    
    * fix: 도메인 설정을 위한 webpack 설정 수정(#231)
    
    * fix: 도메인 설정을 위한 webpack5 설정 수정(#231)
    
    * chore: webpack production 파일 수정 (#231)
    
    * fix: 도메인 배포 환경에서 Invalid 에러 해결 (#231)
    
    * fix: 도메인 수정으로 인한 meta 태그 수정(#231)
    
    * chore: 도메인 설정을 위한 webpack 수정 (#231)
    
    * fix: 도메인을 위한 Webpack 에러 수정 (#231)
    
    * chore: 이미지 정책을 위한 meta 수정 (#231)

--- frontend/.webpack/webpack.dev.js
+++ frontend/.webpack/webpack.dev.js
@@ -5,14 +5,6 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 module.exports = {
   mode: 'development',
   devtool: 'inline-source-map',
-  devServer: {
-    static: {
-      directory: path.resolve(__dirname, '../public'),
-    },
-    hot: true,
-    open: true,
-    historyApiFallback: true,
-  },
   plugins: [
     new CopyWebpackPlugin({
       patterns: [
