2e5dae9c654ecdb8e5408fe28d6c85e84c87775a
shin-mallang <huipulci1@naver.com>
2023-08-02 23:05:03 +0900

    feat: 로그 Context 를 Thread 별로 관리 구현 (#230)

---
+++ backend/src/main/java/com/celuveat/common/log/context/LogContextHolder.java
@@ -0,0 +1,35 @@
+package com.celuveat.common.log.context;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class LogContextHolder {
+
+    private final ThreadLocal<LogContext> holder = new ThreadLocal<>();
+
+    public void setLogContext(LogContext logContext) {
+        holder.set(logContext);
+    }
+
+    public LogContext get() {
+        LogContext logContext = holder.get();
+        if (logContext == null) {
+            logContext = new LogContext(LogId.defaultId());
+            setLogContext(logContext);
+        }
+        return logContext;
+    }
+
+    public void increaseCall() {
+        LogContext logContext = holder.get();
+        logContext.increaseCall();
+    }
+
+    public void decreaseCall() {
+        LogContext logContext = get();
+        logContext.decreaseCall();
+        if (logContext.isFinal()) {
+            holder.remove();
+        }
+    }
+}
