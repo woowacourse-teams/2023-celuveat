6ae3b1379e25add637053176192b85eaf59b7688
odo27 <odo27@naver.com>
2023-08-02 21:52:16 +0900

    feat: 좋아요한 음식점 조회 기능 구현 (#238)

--- backend/src/test/java/com/celuveat/restaurant/application/RestaurantQueryServiceTest.java
+++ backend/src/test/java/com/celuveat/restaurant/application/RestaurantQueryServiceTest.java
@@ -1,25 +1,20 @@
 package com.celuveat.restaurant.application;
 
-import static com.celuveat.auth.fixture.OauthMemberFixture.멤버;
 import static com.celuveat.restaurant.fixture.LocationFixture.isRestaurantInArea;
 import static com.celuveat.restaurant.fixture.LocationFixture.박스_1_2번_지점포함;
 import static com.celuveat.restaurant.fixture.LocationFixture.박스_1번_지점포함;
 import static com.celuveat.restaurant.fixture.LocationFixture.전체영역_검색_범위;
 import static com.celuveat.restaurant.fixture.RestaurantFixture.isCelebVisited;
-import static com.celuveat.restaurant.fixture.RestaurantFixture.음식점;
-import static com.celuveat.restaurant.fixture.RestaurantLikeFixture.음식점_좋아요;
 import static java.util.Comparator.comparing;
 import static org.assertj.core.api.Assertions.assertThat;
 
-import com.celuveat.auth.domain.OauthMember;
 import com.celuveat.auth.domain.OauthMemberRepository;
 import com.celuveat.common.IntegrationTest;
 import com.celuveat.common.SeedData;
 import com.celuveat.common.util.StringUtil;
 import com.celuveat.restaurant.application.dto.CelebQueryResponse;
+import com.celuveat.restaurant.application.dto.RestaurantLikeQueryResponse;
 import com.celuveat.restaurant.application.dto.RestaurantQueryResponse;
-import com.celuveat.restaurant.domain.Restaurant;
-import com.celuveat.restaurant.domain.RestaurantLike;
 import com.celuveat.restaurant.domain.RestaurantLikeRepository;
 import com.celuveat.restaurant.domain.RestaurantQueryRepository.LocationSearchCond;
 import com.celuveat.restaurant.domain.RestaurantQueryRepository.RestaurantSearchCond;
@@ -360,29 +355,37 @@ class RestaurantQueryServiceTest {
     @Test
     void 멤버_아이디로_음식점_좋아요를_검색한다() {
         // given
-        OauthMember 오도 = 멤버("오도");
-        oauthMemberRepository.save(오도);
-        Restaurant 음식점1 = 음식점("맛집1");
-        Restaurant 음식점2 = 음식점("맛집2");
-        Restaurant 음식점3 = 음식점("맛집3");
-        restaurantRepository.saveAll(List.of(음식점1, 음식점2, 음식점3));
-        RestaurantLike 음식점_좋아요1 = 음식점_좋아요(음식점1, 오도);
-        RestaurantLike 음식점_좋아요2 = 음식점_좋아요(음식점2, 오도);
-        RestaurantLike 음식점_좋아요3 = 음식점_좋아요(음식점3, 오도);
-        restaurantLikeRepository.saveAll(List.of(
-                음식점_좋아요1,
-                음식점_좋아요2,
-                음식점_좋아요3
-        ));
+        List<RestaurantLikeQueryResponse> expected = new ArrayList<>();
+        for (RestaurantQueryResponse restaurantQueryResponse : seed) {
+            if (isLikedRestaurant(restaurantQueryResponse.name())) {
+                expected.add(toRestaurantLikeQueryResponse(restaurantQueryResponse));
+            }
+        }
 
         // when
-        List<RestaurantLike> restaurantLikes = restaurantQueryService.findAllRestaurantLikeByMemberId(오도.id());
+        List<RestaurantLikeQueryResponse> restaurantLikes = restaurantQueryService.findAllRestaurantLikeByMemberId(1L);
 
         // then
-        assertThat(restaurantLikes).usingRecursiveComparison().isEqualTo(List.of(
-                음식점_좋아요1,
-                음식점_좋아요2,
-                음식점_좋아요3
-        ));
+        assertThat(restaurantLikes).usingRecursiveComparison().isEqualTo(expected);
+    }
+
+    private boolean isLikedRestaurant(String name) {
+        List<String> likedRestaurants = List.of("말랑1호점", "말랑3호점", "도기2호점", "로이스2호점");
+        return likedRestaurants.contains(name);
+    }
+
+    private RestaurantLikeQueryResponse toRestaurantLikeQueryResponse(RestaurantQueryResponse restaurantQueryResponse) {
+        return new RestaurantLikeQueryResponse(
+                restaurantQueryResponse.id(),
+                restaurantQueryResponse.name(),
+                restaurantQueryResponse.category(),
+                restaurantQueryResponse.roadAddress(),
+                restaurantQueryResponse.latitude(),
+                restaurantQueryResponse.longitude(),
+                restaurantQueryResponse.phoneNumber(),
+                restaurantQueryResponse.naverMapUrl(),
+                restaurantQueryResponse.celebs(),
+                restaurantQueryResponse.images()
+        );
     }
 }
