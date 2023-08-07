e4469e531914a2eef402ba000c0e94e11498749a
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2023-08-07 22:57:36 +0900

    feat: 음식점 상세 조회 응답 객체 생성자 추가 (#274)

---
+++ backend/src/main/java/com/celuveat/restaurant/application/dto/RestaurantDetailQueryResponse.java
@@ -0,0 +1,41 @@
+package com.celuveat.restaurant.application.dto;
+
+import com.celuveat.restaurant.domain.Restaurant;
+import com.celuveat.restaurant.domain.RestaurantImage;
+import com.celuveat.video.domain.Video;
+import java.util.List;
+
+public record RestaurantDetailQueryResponse(
+        Long id,
+        String name,
+        String category,
+        String roadAddress,
+        Double latitude,
+        Double longitude,
+        String phoneNumber,
+        String naverMapUrl,
+        Integer viewCount,
+        List<RestaurantImageQueryResponse> imageUrls,
+        List<VideoWithCelebQueryResponse> videos
+) {
+
+    public static RestaurantDetailQueryResponse of(
+            Restaurant restaurant,
+            List<RestaurantImage> restaurantImages,
+            List<Video> videos
+    ) {
+        return new RestaurantDetailQueryResponse(
+                restaurant.id(),
+                restaurant.name(),
+                restaurant.category(),
+                restaurant.roadAddress(),
+                restaurant.latitude(),
+                restaurant.longitude(),
+                restaurant.phoneNumber(),
+                restaurant.naverMapUrl(),
+                0, //TODO view count
+                restaurantImages.stream().map(RestaurantImageQueryResponse::of).toList(),
+                videos.stream().map(VideoWithCelebQueryResponse::from).toList()
+        );
+    }
+}
---
+++ backend/src/main/java/com/celuveat/restaurant/application/dto/VideoWithCelebQueryResponse.java
@@ -0,0 +1,29 @@
+package com.celuveat.restaurant.application.dto;
+
+import com.celuveat.celeb.domain.Celeb;
+import com.celuveat.video.domain.Video;
+import java.time.LocalDate;
+
+public record VideoWithCelebQueryResponse(
+        Long videoId,
+        String youtubeUrl,
+        LocalDate uploadDate,
+        Long celebId,
+        String name,
+        String youtubeChannelName,
+        String profileImageUrl
+
+) {
+    public static VideoWithCelebQueryResponse from(Video video) {
+        Celeb celeb = video.celeb(); //FIXME: N + 1
+        return new VideoWithCelebQueryResponse(
+                video.id(),
+                video.youtubeUrl(),
+                video.uploadDate(),
+                celeb.id(),
+                celeb.name(),
+                celeb.youtubeChannelName(),
+                celeb.profileImageUrl()
+        );
+    }
+}
