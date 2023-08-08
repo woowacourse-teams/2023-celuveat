87fb9a73c921fa694dd346cfccddf96cfcd071a6
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2023-08-08 17:34:22 +0900

    test: SeedData 내 이미지 작성자 이름 반영 (#274)

--- backend/src/test/java/com/celuveat/restaurant/fixture/RestaurantImageFixture.java
+++ backend/src/test/java/com/celuveat/restaurant/fixture/RestaurantImageFixture.java
@@ -6,11 +6,11 @@ import com.celuveat.restaurant.domain.SocialMedia;
 
 public class RestaurantImageFixture {
 
-    public static RestaurantImage 음식점사진(String name, Restaurant 음식점) {
+    public static RestaurantImage 음식점사진(String name, Restaurant 음식점, String author) {
         return RestaurantImage.builder()
                 .name(name)
                 .socialMedia(SocialMedia.INSTAGRAM)
-                .author(name)
+                .author(author)
                 .restaurant(음식점)
                 .build();
     }
