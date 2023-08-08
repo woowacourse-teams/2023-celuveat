247db239f8871d6925bf55eecb819e61560b439e
kdkdhoho <hkim4410@naver.com>
2023-08-08 21:02:19 +0900

    feat: 운영 환경에서는 admin API에 요청을 보내지 못하도록 AdminController @Profile 설정 (#286)

--- backend/src/main/java/com/celuveat/admin/presentation/AdminController.java
+++ backend/src/main/java/com/celuveat/admin/presentation/AdminController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Profile("!prod")
 @RequiredArgsConstructor
 @RequestMapping("/admin")
 public class AdminController {
