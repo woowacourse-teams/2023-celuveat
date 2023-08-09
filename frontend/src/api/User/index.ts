f18df46626c1984716a5025945e316329ab92b7b
turtle601 <turtle601@naver.com>
2023-08-09 14:58:04 +0900

    refactor: 실제 요청과 msw 요청 시 axios 인스턴스 구분(#228)

---
+++ frontend/src/api/User/index.ts
@@ -0,0 +1,36 @@
+import axios from 'axios';
+import { setupInterceptorsTo } from '~/api/User/HttpUserInterceptor';
+
+import getToken from '~/utils/getToken';
+
+const axiosUserApi = (url: string, options?: Record<string, string>) => {
+  const token = getToken();
+  const instance = axios.create({
+    baseURL: `${process.env.BASE_URL}/api`,
+    headers: {
+      'Content-type': 'application/json',
+      Cookies: `JSESSIONID=${token}`,
+      ...options,
+    },
+  });
+
+  return instance;
+};
+
+const mswUserApi = (url: string, options?: Record<string, string>) => {
+  const token = getToken();
+  const instance = axios.create({
+    baseURL: '/',
+    headers: {
+      'Content-type': 'application/json',
+      Cookies: `JSESSIONID=${token}`,
+      ...options,
+    },
+  });
+
+  return instance;
+};
+
+export const userInstance = setupInterceptorsTo(axiosUserApi(`${process.env.BASE_URL}/api`));
+
+export const userMSWInstance = setupInterceptorsTo(mswUserApi('/'));
