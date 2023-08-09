668acdbd59a1b70e5a68ee021d97d859f708b98e
turtle601 <turtle601@naver.com>
2023-08-09 14:53:56 +0900

    fix: dev 모드 설정 변경 (#228)
    export const SERVER_URL = PROD ? `${process.env.BASE_URL}/api/` : '/'; 구문 삭제

--- frontend/src/constants/api.ts
+++ frontend/src/constants/api.ts
@@ -1,9 +1,5 @@
 export const BASE_URL = `${process.env.BASE_URL}`;
 
-const PROD = process.env.NODE_ENV === 'production';
-
-export const SERVER_URL = PROD ? `${process.env.BASE_URL}/api/` : '/';
-
 export const OAUTH_LINK = {
   google: `${BASE_URL}/api/oauth/google`,
   kakao: `${BASE_URL}/api/oauth/kakao`,
