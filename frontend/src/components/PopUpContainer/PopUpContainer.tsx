ad0b211db0165f35a36993adaf7dcd0a5f0da071
turtle601 <turtle601@naver.com>
2023-08-09 13:01:44 +0900

    fix: 페이지 focus 될 때 마다 PopUp 뜨는 오류 해결 (#228)

--- frontend/src/components/PopUpContainer/PopUpContainer.tsx
+++ frontend/src/components/PopUpContainer/PopUpContainer.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { Modal } from '~/components/@common/Modal';
 import PopUp from '~/components/@common/PopUp/PopUp';
 import useToastState from '~/hooks/store/useToastState';
@@ -7,11 +8,18 @@ interface PopUpContainerProps {
 }
 
 function PopUpContainer({ imgUrl }: PopUpContainerProps) {
-  const { text, isSuccess, isOpen } = useToastState(state => ({
+  const { text, isSuccess, isOpen, close } = useToastState(state => ({
     text: state.text,
+    close: state.close,
     isSuccess: state.isSuccess,
     isOpen: state.isOpen,
   }));
+
+  useEffect(() => {
+    if (isOpen) {
+      close();
+    }
+  }, []);
 
   return <Modal>{isOpen && <PopUp text={text} isSuccess={isSuccess} imgUrl={imgUrl} />}</Modal>;
 }
