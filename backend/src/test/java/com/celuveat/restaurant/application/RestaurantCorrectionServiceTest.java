37a38a927ebd0cf4f1f680c0f3306676a16216e2
shin-mallang <huipulci1@naver.com>
2023-08-09 16:14:35 +0900

    refactor: 레스트랑 수정 요청 서비스 테스트 리팩토링 (#296)

--- backend/src/test/java/com/celuveat/restaurant/application/RestaurantCorrectionServiceTest.java
+++ backend/src/test/java/com/celuveat/restaurant/application/RestaurantCorrectionServiceTest.java
@@ -4,10 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import com.celuveat.common.IntegrationTest;
 import com.celuveat.restaurant.application.dto.CreateRestaurantCorrectionCommand;
-import com.celuveat.restaurant.domain.Restaurant;
-import com.celuveat.restaurant.domain.RestaurantRepository;
 import com.celuveat.restaurant.domain.correction.RestaurantCorrection;
-import com.celuveat.restaurant.domain.correction.RestaurantCorrectionRepository;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DisplayNameGeneration;
@@ -24,25 +21,14 @@ class RestaurantCorrectionServiceTest {
     private RestaurantCorrectionService restaurantCorrectionService;
 
     @Autowired
-    private RestaurantCorrectionRepository restaurantCorrectionRepository;
-
-    @Autowired
-    private RestaurantRepository restaurantRepository;
+    private RestaurantServiceTestHelper restaurantServiceTestHelper;
 
     @Test
     void 음식점_수정_요청을_생성한다() {
         // given
-        Restaurant restaurant = restaurantRepository.save(Restaurant.builder()
-                .name("틀린이름")
-                .category("category")
-                .roadAddress("road")
-                .latitude(1.1)
-                .longitude(1.1)
-                .naverMapUrl("naver")
-                .build()
-        );
+        Long restaurantId = restaurantServiceTestHelper.음식점을_저장한다("틀린이름");
         CreateRestaurantCorrectionCommand command = CreateRestaurantCorrectionCommand.builder()
-                .restaurantId(restaurant.id())
+                .restaurantId(restaurantId)
                 .contents(List.of("음식점 이름이 틀렸어요.", "지도도 틀렸어요. 일좀 똑바로하세요."))
                 .build();
 
@@ -50,11 +36,12 @@ class RestaurantCorrectionServiceTest {
         restaurantCorrectionService.create(command);
 
         // then
-        assertThat(restaurantCorrectionRepository.findAll())
+        List<RestaurantCorrection> restaurantCorrections = restaurantServiceTestHelper.음식점_정보_수정_제안을_모두_조회한다();
+        assertThat(restaurantCorrections)
                 .extracting(RestaurantCorrection::content)
                 .containsExactly("음식점 이름이 틀렸어요.", "지도도 틀렸어요. 일좀 똑바로하세요.");
-        assertThat(restaurantCorrectionRepository.findAll())
+        assertThat(restaurantCorrections)
                 .extracting(it -> it.restaurant().id())
-                .containsExactly(restaurant.id(), restaurant.id());
+                .containsExactly(restaurantId, restaurantId);
     }
 }
---
+++ backend/src/test/java/com/celuveat/restaurant/application/RestaurantServiceTestHelper.java
@@ -0,0 +1,39 @@
+package com.celuveat.restaurant.application;
+
+import com.celuveat.restaurant.domain.Restaurant;
+import com.celuveat.restaurant.domain.RestaurantRepository;
+import com.celuveat.restaurant.domain.correction.RestaurantCorrection;
+import com.celuveat.restaurant.domain.correction.RestaurantCorrectionRepository;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.test.context.ActiveProfiles;
+
+@Component
+@ActiveProfiles("test")
+@RequiredArgsConstructor
+public class RestaurantServiceTestHelper {
+
+    private final RestaurantRepository restaurantRepository;
+    private final RestaurantCorrectionRepository restaurantCorrectionRepository;
+
+    public Long 음식점을_저장한다(String 이름) {
+        return restaurantRepository.save(Restaurant.builder()
+                        .name(이름)
+                        .category(이름)
+                        .roadAddress(이름)
+                        .latitude(1.1)
+                        .longitude(1.1)
+                        .naverMapUrl("naver" + 이름)
+                        .phoneNumber("010-" + 이름)
+                .build()).id();
+    }
+
+    public Restaurant 음식점을_조회한다(Long id) {
+        return restaurantRepository.getById(id);
+    }
+
+    public List<RestaurantCorrection> 음식점_정보_수정_제안을_모두_조회한다() {
+        return restaurantCorrectionRepository.findAll();
+    }
+}
