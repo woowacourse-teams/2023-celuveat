c34064c0fa3abb5be1871cd359f41b2c38dade59
turtle601 <turtle601@naver.com>
2023-08-10 18:08:38 +0900

    feat: InfoDropDown에 위시리스트 항목 추가 (#290)

--- frontend/src/constants/options.ts
+++ frontend/src/constants/options.ts
@@ -1,6 +1,9 @@
 import All from '~/assets/all.png';
 
-export const OPTION_FOR_USER = [{ id: 1, value: '로그아웃' }];
+export const OPTION_FOR_USER = [
+  { id: 1, value: '로그아웃' },
+  { id: 2, value: '위시리스트' },
+];
 
 export const OPTION_FOR_NOT_USER = [{ id: 1, value: '로그인' }];
 
