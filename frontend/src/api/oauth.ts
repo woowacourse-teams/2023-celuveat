37f08284e1099e7a46e2a95d13b2641cf649d781
turtle601 <turtle601@naver.com>
2023-08-11 21:22:36 +0900

    fix: 로그아웃 요청 axios 구문 수정 (#321)

--- frontend/src/api/oauth.ts
+++ frontend/src/api/oauth.ts
@@ -1,8 +1,8 @@
-import { userInstance2 } from '~/api/User';
 import { BASE_URL } from '../constants/api';
 
 import type { Oauth } from '~/@types/oauth.types';
 import { apiClient } from '~/api';
+import { userInstance } from '~/api/User';
 
 const getAccessToken = async (type: Oauth, code: string) => {
   const response = await apiClient.get(`${BASE_URL}/api/oauth/login/${type}?code=${code}`);
@@ -10,7 +10,7 @@ const getAccessToken = async (type: Oauth, code: string) => {
 };
 
 export const getLogout = async (type: Oauth) => {
-  const response = await userInstance2.get(`${BASE_URL}/api/oauth/logout/${type}`);
+  const response = await userInstance.get(`${BASE_URL}/api/oauth/logout/${type}`);
 
   return response.data;
 };
