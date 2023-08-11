c2d24e69e4df93cafe90ff47ae2133c6687dce28
turtle601 <turtle601@naver.com>
2023-08-11 11:40:46 +0900

    feat: 세션 id가 없을 때 로그인 페이지로 이동하는 동작 구현 (#290)

--- frontend/src/pages/WishListPage.tsx
+++ frontend/src/pages/WishListPage.tsx
@@ -1,13 +1,26 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import styled, { css } from 'styled-components';
 
 import Footer from '~/components/@common/Footer';
 import Header from '~/components/@common/Header';
 import RestaurantWishList from '~/components/RestaurantWishList';
+import useTokenState from '~/hooks/store/useTokenState';
 
 import useMediaQuery from '~/hooks/useMediaQuery';
+import { isEmptyString } from '~/utils/compare';
 
 function WishListPage() {
   const { isMobile } = useMediaQuery();
+
+  const token = useTokenState(state => state.token);
+  const navigator = useNavigate();
+
+  useEffect(() => {
+    if (isEmptyString(token)) {
+      navigator('/signUp');
+    }
+  }, []);
 
   return (
     <StyledWishListPageWrapper>
