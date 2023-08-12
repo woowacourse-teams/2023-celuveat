65b5b30039a73aa28626a5a71212910083165ab7
turtle601 <turtle601@naver.com>
2023-08-12 18:53:00 +0900

    fix: JSON.parse 관련 에러 해결 (#327)

--- frontend/src/utils/getToken.ts
+++ frontend/src/utils/getToken.ts
@@ -5,6 +5,10 @@ interface TokenStateType {
 }
 
 const getToken = (): string => {
+  const data = localStorage.getItem('CELUVEAT-STORAGE');
+
+  if (!data) return '';
+
   const tokenState: TokenStateType = JSON.parse(localStorage.getItem('CELUVEAT-STORAGE') || '');
 
   return tokenState.state.token;
