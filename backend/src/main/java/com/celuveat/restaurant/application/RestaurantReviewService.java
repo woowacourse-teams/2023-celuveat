7049fb00289092a983427916308b818b3c06383d
odo27 <odo27@naver.com>
2023-08-14 13:57:18 +0900

    feat: RestaurantReviewService 구현 (#316)

---
+++ backend/src/main/java/com/celuveat/restaurant/application/RestaurantReviewService.java
@@ -0,0 +1,65 @@
+package com.celuveat.restaurant.application;
+
+import static com.celuveat.restaurant.exception.RestaurantReviewExceptionType.PERMISSION_DENIED;
+import static com.celuveat.restaurant.exception.RestaurantReviewExceptionType.RESTAURANT_REVIEW_MISMATCH;
+
+import com.celuveat.auth.domain.OauthMember;
+import com.celuveat.auth.domain.OauthMemberRepository;
+import com.celuveat.restaurant.application.dto.SaveReviewRequest;
+import com.celuveat.restaurant.application.dto.UpdateReviewRequest;
+import com.celuveat.restaurant.domain.Restaurant;
+import com.celuveat.restaurant.domain.RestaurantRepository;
+import com.celuveat.restaurant.domain.review.RestaurantReview;
+import com.celuveat.restaurant.domain.review.RestaurantReviewRepository;
+import com.celuveat.restaurant.exception.RestaurantReviewException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class RestaurantReviewService {
+
+    private final RestaurantRepository restaurantRepository;
+    private final OauthMemberRepository oauthMemberRepository;
+    private final RestaurantReviewRepository restaurantReviewRepository;
+
+    public Long saveReview(SaveReviewRequest request, Long memberId, Long restaurantId) {
+        Restaurant restaurant = restaurantRepository.getById(restaurantId);
+        OauthMember member = oauthMemberRepository.getById(memberId);
+        RestaurantReview restaurantReview = new RestaurantReview(request.content(), member, restaurant);
+        return restaurantReviewRepository.save(restaurantReview).id();
+    }
+
+    public void updateReview(UpdateReviewRequest request, Long reviewId, Long memberId, Long restaurantId) {
+        RestaurantReview review = restaurantReviewRepository.getById(reviewId);
+        validateRequest(review, memberId, restaurantId);
+        review.updateContent(request.content());
+    }
+
+    private void validateRequest(RestaurantReview review, Long memberId, Long restaurantId) {
+        checkReviewAndMemberMatched(review, memberId);
+        checkReviewAndRestaurantMatched(review, restaurantId);
+    }
+
+    private void checkReviewAndMemberMatched(RestaurantReview review, Long memberId) {
+        Long reviewOwnerId = review.oauthMember().id();
+        if (!reviewOwnerId.equals(memberId)) {
+            throw new RestaurantReviewException(PERMISSION_DENIED);
+        }
+    }
+
+    private void checkReviewAndRestaurantMatched(RestaurantReview review, Long restaurantId) {
+        Long actualRestaurantId = review.restaurant().id();
+        if (!actualRestaurantId.equals(restaurantId)) {
+            throw new RestaurantReviewException(RESTAURANT_REVIEW_MISMATCH);
+        }
+    }
+
+    public void deleteReview(Long reviewId, Long memberId, Long restaurantId) {
+        RestaurantReview review = restaurantReviewRepository.getById(reviewId);
+        validateRequest(review, memberId, restaurantId);
+        restaurantReviewRepository.delete(review);
+    }
+}
