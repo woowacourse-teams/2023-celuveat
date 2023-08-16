ebfed9578ab8be375948d59f56fbc8d246a92499
김동호 <66300965+kdkdhoho@users.noreply.github.com>
2023-08-16 11:52:47 +0900

    feat: 데이터 저장시 여러 개의 음식점 사진을 저장할 수 있는 기능 구현 (#340)
    * feat: 음식점 데이터 저장시 여러 개의 음식점 사진을 저장할 수 있는 기능 구현 (#315)
    
    * test: 테스트 고도화 (#315)
    
    * refactor: 사진 이름 구분자를 ", "에서 ","로 변경 (#315)

--- backend/src/test/java/com/celuveat/admin/application/AdminServiceTest.java
+++ backend/src/test/java/com/celuveat/admin/application/AdminServiceTest.java
@@ -26,6 +26,7 @@ import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -51,89 +52,116 @@ class AdminServiceTest {
     @Autowired
     private VideoRepository videoRepository;
 
-    @Test
-    void 저장되어_있지_않은_셀럽으로_데이터를_저장하면_예외가_발생한다() {
-        // given
-        String input = 데이터_입력_생성("도기", "국민연금");
-        List<SaveDataRequest> 요청 = 데이터_저장_요청_생성(input);
+    @Nested
+    class 음식점_데이터_저장 {
 
-        // then
-        BaseExceptionType exceptionType = assertThrows(AdminException.class,
-                () -> adminService.saveData(요청)).exceptionType();
-        assertThat(exceptionType).isEqualTo(NOT_EXISTS_CELEB);
+        @Test
+        void 저장되어_있지_않은_셀럽으로_데이터를_저장하면_예외가_발생한다() {
+            // given
+            String input = 데이터_입력_생성("도기", "국민연금");
+            List<SaveDataRequest> 요청 = 데이터_저장_요청_생성(input);
+
+            // then
+            BaseExceptionType exceptionType = assertThrows(AdminException.class,
+                    () -> adminService.saveData(요청)).exceptionType();
+            assertThat(exceptionType).isEqualTo(NOT_EXISTS_CELEB);
+        }
+
+        @ParameterizedTest
+        @ValueSource(strings = {"2023. 7. 23. ", "23. 7. 23.", "2023.7.23", "2023. 7.8."})
+        void 날짜_형식이_다른_경우_예외가_발생한다(String 영상_업로드_날짜) {
+            // given
+            셀럽_저장(셀럽("도기"));
+
+            // when
+            String input = 잘못된_입력_생성("도기", "농민백암순대", 영상_업로드_날짜);
+            List<SaveDataRequest> 요청 = 데이터_저장_요청_생성(input);
+
+            // then
+            BaseExceptionType exceptionType = assertThrows(AdminException.class,
+                    () -> adminService.saveData(요청)).exceptionType();
+            assertThat(exceptionType).isEqualTo(ILLEGAL_DATE_FORMAT);
+        }
+
+        @Test
+        void 셀럽과_음식점이_저장되어_있고_데이터_두_개를_저장한다_한_개는_존재하는_음식점이고_나머지_하나는_존재하지_않는다() {
+            // given
+            셀럽_저장(셀럽("도기"));
+            음식점_저장(국민연금_구내식당);
+
+            String input = 데이터_입력_생성("도기", "국민연금") + System.lineSeparator() + 데이터_입력_생성("도기", "농민백암순대");
+            List<SaveDataRequest> 요청 = 데이터_저장_요청_생성(input);
+
+            // when
+            adminService.saveData(요청);
+
+            // then
+            assertThat(celebRepository.count()).isEqualTo(1);
+            assertThat(restaurantRepository.count()).isEqualTo(2);
+            assertThat(restaurantImageRepository.count()).isEqualTo(2);
+            assertThat(videoRepository.count()).isEqualTo(2);
+        }
+
+        @Test
+        void 음식점이_이미_저장되어_있고_데이터_두_개를_저장한다_둘_다_같은_음식점_데이터이지만_다른_셀럽이다() {
+            // given
+            셀럽_저장(셀럽("도기"));
+            셀럽_저장(셀럽("로이스"));
+            음식점_저장(국민연금_구내식당);
+
+            String input = 데이터_입력_생성("도기", "국민연금") + System.lineSeparator() + 데이터_입력_생성("로이스", "국민연금");
+            List<SaveDataRequest> 요청 = 데이터_저장_요청_생성(input);
+
+            // when
+            adminService.saveData(요청);
+
+            // then
+            assertThat(celebRepository.count()).isEqualTo(2);
+            assertThat(restaurantRepository.count()).isEqualTo(1);
+            assertThat(restaurantImageRepository.count()).isEqualTo(2);
+            assertThat(videoRepository.count()).isEqualTo(2);
+        }
+
+        @Test
+        void 셀럽만_저장되어_있고_데이터_한_개를_저장한다() {
+            // given
+            셀럽_저장(셀럽("도기"));
+
+            String input = 데이터_입력_생성("도기", "국민연금");
+            List<SaveDataRequest> 요청 = 데이터_저장_요청_생성(input);
+
+            // when
+            adminService.saveData(요청);
+
+            // then
+            assertThat(celebRepository.count()).isEqualTo(1);
+            assertThat(restaurantRepository.count()).isEqualTo(1);
+            assertThat(restaurantImageRepository.count()).isEqualTo(1);
+            assertThat(videoRepository.count()).isEqualTo(1);
+        }
     }
 
-    @ParameterizedTest
-    @ValueSource(strings = {"2023. 7. 23. ", "23. 7. 23.", "2023.7.23", "2023. 7.8."})
-    void 날짜_형식이_다른_경우_예외가_발생한다(String 영상_업로드_날짜) {
-        // given
-        셀럽_저장(셀럽("도기"));
+    @Nested
+    class 셀럽_저장 {
 
-        // when
-        String input = 잘못된_입력_생성("도기", "농민백암순대", 영상_업로드_날짜);
-        List<SaveDataRequest> 요청 = 데이터_저장_요청_생성(input);
+        @Test
+        void 셀럽을_저장한다() {
+            // given
+            List<Celeb> expected = List.of(셀럽("도기"), 셀럽("로이스"));
+            String input = 셀럽_입력_생성("도기")
+                    + System.lineSeparator()
+                    + 셀럽_입력_생성("로이스");
+            List<SaveCelebRequest> 요청 = 셀럽_저장_요청_생성(input);
 
-        // then
-        BaseExceptionType exceptionType = assertThrows(AdminException.class,
-                () -> adminService.saveData(요청)).exceptionType();
-        assertThat(exceptionType).isEqualTo(ILLEGAL_DATE_FORMAT);
-    }
+            // when
+            adminService.saveCelebs(요청);
 
-    @Test
-    void 셀럽과_음식점이_저장되어_있고_데이터_두_개를_저장한다_한_개는_존재하는_음식점이고_나머지_하나는_존재하지_않는다() {
-        // given
-        셀럽_저장(셀럽("도기"));
-        음식점_저장(국민연금_구내식당);
-
-        String input = 데이터_입력_생성("도기", "국민연금") + System.lineSeparator() + 데이터_입력_생성("도기", "농민백암순대");
-        List<SaveDataRequest> 요청 = 데이터_저장_요청_생성(input);
-
-        // when
-        adminService.saveData(요청);
-
-        // then
-        assertThat(celebRepository.count()).isEqualTo(1);
-        assertThat(restaurantRepository.count()).isEqualTo(2);
-        assertThat(restaurantImageRepository.count()).isEqualTo(2);
-        assertThat(videoRepository.count()).isEqualTo(2);
-    }
-
-    @Test
-    void 음식점이_이미_저장되어_있고_데이터_두_개를_저장한다_둘_다_같은_음식점_데이터이지만_다른_셀럽이다() {
-        // given
-        셀럽_저장(셀럽("도기"));
-        셀럽_저장(셀럽("로이스"));
-        음식점_저장(국민연금_구내식당);
-
-        String input = 데이터_입력_생성("도기", "국민연금") + System.lineSeparator() + 데이터_입력_생성("로이스", "국민연금");
-        List<SaveDataRequest> 요청 = 데이터_저장_요청_생성(input);
-
-        // when
-        adminService.saveData(요청);
-
-        // then
-        assertThat(celebRepository.count()).isEqualTo(2);
-        assertThat(restaurantRepository.count()).isEqualTo(1);
-        assertThat(restaurantImageRepository.count()).isEqualTo(2);
-        assertThat(videoRepository.count()).isEqualTo(2);
-    }
-
-    @Test
-    void 셀럽만_저장되어_있고_데이터_한_개를_저장한다() {
-        // given
-        셀럽_저장(셀럽("도기"));
-
-        String input = 데이터_입력_생성("도기", "국민연금");
-        List<SaveDataRequest> 요청 = 데이터_저장_요청_생성(input);
-
-        // when
-        adminService.saveData(요청);
-
-        // then
-        assertThat(celebRepository.count()).isEqualTo(1);
-        assertThat(restaurantRepository.count()).isEqualTo(1);
-        assertThat(restaurantImageRepository.count()).isEqualTo(1);
-        assertThat(videoRepository.count()).isEqualTo(1);
+            // then
+            assertThat(celebRepository.count()).isEqualTo(2);
+            assertThat(celebRepository.findAll()).usingRecursiveComparison()
+                    .comparingOnlyFields("name", "youtubeChannelName")
+                    .isEqualTo(expected);
+        }
     }
 
     private Celeb 셀럽_저장(Celeb 셀럽) {
@@ -156,24 +184,5 @@ class AdminServiceTest {
                 "\t음식점네이버링크" +
                 "\t12.3456" +
                 "\t12.3456";
-    }
-
-    @Test
-    void 셀럽을_저장한다() {
-        // given
-        List<Celeb> expected = List.of(셀럽("도기"), 셀럽("로이스"));
-        String input = 셀럽_입력_생성("도기")
-                + System.lineSeparator()
-                + 셀럽_입력_생성("로이스");
-        List<SaveCelebRequest> 요청 = 셀럽_저장_요청_생성(input);
-
-        // when
-        adminService.saveCelebs(요청);
-
-        // then
-        assertThat(celebRepository.count()).isEqualTo(2);
-        assertThat(celebRepository.findAll()).usingRecursiveComparison()
-                .comparingOnlyFields("name", "youtubeChannelName")
-                .isEqualTo(expected);
     }
 }
