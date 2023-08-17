84926569837cb17accb4de8113ebb16ab475c5f3
turtle601 <turtle601@naver.com>
2023-08-17 10:54:39 +0900

    feat: 로그인 팝업 창 구현 (#373)

--- frontend/src/components/@common/LoginButton/LoginButton.tsx
+++ frontend/src/components/@common/LoginButton/LoginButton.tsx
@@ -1,6 +1,6 @@
 import { useNavigate } from 'react-router-dom';
 import styled, { css } from 'styled-components';
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { OAUTH_BUTTON_MESSAGE, OAUTH_LINK } from '~/constants/api';
 
 import KaKao from '~/assets/icons/oauth/kakao.svg';
@@ -9,6 +9,11 @@ import Google from '~/assets/icons/oauth/google.svg';
 import { Oauth } from '~/@types/oauth.types';
 import useTokenState from '~/hooks/store/useTokenState';
 import { FONT_SIZE } from '~/styles/common';
+import { getAccessToken } from '~/api/oauth';
+
+interface OauthCodeResponse {
+  jsessionId: string;
+}
 
 interface LoginButtonProps {
   type: Oauth;
@@ -21,18 +26,64 @@ const LoginIcon: Record<string, React.ReactNode> = {
 };
 
 function LoginButton({ type }: LoginButtonProps) {
+  const [popUp, setPopup] = useState<Window | null>(null);
+
   const navigator = useNavigate();
-  const updateOauth = useTokenState(state => state.updateOauth);
+  const [updateOauth, updateToken] = useTokenState(state => [state.updateOauth, state.updateToken]);
 
-  const onClick = () => {
-    window.open(OAUTH_LINK[type]);
+  const handleOpenGithubOAuthPopup = useCallback((): void => {
+    const title = '로그인 중...';
+    const width = 500;
+    const height = 400;
+    const left = window.screenX + (window.outerWidth - width) / 2;
+    const top = window.screenY + (window.outerHeight - height) / 2.5;
+    const url = OAUTH_LINK[type];
+    const popup = window.open(url, title, `width=${width},height=${height},left=${left},top=${top}`);
 
+    setPopup(popup);
     updateOauth(type);
-    navigator('/');
-  };
+  }, [popUp]);
+
+  const clearPopup = useCallback((): void => {
+    if (popUp !== null) {
+      popUp.close();
+    }
+
+    setPopup(null);
+  }, [popUp]);
+
+  useEffect(() => {
+    if (popUp === null) {
+      return;
+    }
+
+    const oauthCodeListener = (e: MessageEvent<any>): void => {
+      if (e.origin !== window.location.origin) return;
+
+      const { code } = e.data;
+
+      getAccessToken(type, code)
+        .then(response => {
+          const { jsessionId } = response.data as OauthCodeResponse;
+          updateToken(jsessionId);
+        })
+        .catch(() => {
+          navigator('/fail');
+        })
+        .finally(() => clearPopup());
+    };
+
+    window.addEventListener('message', oauthCodeListener, false);
+
+    // eslint-disable-next-line consistent-return
+    return () => {
+      window.removeEventListener('message', oauthCodeListener);
+      clearPopup();
+    };
+  }, [popUp]);
 
   return (
-    <StyledLoginButtonWrapper type={type} onClick={onClick}>
+    <StyledLoginButtonWrapper type={type} onClick={handleOpenGithubOAuthPopup}>
       <div>{LoginIcon[type]}</div>
       <StyledLoginButtonText>{OAUTH_BUTTON_MESSAGE[type]}</StyledLoginButtonText>
     </StyledLoginButtonWrapper>
