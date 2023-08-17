7c04e7374cc4c7daad171e52f32193eaa65aea38
Jeremy <102432453+shackstack@users.noreply.github.com>
2023-08-17 21:52:23 +0900

    fix: 리액트 쿼리 아이콘 제거 (#395)

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,5 @@
 import ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import App from '~/App';
 import GlobalStyles from './styles/GlobalStyles';
 import { worker } from './mocks/browser';
@@ -20,7 +19,6 @@ root.render(
     <GlobalStyles />
     <QueryClientProvider client={queryClient}>
       <App />
-      {!PROD && <ReactQueryDevtools />}
     </QueryClientProvider>
   </>,
 );
