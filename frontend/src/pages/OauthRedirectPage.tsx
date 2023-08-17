253bd5555cbb2f194257143cb3bc0f355cb27d57
d0dam <smallkdb@gmail.com>
2023-08-17 17:42:28 +0900

    fix: 로그인 새로고침 에러 수정

--- frontend/src/pages/OauthRedirectPage.tsx
+++ frontend/src/pages/OauthRedirectPage.tsx
@@ -1,16 +1,13 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQuery } from '@tanstack/react-query';
 import React, { useEffect } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 
 import { getAccessToken } from '~/api/oauth';
 import useTokenState from '~/hooks/store/useTokenState';
+import MainPage from './MainPage';
 
 interface OauthRedirectProps {
   type: 'google' | 'kakao' | 'naver';
-}
-
-interface OauthCodeResponse {
-  jsessionId: string;
 }
 
 function OauthRedirectPage({ type }: OauthRedirectProps) {
@@ -22,32 +19,32 @@ function OauthRedirectPage({ type }: OauthRedirectProps) {
   const searchParams = new URLSearchParams(location.search);
   const code = searchParams.get('code');
 
-  const oauthTokenMutation = useMutation({
-    mutationFn: () => getAccessToken(type, code),
-    onSuccess: (data: OauthCodeResponse) => {
-      const { jsessionId } = data;
-
-      updateToken(jsessionId);
-
-      navigator('/');
-    },
+  const {
+    data: { jsessionId },
+    isLoading,
+  } = useQuery({
+    queryKey: ['oauthToken', type, code],
+    queryFn: () => getAccessToken(type, code),
     onError: () => {
       navigator('/fail');
     },
   });
 
   useEffect(() => {
-    if (code) {
-      oauthTokenMutation.mutate();
-      updateOauth(type);
-    }
-  }, [code]);
+    updateToken(jsessionId);
+    updateOauth(type);
+    navigator('/');
+  }, [jsessionId]);
 
-  return (
-    <div>
-      <div>Processing...</div>
-    </div>
-  );
+  if (isLoading) {
+    return (
+      <div>
+        <div>Processing...</div>
+      </div>
+    );
+  }
+
+  return <MainPage />;
 }
 
 export default OauthRedirectPage;
