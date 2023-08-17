4906050678864c3f20cf006d5b37eb61bc5dc7d5
shin-mallang <huipulci1@naver.com>
2023-08-17 11:16:34 +0900

    fix: 예외 로그에 uri 추가

--- backend/src/main/java/com/celuveat/common/exception/ExceptionControllerAdvice.java
+++ backend/src/main/java/com/celuveat/common/exception/ExceptionControllerAdvice.java
@@ -4,6 +4,7 @@ import static org.springframework.http.HttpStatus.BAD_REQUEST;
 
 import com.celuveat.common.log.context.LogContext;
 import com.celuveat.common.log.context.LogContextHolder;
+import jakarta.servlet.http.HttpServletRequest;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -21,27 +22,31 @@ public class ExceptionControllerAdvice {
     private final LogContextHolder logContextHolder;
 
     @ExceptionHandler(BaseException.class)
-    ResponseEntity<ExceptionResponse> handleException(BaseException e) {
+    ResponseEntity<ExceptionResponse> handleException(HttpServletRequest request, BaseException e) {
         BaseExceptionType type = e.exceptionType();
         LogContext logContext = logContextHolder.get();
-        log.info("[BAD_REQUEST][{}] 잘못된 요청이 들어왔습니다. 내용:  {}", logContext.logId(), type.errorMessage());
+        log.info("[BAD_REQUEST][{}] 잘못된 요청이 들어왔습니다. URI: {},  내용:  {}",
+                logContext.logId(), request.getRequestURI(), type.errorMessage());
         return ResponseEntity.status(type.httpStatus())
                 .body(new ExceptionResponse(type.errorMessage()));
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    ResponseEntity<ExceptionResponse> handleMethodArgumentNotValid(MethodArgumentNotValidException e) {
+    ResponseEntity<ExceptionResponse> handleMethodArgumentNotValid(HttpServletRequest request,
+                                                                   MethodArgumentNotValidException e) {
         String errorMessage = e.getFieldErrors().stream()
                 .map(it -> it.getField() + " : " + it.getDefaultMessage())
                 .collect(Collectors.joining("  "));
         LogContext logContext = logContextHolder.get();
-        log.info("[BAD_REQUEST][{}] 잘못된 요청이 들어왔습니다. 내용:  {}", logContext.logId(), errorMessage);
+        log.info("[BAD_REQUEST][{}] 잘못된 요청이 들어왔습니다. URI: {},  내용:  {}",
+                logContext.logId(), request.getRequestURI(), errorMessage);
         return ResponseEntity.status(BAD_REQUEST)
                 .body(new ExceptionResponse(errorMessage));
     }
 
     @ExceptionHandler(MissingServletRequestParameterException.class)
-    ResponseEntity<ExceptionResponse> handleMissingParams(MissingServletRequestParameterException e) {
+    ResponseEntity<ExceptionResponse> handleMissingParams(HttpServletRequest request,
+                                                          MissingServletRequestParameterException e) {
         LogContext logContext = logContextHolder.get();
         String errorMessage = e.getParameterName() + " 값이 누락 되었습니다.";
         log.info("[BAD_REQUEST][{}] 잘못된 요청이 들어왔습니다. 내용:  {}", logContext.logId(), errorMessage);
@@ -50,9 +55,10 @@ public class ExceptionControllerAdvice {
     }
 
     @ExceptionHandler(Exception.class)
-    ResponseEntity<ExceptionResponse> handleException(Exception e) {
+    ResponseEntity<ExceptionResponse> handleException(HttpServletRequest request, Exception e) {
         LogContext logContext = logContextHolder.get();
-        log.error("[ERROR][{}] 예상하지 못한 예외가 발생했습니다.", logContext.logId(), e);
+        log.error("[ERROR][{}] 예상하지 못한 예외가 발생했습니다. URI: {}, ",
+                logContext.logId(), request.getRequestURI(), e);
         return ResponseEntity.internalServerError()
                 .body(new ExceptionResponse("알 수 없는 오류가 발생했습니다."));
     }
