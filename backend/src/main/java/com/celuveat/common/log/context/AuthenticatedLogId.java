fddb1aa31bdfb9804e3489180bad99579135a803
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2023-08-18 11:23:09 +0900

    hotfix: 회원 요청 로깅 분리

--- backend/src/main/java/com/celuveat/common/log/context/AuthenticatedLogId.java
+++ backend/src/main/java/com/celuveat/common/log/context/AuthenticatedLogId.java
@@ -1,15 +1,19 @@
 package com.celuveat.common.log.context;
 
+import java.util.UUID;
+
 public class AuthenticatedLogId implements LogId {
 
     private final String memberId;
+    private final String requestId;
 
     public AuthenticatedLogId(Object memberId) {
         this.memberId = String.valueOf(memberId);
+        this.requestId = UUID.randomUUID().toString().substring(0, 8);
     }
 
     @Override
     public String logId() {
-        return memberId + "(memberId)";
+        return memberId + "(memberId)" + "-" + requestId;
     }
 }
