680dac1072a34cca91302fe5f779778d7b83eaa2
odo27 <odo27@naver.com>
2023-09-01 14:57:46 +0900

    refactor: !prod 로 변경 (#405)

--- backend/src/main/java/com/celuveat/auth/presentation/LocalAuthController.java
+++ backend/src/main/java/com/celuveat/auth/presentation/LocalAuthController.java
@@ -16,8 +16,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@Profile("dev")
 @RestController
+@Profile("!prod")
 @RequiredArgsConstructor
 public class LocalAuthController {
 
