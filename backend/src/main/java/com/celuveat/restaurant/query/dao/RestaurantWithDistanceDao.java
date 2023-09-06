79f5c4c153f8d9420749f3e221a5cd4f89da50f7
Donghun Shin <huipulci1@naver.com>
2023-09-06 16:43:44 +0900

    feat: querydsl을 도입한다 (#424)
    * infra: querydsl 설정 (#419)
    
    * infra: querydsl 편의 세팅 (#419)
    
    * refactor: VideoWithCelebQueryResponseDao 쿼리Dsl 적용 (#419)
    
    * refactor: RestaurantWithDistanceDao 쿼리Dsl 1차 적용 (#419)
    
    * refactor: RestaurantWithDistanceDao 쿼리Dsl 적용 (#419)
    
    * refactor: polishing (#419)

--- backend/src/main/java/com/celuveat/restaurant/query/dao/RestaurantWithDistanceDao.java
+++ backend/src/main/java/com/celuveat/restaurant/query/dao/RestaurantWithDistanceDao.java
@@ -1,17 +1,28 @@
 package com.celuveat.restaurant.query.dao;
 
-import static com.celuveat.common.query.DynamicQueryCondition.notNull;
-import static com.celuveat.common.query.DynamicQueryCondition.notNullRecursive;
+import static com.celuveat.celeb.command.domain.QCeleb.celeb;
 import static com.celuveat.common.util.StringUtil.removeAllBlank;
-import static org.springframework.util.StringUtils.hasText;
+import static com.celuveat.restaurant.command.domain.QRestaurant.restaurant;
+import static com.celuveat.restaurant.command.domain.QRestaurantLike.restaurantLike;
+import static com.celuveat.video.command.domain.QVideo.video;
+import static com.querydsl.core.types.Order.DESC;
 
-import com.celuveat.common.query.DynamicQuery;
-import com.celuveat.common.query.DynamicQueryAssembler;
 import com.celuveat.restaurant.command.domain.Restaurant;
 import com.celuveat.restaurant.exception.RestaurantException;
 import com.celuveat.restaurant.exception.RestaurantExceptionType;
+import com.celuveat.restaurant.query.dao.support.RestaurantQueryDaoSupport;
 import com.celuveat.restaurant.query.dto.RestaurantWithDistance;
-import jakarta.persistence.EntityManager;
+import com.querydsl.core.types.OrderSpecifier;
+import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.SubQueryExpression;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.core.types.dsl.Expressions;
+import com.querydsl.core.types.dsl.NumberExpression;
+import com.querydsl.core.types.dsl.NumberPath;
+import com.querydsl.jpa.JPAExpressions;
+import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import io.micrometer.common.util.StringUtils;
 import java.util.Arrays;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -27,98 +38,20 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 public class RestaurantWithDistanceDao {
 
-    private static final String HAVERSINE_FORMULA = """
-            (6371 * acos(cos(radians(%s)) * cos(radians(latitude))
-                 * cos(radians(longitude) - radians(%s))
-                 + sin(radians(%s)) * sin(radians(latitude)))) * 1000
-             """;
+    private static final NumberPath<Double> distanceColumn = Expressions.numberPath(Double.class, "distance");
 
-    private static final String SELECT_RESTAURANT_JOIN_VIDEO_AND_CELEB = """
-            SELECT DISTINCT new com.celuveat.restaurant.query.dto.RestaurantWithDistance(
-                r.id,
-                r.name,
-                r.category,
-                r.roadAddress,
-                r.latitude,
-                r.longitude,
-                r.phoneNumber,
-                r.naverMapUrl,
-                r.viewCount,
-                %s AS dist
-            )
-            FROM Restaurant r
-            JOIN Video v
-            ON v.restaurant = r
-            JOIN Celeb c
-            ON c = v.celeb
-            """;
+    private final JPAQueryFactory query;
+    private final RestaurantQueryDaoSupport restaurantQueryDaoSupport;
 
-    private static final String CELEB_ID_EQUAL = "c.id = %d";
-
-    private static final String RESTAURANT_CATEGORY_EQUAL = "r.category = '%s'";
-
-    private static final String RESTAURANT_NAME_LIKE_IGNORE_CASE_IGNORE_BLANK = """
-            Function('replace', lower(r.name), ' ', '') like lower('%%%s%%')
-            """;
-
-    private static final String RESTAURANT_IN_AREA = """
-            latitude BETWEEN %s AND %s
-            AND
-            longitude BETWEEN %s AND %s
-            """;
-
-    private static final String ORDER_BY_DISTANCE_ASC = """
-            ORDER BY dist ASC
-            """;
-
-    //FIXME
-    private static final String ORDER_BY_LIKE_DESC = """
-            ORDER BY (
-                SELECT count(rl)
-                FROM RestaurantLike rl
-                WHERE rl.restaurant = r
-                ) DESC
-            """;
-
-    private static final String COUNT_QUERY_JOIN_VIDEO_AND_CELEB = """
-            SELECT count(DISTINCT r)
-            FROM Restaurant r
-            JOIN Video v
-            ON v.restaurant = r
-            JOIN Celeb c
-            ON c = v.celeb
-            """;
-
-    private static final String SELECT_RESTAURANT_NEARBY_SPECIFIC_DISTANCE = """
-            SELECT new com.celuveat.restaurant.query.dto.RestaurantWithDistance(
-                r.id,
-                r.name,
-                r.category,
-                r.roadAddress,
-                r.latitude,
-                r.longitude,
-                r.phoneNumber,
-                r.naverMapUrl,
-                r.viewCount,
-                %s AS dist
-            )
-            FROM Restaurant r
-            """;
-
-    private static final String DISTANCE_MIN_OR_EQUAL = """
-            %s <= %d
-            """;
-
-    private static final String RESTAURANT_ID_NOT_EQUAL = """
-            r.id != %d
-            """;
-
-    private static final String COUNT_QUERY_NEARBY_DISTANCE = """
-            SELECT count(r)
-            FROM Restaurant r
-            """;
-
-    private final EntityManager em;
+    private NumberExpression<Double> distance(double latitude, double longitude) {
+        return Expressions.numberTemplate(Double.class,
+                """
+                        6371 * acos(cos(radians({1})) * cos(radians({0}.latitude))
+                        * cos(radians({0}.longitude) - radians({2}))
+                        + sin(radians({1})) * sin(radians({0}.latitude))) * 1000
+                        """,
+                restaurant, latitude, longitude);
+    }
 
     public Page<RestaurantWithDistance> search(
             RestaurantSearchCond restaurantSearchCond,
@@ -127,87 +60,91 @@ public class RestaurantWithDistanceDao {
     ) {
         double middleLat = calculateMiddle(locationSearchCond.lowLatitude, locationSearchCond.highLatitude);
         double middleLng = calculateMiddle(locationSearchCond.lowLongitude, locationSearchCond.highLongitude);
-        String whereQuery = DynamicQueryAssembler.assemble(
-                celebIdEqual(restaurantSearchCond),
-                restaurantCategoryEqual(restaurantSearchCond),
-                restaurantNameLike(restaurantSearchCond),
-                restaurantInArea(locationSearchCond)
-        );
-        List<RestaurantWithDistance> resultList = em.createQuery(
-                        SELECT_RESTAURANT_JOIN_VIDEO_AND_CELEB.formatted(getDistanceColumn(middleLat, middleLng))
-                                + whereQuery
-                                + applyOrderBy(pageable),
-                        RestaurantWithDistance.class
-                )
-                .setFirstResult((int) pageable.getOffset())
-                .setMaxResults(pageable.getPageSize())
-                .getResultList();
-        return PageableExecutionUtils.getPage(
-                resultList,
-                pageable,
-                () -> (Long) em.createQuery(COUNT_QUERY_JOIN_VIDEO_AND_CELEB + whereQuery).getSingleResult()
-        );
+        List<RestaurantWithDistance> resultList = query.selectDistinct(Projections.constructor(
+                        RestaurantWithDistance.class,
+                        restaurant.id,
+                        restaurant.name,
+                        restaurant.category,
+                        restaurant.roadAddress,
+                        restaurant.latitude,
+                        restaurant.longitude,
+                        restaurant.phoneNumber,
+                        restaurant.naverMapUrl,
+                        restaurant.viewCount,
+                        distance(middleLat, middleLng).as(distanceColumn)
+                ))
+                .from(restaurant)
+                .join(video).on(video.restaurant.eq(restaurant))
+                .join(celeb).on(celeb.eq(video.celeb))
+                .where(
+                        celebIdEqual(restaurantSearchCond.celebId),
+                        restaurantCategoryEqual(restaurantSearchCond.category),
+                        restaurantNameLike(restaurantSearchCond.restaurantName),
+                        restaurantInArea(locationSearchCond)
+                ).orderBy(applyOrderBy(pageable))
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+
+        JPAQuery<Long> countQuery = query.select(restaurant.countDistinct())
+                .from(restaurant)
+                .join(video).on(video.restaurant.eq(restaurant))
+                .join(celeb).on(celeb.eq(video.celeb))
+                .where(
+                        celebIdEqual(restaurantSearchCond.celebId),
+                        restaurantCategoryEqual(restaurantSearchCond.category),
+                        restaurantNameLike(restaurantSearchCond.restaurantName),
+                        restaurantInArea(locationSearchCond)
+                );
+
+        return PageableExecutionUtils.getPage(resultList, pageable, countQuery::fetchOne);
     }
 
     private double calculateMiddle(double x, double y) {
         return (x + y) / 2.0;
     }
 
-    private DynamicQuery celebIdEqual(RestaurantSearchCond restaurantSearchCond) {
-        return DynamicQuery.builder()
-                .query(CELEB_ID_EQUAL)
-                .params(restaurantSearchCond.celebId())
-                .condition(notNull(restaurantSearchCond.celebId()))
-                .build();
+    private BooleanExpression celebIdEqual(Long celebId) {
+        if (celebId == null) {
+            return null;
+        }
+        return celeb.id.eq(celebId);
     }
 
-    private DynamicQuery restaurantCategoryEqual(
-            RestaurantSearchCond restaurantSearchCond) {
-        return DynamicQuery.builder()
-                .query(RESTAURANT_CATEGORY_EQUAL)
-                .params(restaurantSearchCond.category())
-                .condition(hasText(restaurantSearchCond.category()))
-                .build();
+    private BooleanExpression restaurantCategoryEqual(String category) {
+        if (StringUtils.isBlank(category)) {
+            return null;
+        }
+        return restaurant.category.eq(category);
     }
 
-    private DynamicQuery restaurantNameLike(RestaurantSearchCond restaurantSearchCond) {
-        return DynamicQuery.builder()
-                .query(RESTAURANT_NAME_LIKE_IGNORE_CASE_IGNORE_BLANK)
-                .params(removeAllBlank(restaurantSearchCond.restaurantName()))
-                .condition(hasText(restaurantSearchCond.restaurantName()))
-                .build();
+    private BooleanExpression restaurantNameLike(String restaurantName) {
+        if (StringUtils.isBlank(restaurantName)) {
+            return null;
+        }
+        return restaurant.name.contains(removeAllBlank(restaurantName));
     }
 
-    private DynamicQuery restaurantInArea(LocationSearchCond locationSearchCond) {
-        return DynamicQuery.builder()
-                .query(RESTAURANT_IN_AREA)
-                .params(locationSearchCond.lowLatitude, locationSearchCond.highLatitude,
-                        locationSearchCond.lowLongitude, locationSearchCond.highLongitude)
-                .condition(notNullRecursive(locationSearchCond))
-                .build();
+    private BooleanExpression restaurantInArea(LocationSearchCond locationSearchCond) {
+        // TODO null 처리
+        return restaurant.latitude.between(locationSearchCond.lowLatitude, locationSearchCond.highLatitude)
+                .and(restaurant.longitude.between(locationSearchCond.lowLongitude, locationSearchCond.highLongitude));
     }
 
-    private String getDistanceColumn(double middleLat, double middleLng) {
-        return HAVERSINE_FORMULA.formatted(middleLat, middleLng, middleLat);
-    }
-
-    //FIXME
-    private String applyOrderBy(Pageable pageable) {
-        String sortProperty = pageable.getSort().stream()
+    private OrderSpecifier<?> applyOrderBy(Pageable pageable) {
+        RestaurantSortType sortType = pageable.getSort().stream()
                 .map(Order::getProperty)
                 .findFirst()
-                .orElse(RestaurantSortType.DISTANCE.sortProperty);
-        RestaurantSortType sortType = RestaurantSortType.from(
-                sortProperty);
-        if (sortType == RestaurantSortType.DISTANCE) {
-            return ORDER_BY_DISTANCE_ASC;
-        }
-
+                .map(RestaurantSortType::from)
+                .orElse(RestaurantSortType.DISTANCE);
         if (sortType == RestaurantSortType.LIKE_COUNT) {
-            return ORDER_BY_LIKE_DESC;
+            SubQueryExpression<Long> orderByLikeDesc = JPAExpressions
+                    .select(restaurantLike.count())
+                    .from(restaurantLike)
+                    .where(restaurantLike.restaurant.eq(restaurant));
+            return new OrderSpecifier<>(DESC, orderByLikeDesc);
         }
-
-        return ORDER_BY_DISTANCE_ASC;
+        return distanceColumn.asc();
     }
 
     public Page<RestaurantWithDistance> searchNearBy(
@@ -215,45 +152,46 @@ public class RestaurantWithDistanceDao {
             int distance,
             Pageable pageable
     ) {
-        Restaurant restaurant = em.find(Restaurant.class, restaurantId);
-        String dist = getDistanceColumn(restaurant.latitude(), restaurant.longitude());
-        String whereQuery = DynamicQueryAssembler.assemble(
-                distanceMinOrEqual(dist, distance),
-                restaurantIdNotEqual(restaurant.id())
-        );
-        List<RestaurantWithDistance> result = em.createQuery(
-                        SELECT_RESTAURANT_NEARBY_SPECIFIC_DISTANCE.formatted(dist)
-                                + whereQuery
-                                + ORDER_BY_DISTANCE_ASC,
-                        RestaurantWithDistance.class
-                )
-                .setFirstResult((int) pageable.getOffset())
-                .setMaxResults(pageable.getPageSize())
-                .getResultList();
-        return PageableExecutionUtils.getPage(
-                result,
-                pageable,
-                () -> (Long) em.createQuery(COUNT_QUERY_NEARBY_DISTANCE + whereQuery).getSingleResult()
-        );
+        Restaurant standard = restaurantQueryDaoSupport.getById(restaurantId);
+        List<RestaurantWithDistance> result = query.select(Projections.constructor(RestaurantWithDistance.class,
+                        restaurant.id,
+                        restaurant.name,
+                        restaurant.category,
+                        restaurant.roadAddress,
+                        restaurant.latitude,
+                        restaurant.longitude,
+                        restaurant.phoneNumber,
+                        restaurant.naverMapUrl,
+                        restaurant.viewCount,
+                        distance(standard.latitude(), standard.longitude()).as(RestaurantWithDistanceDao.distanceColumn)
+                ))
+                .from(restaurant)
+                .where(
+                        distanceMinOrEqual(standard, distance),
+                        restaurantIdNotEqual(restaurantId)
+                ).orderBy(RestaurantWithDistanceDao.distanceColumn.asc())
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+
+        JPAQuery<Long> countQuery = query.select(restaurant.count())
+                .from(restaurant)
+                .where(
+                        distanceMinOrEqual(standard, distance),
+                        restaurantIdNotEqual(restaurantId)
+                );
+
+        return PageableExecutionUtils.getPage(result, pageable, countQuery::fetchOne);
     }
 
-    private DynamicQuery distanceMinOrEqual(String distColumn, int distance) {
-        return DynamicQuery.builder()
-                .query(DISTANCE_MIN_OR_EQUAL)
-                .params(distColumn, distance)
-                .condition(true)
-                .build();
+    private BooleanExpression distanceMinOrEqual(Restaurant standard, int distance) {
+        return distance(standard.latitude(), standard.longitude()).loe(distance);
     }
 
-    private DynamicQuery restaurantIdNotEqual(Long id) {
-        return DynamicQuery.builder()
-                .query(RESTAURANT_ID_NOT_EQUAL)
-                .params(id)
-                .condition(true)
-                .build();
+    private BooleanExpression restaurantIdNotEqual(Long id) {
+        return restaurant.id.ne(id);
     }
 
-    //FIXME
     @RequiredArgsConstructor
     public enum RestaurantSortType {
 
