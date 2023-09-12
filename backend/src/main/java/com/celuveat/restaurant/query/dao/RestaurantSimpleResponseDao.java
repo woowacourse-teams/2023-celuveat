b267441b8da0230f6fdc18e81e235af08dc2a9f4
Donghun Shin <huipulci1@naver.com>
2023-09-12 14:56:13 +0900

    refactor: db 쿼리 개선 (#450)
    * chore: 서브모듈 업데이트 (#440)
    
    * refactor: 주변 음식점 조회 API 쿼리 개선 (#440)
    
    * refactor: Facade 계층 제거 (#447)
    
    * refactor: Polishing
    
    ---------
    
    Co-authored-by: kdkdhoho <hkim4410@naver.com>

--- backend/src/main/java/com/celuveat/restaurant/query/dao/RestaurantSimpleResponseDao.java
+++ backend/src/main/java/com/celuveat/restaurant/query/dao/RestaurantSimpleResponseDao.java
@@ -2,27 +2,20 @@ package com.celuveat.restaurant.query.dao;
 
 import static com.celuveat.common.util.StreamUtil.groupBySameOrder;
 import static java.util.stream.Collectors.toMap;
-import static java.util.stream.Collectors.toUnmodifiableSet;
 
 import com.celuveat.celeb.command.domain.Celeb;
-import com.celuveat.restaurant.command.domain.Restaurant;
 import com.celuveat.restaurant.command.domain.RestaurantImage;
-import com.celuveat.restaurant.command.domain.RestaurantLike;
 import com.celuveat.restaurant.query.dao.RestaurantWithDistanceDao.LocationSearchCond;
 import com.celuveat.restaurant.query.dao.RestaurantWithDistanceDao.RestaurantSearchCond;
 import com.celuveat.restaurant.query.dao.support.RestaurantImageQueryDaoSupport;
-import com.celuveat.restaurant.query.dao.support.RestaurantLikeQueryDaoSupport;
-import com.celuveat.restaurant.query.dto.RestaurantIdWithLikeCount;
 import com.celuveat.restaurant.query.dto.RestaurantSimpleResponse;
 import com.celuveat.restaurant.query.dto.RestaurantWithDistance;
 import com.celuveat.video.command.domain.Video;
 import com.celuveat.video.query.dao.VideoQueryDaoSupport;
 import io.micrometer.common.lang.Nullable;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.Set;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -35,7 +28,6 @@ import org.springframework.transaction.annotation.Transactional;
 public class RestaurantSimpleResponseDao {
 
     private final RestaurantWithDistanceDao restaurantWithDistanceDao;
-    private final RestaurantLikeQueryDaoSupport restaurantLikeQueryDaoSupport;
     private final VideoQueryDaoSupport videoQueryDaoSupport;
     private final RestaurantImageQueryDaoSupport restaurantImageQueryDaoSupport;
 
@@ -46,8 +38,8 @@ public class RestaurantSimpleResponseDao {
             @Nullable Long memberId
     ) {
         Page<RestaurantWithDistance> restaurants =
-                restaurantWithDistanceDao.search(restaurantCond, locationCond, pageable);
-        return toSimpleResponse(restaurants, memberId);
+                restaurantWithDistanceDao.search(restaurantCond, locationCond, memberId, pageable);
+        return toSimpleResponse(restaurants);
     }
 
     public Page<RestaurantSimpleResponse> findAllNearByDistanceWithoutSpecificRestaurant(
@@ -57,22 +49,18 @@ public class RestaurantSimpleResponseDao {
             Pageable pageable
     ) {
         Page<RestaurantWithDistance> restaurants =
-                restaurantWithDistanceDao.searchNearBy(restaurantId, distance, pageable);
-        return toSimpleResponse(restaurants, memberId);
+                restaurantWithDistanceDao.searchNearBy(restaurantId, distance, memberId, pageable);
+        return toSimpleResponse(restaurants);
     }
 
     private Page<RestaurantSimpleResponse> toSimpleResponse(
-            Page<RestaurantWithDistance> restaurants,
-            @Nullable Long memberId
+            Page<RestaurantWithDistance> restaurants
     ) {
         List<Long> restaurantIds = restaurants.map(RestaurantWithDistance::id).toList();
         Map<Long, List<Celeb>> celebsMap = getCelebsGroupByRestaurantsId(restaurantIds);
         Map<Long, List<RestaurantImage>> restaurantMap = getImagesGroupByRestaurantsId(restaurantIds);
-        Set<Long> likedRestaurantIds = getRestaurantLikes(memberId);
-        Map<Long, Long> likeCountsGroupById = likeCountGroupByRestaurantId(restaurantIds);
         return RestaurantSimpleResponse.of(
-                restaurants, celebsMap, restaurantMap,
-                likedRestaurantIds, likeCountsGroupById
+                restaurants, celebsMap, restaurantMap
         );
     }
 
@@ -88,35 +76,15 @@ public class RestaurantSimpleResponseDao {
                 ));
     }
 
-    private List<Celeb> videosToCelebs(Entry<Long, List<Video>> idWithVideosEntry) {
-        return idWithVideosEntry.getValue()
-                .stream()
-                .map(Video::celeb)
-                .toList();
-    }
-
     private Map<Long, List<RestaurantImage>> getImagesGroupByRestaurantsId(List<Long> restaurantIds) {
         List<RestaurantImage> images = restaurantImageQueryDaoSupport.findAllByRestaurantIdIn(restaurantIds);
         return groupBySameOrder(images, image -> image.restaurant().id());
     }
 
-    private Set<Long> getRestaurantLikes(@Nullable Long memberId) {
-        if (memberId == null) {
-            return Collections.emptySet();
-        }
-        return restaurantLikeQueryDaoSupport.findAllByMemberId(memberId)
+    private List<Celeb> videosToCelebs(Entry<Long, List<Video>> idWithVideosEntry) {
+        return idWithVideosEntry.getValue()
                 .stream()
-                .map(RestaurantLike::restaurant)
-                .map(Restaurant::id)
-                .collect(toUnmodifiableSet());
-    }
-
-    private Map<Long, Long> likeCountGroupByRestaurantId(List<Long> restaurantIds) {
-        return restaurantLikeQueryDaoSupport.likeCountGroupByRestaurantsId(restaurantIds)
-                .stream()
-                .collect(toMap(
-                        RestaurantIdWithLikeCount::restaurantId,
-                        RestaurantIdWithLikeCount::count
-                ));
+                .map(Video::celeb)
+                .toList();
     }
 }
