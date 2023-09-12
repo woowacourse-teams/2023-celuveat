37c30f69e43f034d64dccd876648522c84049e78
kdkdhoho <hkim4410@naver.com>
2023-09-12 16:21:46 +0900

    feat: 데이터 넣기 API 수정 (#452)

--- backend/src/main/java/com/celuveat/admin/exception/AdminExceptionType.java
+++ backend/src/main/java/com/celuveat/admin/exception/AdminExceptionType.java
@@ -16,6 +16,7 @@ public enum AdminExceptionType implements BaseExceptionType {
     INVALID_YOUTUBE_CHANNEL_NAME(BAD_REQUEST, "유튜브 채널 명이 잘못됐습니다. 앞에 '@'를 붙여주세요."),
     INVALID_URL_PATTERN(BAD_REQUEST, "URL 패턴이 잘못됐습니다. 확인 후 다시 입력해주세요."),
     MISMATCH_COUNT_YOUTUBE_VIDEO_LINK_AND_UPLOAD_DATE(BAD_REQUEST, "유튜브 영상 링크와 업로드 일에 입력된 값의 개수가 다릅니다."),
+    MISMATCH_COUNT_IMAGE_NAME_AND_INSTAGRAM_NAME(BAD_REQUEST, "사진 이름의 수와 출처가 1:1 매핑이 아닙니다."),
     ;
 
     private final HttpStatus httpStatus;
