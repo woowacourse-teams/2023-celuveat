26a5fbbe4b56e4b993254865bc6ec190d263b033
Jeremy <102432453+shackstack@users.noreply.github.com>
2023-09-12 10:49:13 +0900

    feat: 워터마크 클릭시 해당 출처로 새 창을 통해 이동 (#444)

--- frontend/src/components/@common/ImageCarousel/ImageCarousel.tsx
+++ frontend/src/components/@common/ImageCarousel/ImageCarousel.tsx
@@ -46,8 +46,8 @@ function ImageCarousel({ images, type }: ImageCarouselProps) {
   return (
     <StyledCarouselContainer type={type}>
       <StyledCarouselSlide ref={ref} isMobile={isMobile}>
-        {images.map(({ id, name, author }) => (
-          <WaterMarkImage key={id} imageUrl={name} waterMark={author} type={type} isMobile={isMobile} />
+        {images.map(({ id, name, author, sns }) => (
+          <WaterMarkImage key={id} imageUrl={name} waterMark={author} type={type} sns={sns} />
         ))}
       </StyledCarouselSlide>
       {currentIndex !== 0 && !isMobile && (
