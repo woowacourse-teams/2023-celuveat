f2c741dda02659f77ea7a4b555d8f49c3d713aba
odo27 <odo27@naver.com>
2023-09-15 17:35:32 +0900

    hotfix: 프론트 개발용 로그인 쿠키 설정 변경

--- backend/src/main/java/com/celuveat/auth/presentation/LocalAuthController.java
+++ backend/src/main/java/com/celuveat/auth/presentation/LocalAuthController.java
@@ -6,7 +6,9 @@ import static com.celuveat.common.auth.AuthConstant.JSESSION_ID;
 import com.celuveat.auth.command.domain.OauthId;
 import com.celuveat.auth.command.domain.OauthMember;
 import com.celuveat.auth.command.domain.OauthMemberRepository;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Profile;
@@ -23,13 +25,22 @@ public class LocalAuthController {
     private final OauthMemberRepository oauthMemberRepository;
 
     @GetMapping("/login/local")
-    ResponseEntity<Void> login(@RequestParam String id, HttpServletRequest request) {
+    ResponseEntity<Void> login(
+            @RequestParam String id,
+            HttpServletRequest request,
+            HttpServletResponse response) {
         OauthId oauthId = new OauthId(id, KAKAO);
         OauthMember oauthMember = new OauthMember(oauthId, id, null);
         OauthMember savedMember = oauthMemberRepository.findByOauthId(oauthId)
                 .orElseGet(() -> oauthMemberRepository.save(oauthMember));
         HttpSession session = request.getSession(true);
         session.setAttribute(JSESSION_ID, savedMember.id());
+        Cookie cookie = new Cookie(JSESSION_ID, session.getId());
+        cookie.setPath("/");
+        cookie.setHttpOnly(false);
+        cookie.setSecure(false);
+        cookie.setMaxAge(60 * 60 * 24 * 30);
+        response.addCookie(cookie);
         return ResponseEntity.ok().build();
     }
 }
