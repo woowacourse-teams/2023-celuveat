55406c57cd3b7d29be625320beab28f5d11bb8d7
Minjae Kim <smallkdb@gmail.com>
2023-09-19 15:29:50 +0900

    refactor: 모바일페이지 컴포넌트 분리 (#477)
    * feat: 데이터 추가 mock api 연결
    
    * refactor: 인증 필요 없는 기능 client 변경
    
    * feat: mse 동영상, 리뷰 기능 구현 (#472)
    
    * refactor: api 훅 함수로 분리, 적용 (#472)
    
    * refactor: 쓰지 않는 파일 삭제 (#472)
    
    * fix: console log 삭제 (#472)
    
    * Squashed commit of the following:
    
    commit 6e0068735b155b95ec45de7fd78eb241993dc513
    Author: Jeremy <102432453+shackstack@users.noreply.github.com>
    Date:   Fri Sep 15 17:18:33 2023 +0900
    
        bug: 옵션관련 ux 개선 (#474)
    
        * fix: 검색창 옵션 수정 (#473)
    
        * fix: 지도 줌 limit 설정 (#473)
    
        * fix: 모바일 환경에서 거리순으로 설정 (#473)
    
        * design: 오버레이 마커 디자인 개선 (#473)
    
    * fix: 오류 수정 (#476)
    
    * refactor: useScrollBlock 보강 (#476)
    
    * refactor:  모바일 페이지 컴포넌트 분리 (#476)
    
    * refactor:  라우터 재설정 (#476)
    
    * fix: 잘못된 import 수정 (#476)
    
    * refactor: router 및 outlet 새로 설정 (#476)
    
    ---------
    
    Co-authored-by: Jeremy <102432453+shackstack@users.noreply.github.com>

--- frontend/src/components/@common/Header/index.tsx
+++ frontend/src/components/@common/Header/index.tsx
@@ -1,3 +1,4 @@
 import Header from './Header';
+import MobileHeader from './MobileHeader';
 
-export default Header;
+export { Header, MobileHeader };
--- frontend/src/hooks/useScrollBlock.ts
+++ frontend/src/hooks/useScrollBlock.ts
@@ -1,13 +1,19 @@
 import { useEffect } from 'react';
 
-const useScrollBlock = (refs: React.MutableRefObject<Element>[]) => {
+type Ref = React.MutableRefObject<Element>;
+
+const useScrollBlock = (refs: Ref[] | Ref) => {
   const blockScroll = (ref: React.MutableRefObject<Element>) => {
     ref.current.addEventListener('touchmove', e => e.preventDefault(), { passive: false });
   };
 
   useEffect(() => {
-    refs.forEach(ref => blockScroll(ref));
-  }, []);
+    if (Array.isArray(refs)) {
+      refs.forEach(ref => blockScroll(ref));
+    } else {
+      blockScroll(refs);
+    }
+  }, [refs]);
 };
 
 export default useScrollBlock;
