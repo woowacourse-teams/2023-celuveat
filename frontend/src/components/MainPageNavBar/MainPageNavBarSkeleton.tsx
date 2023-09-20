bc8e75801c82e7d884c6546b1db5c440e7fd61f9
d0dam <smallkdb@gmail.com>
2023-09-20 13:38:29 +0900

    feat: NavBar 관련 Skeleton 제작 (#480)

---
+++ frontend/src/components/MainPageNavBar/MainPageNavBarSkeleton.tsx
@@ -0,0 +1,53 @@
+import styled from 'styled-components';
+import { NavItemSkeleton } from '../@common/NavItem';
+
+function MainPageNavBarSkeleton() {
+  return (
+    <StyledNavBar>
+      <NavItemSkeleton />
+      <StyledLine />
+      <div>
+        {Array.from({ length: 11 }, () => (
+          <NavItemSkeleton />
+        ))}
+      </div>
+    </StyledNavBar>
+  );
+}
+
+export default MainPageNavBarSkeleton;
+
+const StyledNavBar = styled.nav`
+  display: flex;
+  align-items: center;
+
+  position: sticky;
+  top: 80px;
+  z-index: 11;
+
+  width: 100%;
+  height: 80px;
+
+  background-color: var(--white);
+
+  padding-left: 1.2rem;
+  border-bottom: 1px solid var(--gray-1);
+
+  & > div:last-child {
+    display: flex;
+    align-items: center;
+
+    & > * {
+      margin: 0 0.6rem;
+    }
+  }
+`;
+
+const StyledLine = styled.div`
+  margin-left: 1.2rem;
+
+  width: 1px;
+  height: 46px;
+
+  background-color: var(--gray-3);
+`;
