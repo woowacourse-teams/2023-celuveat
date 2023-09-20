ac40d18c9959fb488c8bded376ff554429bbf712
황준승 <78203399+turtle601@users.noreply.github.com>
2023-09-20 16:01:35 +0900

    [✨feat] 폰트 + 이미지 최적화 (#491)
    * feat: 사용하지 않는 .png 파일 제거 (#478)
    
    * refactor: Webpack 파일 리팩토링 (#478)
    
    * refactor: 불필요한 코드 구문 제거 (#478)
    
    타입스크립트 컴파일은 ts loader로만으로도 충분
    
    * refactor: devDependency vs Dependency 라이브러리 분리 (#478)
    
    * refactor: 불필요한 코드 삭제 (#478)
    
    Changed:
    불필요한 optimize 구문 삭제'
    
    * feat: picture 태그를 활용한 이미지 최적화 (#478)
    
    * refactor: 불필요한 react.memo 제거 (#478)
    
    * feat: 이미지 url 관련 유틸함수 구현 (#478)
    
    * feat: 폰트 리로드 적용 (#478)
    
    * feat: video lazy로딩 적용 (#478)
    
    * fix: restaurants mock 데이터 수정(#478)
    Changed:
    .jpeg 삭제
    
    * refactor: 불필요한 폰트 리로딩, lazy loading 제거 (#478)
    
    * feat: 폰트 preload 최적화 (#487)
    
    * refactor: devDependency 허용하는 eslint 설정 추가 (#487)
    
    * fix: webp이미지 불러오지 못하는 오류 해결 (#487)
    
    * refactor: 불필요한 코드 삭제 (#478)
    
    Changed:
    ${} 삭제
    
    * refactor: url 전역 변수 선언 (#487)
    
    * refactor: React.MouseEvent 코드 컨벤션 수정 (#487)
    
    * fix: 이미지 .. 붙여지는 오류 해결 (#487)
    
    * fix: react-query devtools 디펜던시 lint 에러 해결 (#487)

---
+++ frontend/src/constants/url.ts
@@ -0,0 +1,1 @@
+export const SERVER_IMG_URL = 'https://www.celuveat.com/images-data/';
--- frontend/src/styles/GlobalStyles.ts
+++ frontend/src/styles/GlobalStyles.ts
@@ -1,11 +1,9 @@
 import { createGlobalStyle } from 'styled-components';
 import reset from './reset';
 import base from './base';
-import fonts from './fonts';
 
 const GlobalStyles = createGlobalStyle`
   ${reset}
-  ${fonts}
   ${base}
 `;
 
