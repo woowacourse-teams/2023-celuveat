8e379640f13e8779257c19049ef541ccaf44e2f3
turtle601 <turtle601@naver.com>
2023-09-28 12:20:00 +0900

    feat: 리뷰 좋아요 및 신고하기 관련 기능 훅 구현(#528)

--- frontend/src/hooks/server/useRestaurantReview.ts
+++ frontend/src/hooks/server/useRestaurantReview.ts
@@ -1,22 +1,25 @@
 import { AxiosError } from 'axios';
-import { useQuery, useMutation } from '@tanstack/react-query';
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 import { useParams } from 'react-router-dom';
 import { shallow } from 'zustand/shallow';
 
-import useToastState from '~/hooks/store/useToastState';
-
-import type { RestaurantReviewData, RestaurantReviewPatchBody, RestaurantReviewPostBody } from '../../@types/api.types';
-import { apiClient } from '~/api/apiClient';
 import {
   deleteRestaurantReview,
   getRestaurantReview,
   patchRestaurantReview,
   postRestaurantReview,
+  postRestaurantReviewLike,
+  postRestaurantReviewReport,
 } from '~/api/restaurantReview';
 
+import useToastState from '~/hooks/store/useToastState';
+
+import type { RestaurantReviewData, RestaurantReviewPatchBody, RestaurantReviewPostBody } from '../../@types/api.types';
+
 const useRestaurantReview = () => {
+  const queryClient = useQueryClient();
   const { id: restaurantId } = useParams();
-  const { onFailure } = useToastState(
+  const { onFailure, onSuccess: successReviewLike } = useToastState(
     state => ({
       onFailure: state.onFailure,
       onSuccess: state.onSuccess,
@@ -36,7 +39,7 @@ const useRestaurantReview = () => {
   };
 
   const { data: restaurantReviewsData, isLoading } = useQuery<RestaurantReviewData>({
-    queryKey: ['restaurantReview', restaurantId, apiClient],
+    queryKey: ['restaurantReview', restaurantId],
     queryFn: () => getRestaurantReview(restaurantId),
     suspense: true,
   });
@@ -57,12 +60,57 @@ const useRestaurantReview = () => {
     onError: errorHandler,
   });
 
+  const postReviewLike = useMutation({
+    mutationFn: postRestaurantReviewLike,
+    onMutate: async reviewId => {
+      await queryClient.cancelQueries(['restaurantReview']);
+      const previousReviews: RestaurantReviewData = queryClient.getQueryData(['restaurantReview', restaurantId]);
+
+      queryClient.setQueryData(['restaurantReview', restaurantId], (oldReviewsQueryData: RestaurantReviewData) => {
+        const newReviewListData = oldReviewsQueryData?.reviews.map(reviewItem =>
+          reviewItem.id === reviewId ? { ...reviewItem, isLiked: !reviewItem.isLiked } : reviewItem,
+        );
+
+        return { ...oldReviewsQueryData, reviews: newReviewListData };
+      });
+
+      return { previousReviews };
+    },
+
+    onSuccess: () => {
+      successReviewLike('해당 리뷰를 추천하였습니다!!');
+    },
+
+    onError: (error: AxiosError, reviewId, context) => {
+      queryClient.setQueryData(['restaurantReview', restaurantId], context.previousReviews);
+      errorHandler(error);
+    },
+
+    onSettled: () => {
+      queryClient.invalidateQueries(['restaurantReview', restaurantId]);
+    },
+  });
+
+  const postReviewReport = useMutation({
+    mutationFn: postRestaurantReviewReport,
+    onError: errorHandler,
+  });
+
+  const getReviewIsLiked = (reviewId: number) => {
+    const review = restaurantReviewsData.reviews.find(reviewItem => reviewItem.id === reviewId);
+
+    return review ? review.isLiked : null;
+  };
+
   return {
-    restaurantReviewsData,
     isLoading,
+    restaurantReviewsData,
+    getReviewIsLiked,
     createReview: createReview.mutate,
     updateReview: updateReview.mutate,
     deleteReview: deleteReview.mutate,
+    postReviewLike: postReviewLike.mutate,
+    postReviewReport: postReviewReport.mutate,
   };
 };
 
