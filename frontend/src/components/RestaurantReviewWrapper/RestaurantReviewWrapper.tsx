1028c19d633649e98ca3f51f0f607ee966139091
turtle601 <turtle601@naver.com>
2023-09-28 17:42:50 +0900

    refactor: REVIEW_FORM_TITLE 변수 생성 및 코드 수정 (#528)

--- frontend/src/components/RestaurantReviewWrapper/RestaurantReviewWrapper.tsx
+++ frontend/src/components/RestaurantReviewWrapper/RestaurantReviewWrapper.tsx
@@ -1,38 +1,35 @@
-import { useMemo } from 'react';
 import { styled } from 'styled-components';
 
 import Alert from '~/assets/icons/alert.svg';
-
-import RestaurantReviewList from '~/components/RestaurantReviewList';
-import Modal from '~/components/@common/Modal';
-import ReviewForm from '../ReviewForm/ReviewForm';
 import Pencil from '~/assets/icons/pencil.svg';
+
+import Modal from '~/components/@common/Modal';
 
 import useRestaurantReview from '~/hooks/server/useRestaurantReview';
 import { useReviewModalContext } from '~/hooks/context/ReviewModalProvider';
 import { FONT_SIZE } from '~/styles/common';
-import { isMoreThan } from '~/utils/compare';
+
 import { REVIEW_SHOW_COUNT } from '~/constants/options';
-import DeleteButton from '../ReviewForm/DeleteButton';
-import LoginModal from '../LoginModal';
+
+import RestaurantReviewList from '~/components/RestaurantReviewList';
+import LoginModal from '~/components/LoginModal';
+import { ReviewForm, DeleteButton } from '~/components/ReviewForm';
+
+const REVIEW_FORM_TITLE = {
+  create: '리뷰 작성하기',
+  update: '리뷰 수정하기',
+  delete: '리뷰 삭제하기',
+  report: '리뷰 신고하기',
+  all: '리뷰 모두 보기',
+} as const;
 
 function RestaurantReviewWrapper() {
   const { restaurantReviewsData } = useRestaurantReview();
-  const { formType, isModalOpen, closeModal, openCreateReview, openShowAll, reviewId } = useReviewModalContext();
-  const { totalElementsCount: reviewCount, reviews } = restaurantReviewsData;
+  const { formType, isModalOpen, reviewId, closeModal, openReviewModal } = useReviewModalContext();
 
-  const previewReviews = useMemo(() => reviews.slice(0, REVIEW_SHOW_COUNT), [reviews]);
-  const isMoreReviews = isMoreThan(reviewCount, REVIEW_SHOW_COUNT);
-
-  const getTitle = (type: string) => {
-    if (type === 'create') return '리뷰 작성하기';
-    if (type === 'update') return '리뷰 수정하기';
-    if (type === 'delete') return '리뷰 삭제하기';
-    if (type === 'report') return '리뷰 신고하기';
-    if (type === 'all') return '리뷰 모두 보기';
-    return null;
-  };
   const { totalElementsCount: reviewCount, reviews } = restaurantReviewsData;
+  const previewReviews = reviews.slice(0, REVIEW_SHOW_COUNT);
+  const isMoreReviews = reviewCount > REVIEW_SHOW_COUNT;
 
   return (
     <>
@@ -40,18 +37,21 @@ function RestaurantReviewWrapper() {
         <StyledReviewCountText>리뷰 {reviewCount ? `${reviewCount}개` : '없음'}</StyledReviewCountText>
         <RestaurantReviewList reviews={previewReviews} />
         <StyledButtonContainer>
-          <StyledButton type="button" onClick={openCreateReview}>
+          <StyledButton type="button" onClick={() => openReviewModal('create')}>
             <Pencil width={20} />
             리뷰 작성하기
           </StyledButton>
           {isMoreReviews && (
-            <StyledButton type="button" onClick={openShowAll}>{`리뷰 ${reviewCount}개 모두 보기`}</StyledButton>
+            <StyledButton
+              type="button"
+              onClick={() => openReviewModal('all')}
+            >{`리뷰 ${reviewCount}개 모두 보기`}</StyledButton>
           )}
         </StyledButtonContainer>
       </StyledRestaurantReviewWrapper>
 
       {formType ? (
-        <Modal title={getTitle(formType)} isOpen={isModalOpen} close={closeModal}>
+        <Modal title={REVIEW_FORM_TITLE[formType]} isOpen={isModalOpen} close={closeModal}>
           <>
             {formType === 'create' && <ReviewForm type="create" />}
             {formType === 'report' && <ReviewForm type="report" reviewId={reviewId} />}
