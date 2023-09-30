d629f26279a8ff669025e1ac9350f190f69bcad4
Jeremy <102432453+shackstack@users.noreply.github.com>
2023-09-30 16:55:20 +0900

    refactor: 카테고리 네브바 스토리북 리팩터링 (#540)

--- frontend/src/components/CategoryNavbar/CategoryNavbar.stories.tsx
+++ frontend/src/components/CategoryNavbar/CategoryNavbar.stories.tsx
@@ -1,55 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import CategoryNavbar from './CategoryNavbar';
-import Izakaya from '~/assets/icons/restaurantCategory/izakaya.svg';
-import { RestaurantCategory } from '~/@types/restaurant.types';
-
-interface Category {
-  label: RestaurantCategory;
-  icon: React.ReactNode;
-}
-
-const categories: Category[] = [
-  {
-    label: '일식당',
-    icon: <Izakaya />,
-  },
-  {
-    label: '한식',
-    icon: <Izakaya />,
-  },
-  {
-    label: '초밥,롤',
-    icon: <Izakaya />,
-  },
-  {
-    label: '생선회',
-    icon: <Izakaya />,
-  },
-  {
-    label: '양식',
-    icon: <Izakaya />,
-  },
-  {
-    label: '육류,고기요리',
-    icon: <Izakaya />,
-  },
-  {
-    label: '이자카야',
-    icon: <Izakaya />,
-  },
-  {
-    label: '돼지고기구이',
-    icon: <Izakaya />,
-  },
-  {
-    label: '요리주점',
-    icon: <Izakaya />,
-  },
-  {
-    label: '와인',
-    icon: <Izakaya />,
-  },
-];
+import RESTAURANT_CATEGORY from '~/constants/restaurantCategory';
 
 const meta: Meta<typeof CategoryNavbar> = {
   title: 'Selector/CategoryNavbar',
@@ -62,6 +13,6 @@ type Story = StoryObj<typeof CategoryNavbar>;
 
 export const Default: Story = {
   args: {
-    categories,
+    categories: RESTAURANT_CATEGORY,
   },
 };
