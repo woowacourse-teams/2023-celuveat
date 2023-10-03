9e487c43401da1273f2a1cba3f44d785b3fc67d6
shin-mallang <huipulci1@naver.com>
2023-10-04 00:53:44 +0900

    feat: 행정구역에 대한 엔티티 추가 (#535)

---
+++ backend/src/main/java/com/celuveat/administrativedistrict/domain/AdministrativeDistrict.java
@@ -0,0 +1,33 @@
+package com.celuveat.administrativedistrict.domain;
+
+import static lombok.AccessLevel.PROTECTED;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import org.locationtech.jts.geom.Polygon;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = PROTECTED)
+public class AdministrativeDistrict {
+
+    @Id
+    private Long id;
+
+    @Column(nullable = false)
+    private Polygon polygon;
+
+    @Column(unique = true, nullable = false)
+    private String code;
+
+    @Column(nullable = false)
+    private String englishName;
+
+    @Column(nullable = false)
+    private String koreanName;
+}
