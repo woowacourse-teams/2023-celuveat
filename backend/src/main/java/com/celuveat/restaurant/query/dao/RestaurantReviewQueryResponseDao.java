d8e54fcc7f5e88e257af87dbaa8a41118ed9102d
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2023-10-05 13:28:57 +0900

    refactor: Dao에서만 사용 되는 support로 분리 (#552)

--- backend/src/main/java/com/celuveat/restaurant/query/dao/RestaurantReviewQueryResponseDao.java
+++ backend/src/main/java/com/celuveat/restaurant/query/dao/RestaurantReviewQueryResponseDao.java
@@ -1,9 +1,14 @@
 package com.celuveat.restaurant.query.dao;
 
+import static com.celuveat.common.util.StreamUtil.groupBySameOrder;
+
 import com.celuveat.restaurant.command.domain.review.RestaurantReview;
-import com.celuveat.restaurant.command.domain.review.RestaurantReviewRepository;
+import com.celuveat.restaurant.command.domain.review.RestaurantReviewImage;
+import com.celuveat.restaurant.query.dao.support.RestaurantReviewImageQueryDaoSupport;
+import com.celuveat.restaurant.query.dao.support.RestaurantReviewQueryDaoSupport;
 import com.celuveat.restaurant.query.dto.RestaurantReviewQueryResponse;
 import java.util.List;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,11 +18,19 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 public class RestaurantReviewQueryResponseDao {
 
-    private final RestaurantReviewRepository restaurantReviewRepository;
+    private final RestaurantReviewQueryDaoSupport restaurantReviewQueryDaoSupport;
+    private final RestaurantReviewImageQueryDaoSupport restaurantReviewImageQueryDaoSupport;
 
     public RestaurantReviewQueryResponse findAllByRestaurantId(Long restaurantId) {
         List<RestaurantReview> reviews =
-                restaurantReviewRepository.findAllByRestaurantIdOrderByCreatedDateDesc(restaurantId);
-        return RestaurantReviewQueryResponse.from(reviews);
+                restaurantReviewQueryDaoSupport.findAllByRestaurantIdOrderByCreatedDateDesc(restaurantId);
+        Map<Long, List<RestaurantReviewImage>> imagesGroupByReviewId = getImagesGroupByReviewId(reviews);
+        return RestaurantReviewQueryResponse.from(reviews, imagesGroupByReviewId);
+    }
+
+    private Map<Long, List<RestaurantReviewImage>> getImagesGroupByReviewId(List<RestaurantReview> reviews) {
+        List<RestaurantReviewImage> restaurantReviewImages
+                = restaurantReviewImageQueryDaoSupport.findAllByRestaurantReviewIn(reviews);
+        return groupBySameOrder(restaurantReviewImages, image -> image.restaurantReview().id());
     }
 }
