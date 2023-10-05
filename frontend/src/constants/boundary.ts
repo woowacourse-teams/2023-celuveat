24f0efc9441ff9f44be93373e887e12164306cd4
Jeremy <102432453+shackstack@users.noreply.github.com>
2023-10-05 14:11:29 +0900

    feat: 전국 바운더리 상수화(#540)

---
+++ frontend/src/constants/boundary.ts
@@ -0,0 +1,8 @@
+import { CoordinateBoundary } from '~/@types/map.types';
+
+export const WHOLE_BOUNDARY: CoordinateBoundary = {
+  lowLatitude: '32',
+  highLatitude: '40',
+  lowLongitude: '120',
+  highLongitude: '132',
+};
