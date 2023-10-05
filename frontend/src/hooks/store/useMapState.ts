e27dd9baeb525595c83999abe512bb2215782d14
Jeremy <102432453+shackstack@users.noreply.github.com>
2023-10-06 00:23:47 +0900

    feat: 모바일 지도페이지에서 마커 클릭 시 미리보기 구현 (#557)

--- frontend/src/hooks/store/useMapState.ts
+++ frontend/src/hooks/store/useMapState.ts
@@ -1,14 +1,17 @@
 import { create } from 'zustand';
+import type { RestaurantData } from '~/@types/api.types';
 import type { Coordinate } from '~/@types/map.types';
 
 interface MapState {
   center: Coordinate;
   zoom: number;
+  preview: RestaurantData | null;
 }
 
 interface MapAction {
   setCenter: (newCenter: Coordinate) => void;
   setZoom: (newZoom: number) => void;
+  setPreview: (newRestaurant: RestaurantData) => void;
 }
 
 const JamsilCampus = { lat: 37.515271, lng: 127.1029949 };
@@ -16,9 +19,11 @@ const JamsilCampus = { lat: 37.515271, lng: 127.1029949 };
 const useMapState = create<MapState & MapAction>(set => ({
   center: JamsilCampus,
   zoom: 13,
+  preview: null,
 
   setCenter: newCenter => set(() => ({ center: newCenter })),
   setZoom: newZoom => set(() => ({ zoom: newZoom })),
+  setPreview: newRestaurant => set(() => ({ preview: newRestaurant })),
 }));
 
 export default useMapState;
--- frontend/src/pages/MobileMapPage.tsx
+++ frontend/src/pages/MobileMapPage.tsx
@@ -11,59 +11,66 @@ import ListIcon from '~/assets/icons/list.svg';
 import RestaurantCardList from '~/components/RestaurantCardList';
 import Footer from '~/components/@common/Footer';
 import Map from '~/components/@common/Map';
+import MiniRestaurantCard from '~/components/MiniRestaurantCard';
+import useMapState from '~/hooks/store/useMapState';
 
 function MobileMapPage() {
-  const ref = useRef();
-  const scrollDirection = useScrollDirection();
-  const { isEnd } = useScrollEnd({ direction: 'Y', threshold: 200 });
-  const { value: isListShowed, toggle: toggleShowedList } = useBooleanState(false);
+  // const ref = useRef();
+  // const scrollDirection = useScrollDirection();
+  // const { isEnd } = useScrollEnd({ direction: 'Y', threshold: 200 });
+  const { value: isListShowed } = useBooleanState(false);
+  const [preview] = useMapState(state => [state.preview]);
 
-  useScrollBlock(ref);
+  // useScrollBlock(ref);
+
+  const getPreview = () => {
+    const { celebs, ...restaurant } = preview;
+
+    return (
+      <StyledPreview>
+        <MiniRestaurantCard restaurant={restaurant} celebs={celebs} showWaterMark={false} />
+      </StyledPreview>
+    );
+  };
 
   return (
-    <StyledMobileMapPageContainer>
-      {isListShowed ? (
-        <StyledToggleButton
-          type="button"
-          onClick={toggleShowedList}
-          isHide={isEnd}
-          isNavBarHide={isListShowed && scrollDirection.y === 'down'}
-          ref={ref}
-        >
-          <span>지도</span>
-          <MapIcon width={24} />
-        </StyledToggleButton>
-      ) : (
-        <StyledToggleButton type="button" onClick={toggleShowedList} isHide={false} isNavBarHide={false} ref={ref}>
-          <span>리스트</span>
-          <ListIcon width={20} stroke="#fff" />
-        </StyledToggleButton>
-      )}
-
-      <StyledMobileLayout isListShowed={isListShowed}>
-        <div>
-          <RestaurantCardList />
-          <Footer />
-        </div>
-        <Map />
-      </StyledMobileLayout>
-    </StyledMobileMapPageContainer>
+    <StyledMobileLayout isListShowed={isListShowed}>
+      <div>
+        <RestaurantCardList />
+        <Footer />
+      </div>
+      <Map />
+      <StyledPreviewContainer>
+        {/* {isListShowed ? (
+            <StyledToggleButton
+              type="button"
+              onClick={toggleShowedList}
+              isHide={isEnd}
+              isNavBarHide={isListShowed && scrollDirection.y === 'down'}
+              ref={ref}
+            >
+              <span>지도</span>
+              <MapIcon width={24} />
+            </StyledToggleButton>
+          ) : (
+            <StyledToggleButton type="button" onClick={toggleShowedList} isHide={false} isNavBarHide={false} ref={ref}>
+              <span>리스트</span>
+              <ListIcon width={20} stroke="#fff" />
+            </StyledToggleButton>
+          )} */}
+        {preview && getPreview()}
+      </StyledPreviewContainer>
+    </StyledMobileLayout>
   );
 }
 
 export default MobileMapPage;
 
-const StyledMobileMapPageContainer = styled.div`
-  width: 100%;
-  height: 100vh;
-`;
-
 const StyledMobileLayout = styled.main<{ isListShowed: boolean }>`
-  position: absolute;
-  top: 88px;
+  position: relative;
 
-  width: 100vw;
-  height: calc(100vh - 88px - 48px);
+  width: 100%;
+  height: 100%;
 
   & > div:first-child {
     position: absolute;
@@ -126,4 +133,23 @@ const StyledToggleButton = styled.button<{ isHide: boolean; isNavBarHide: boolea
       transition: 0.4s ease-in-out;
       transform: translateY(64px);
     `}
+`;
+
+const StyledPreviewContainer = styled.div`
+  display: flex;
+  justify-content: center;
+
+  position: absolute;
+  bottom: 12px;
+
+  width: 100%;
+`;
+
+const StyledPreview = styled.div`
+  width: 90%;
+
+  padding: 1.2rem 0.8rem;
+
+  border-radius: 12px;
+  background-color: white;
 `;
