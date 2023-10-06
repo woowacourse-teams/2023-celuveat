6acae4db596f7c6e2d370573fff04cd154c84ac7
shin-mallang <huipulci1@naver.com>
2023-10-07 03:47:20 +0900

    test: Oauth 관련 서비스(OauthService) 테스트코드 개선 (#583)

--- backend/src/test/java/com/celuveat/auth/command/application/OauthServiceTest.java
+++ backend/src/test/java/com/celuveat/auth/command/application/OauthServiceTest.java
@@ -1,21 +1,18 @@
 package com.celuveat.auth.command.application;
 
 import static com.celuveat.auth.command.domain.OauthServerType.KAKAO;
-import static com.celuveat.auth.exception.AuthExceptionType.NOT_FOUND_MEMBER;
-import static com.celuveat.auth.fixture.OauthMemberFixture.멤버;
-import static com.celuveat.restaurant.fixture.RestaurantFixture.음식점;
-import static com.celuveat.restaurant.fixture.RestaurantLikeFixture.음식점_좋아요;
+import static com.celuveat.auth.fixture.OauthMemberFixture.말랑;
+import static com.celuveat.restaurant.fixture.RestaurantFixture.대성집;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import com.celuveat.auth.command.domain.OauthMember;
 import com.celuveat.auth.command.domain.client.OauthMemberClientComposite;
-import com.celuveat.auth.exception.AuthException;
 import com.celuveat.common.IntegrationTest;
-import com.celuveat.common.exception.BaseExceptionType;
 import com.celuveat.restaurant.command.domain.Restaurant;
+import com.celuveat.restaurant.command.domain.RestaurantLike;
+import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -41,31 +38,31 @@ class OauthServiceTest extends IntegrationTest {
     @Test
     void 로그인하면_회원의_아이디가_반환된다() {
         // given
-        OauthMember 도기 = oauthMemberRepository.save(멤버("도기"));
-        String authCode = "abcd";
-        when(oauthMemberClientComposite.fetch(KAKAO, authCode)).thenReturn(도기);
+        OauthMember 말랑 = oauthMemberRepository.save(말랑());
+        String authCode = "말랑 auth code";
+        when(oauthMemberClientComposite.fetch(KAKAO, authCode)).thenReturn(말랑);
 
         // when
-        Long result = oauthService.login(KAKAO, authCode);
+        Long memberId = oauthService.login(KAKAO, authCode);
 
         // then
-        assertThat(result).isEqualTo(도기.id());
+        assertThat(memberId).isEqualTo(말랑.id());
     }
 
     @Test
     void 회원_탈퇴하면_회원의_좋아요와_정보가_모두_삭제된다() {
         // given
-        Restaurant 새벽집 = restaurantRepository.save(음식점("새벽집"));
-        OauthMember 도기 = oauthMemberRepository.save(멤버("도기"));
-        restaurantLikeRepository.save(음식점_좋아요(새벽집, 도기));
+        Restaurant 대성집 = restaurantRepository.save(대성집());
+        OauthMember 말랑 = oauthMemberRepository.save(말랑());
+        restaurantLikeRepository.save(new RestaurantLike(대성집, 말랑));
 
         // when
-        oauthService.withdraw(KAKAO, 도기.id());
+        oauthService.withdraw(KAKAO, 말랑.id());
 
         // then
-        assertThat(restaurantLikeQueryDaoSupport.countByRestaurant(새벽집)).isEqualTo(0);
-        BaseExceptionType exceptionType = assertThrows(AuthException.class,
-                () -> oauthMemberRepository.getById(도기.id())).exceptionType();
-        assertThat(exceptionType).isEqualTo(NOT_FOUND_MEMBER);
+        int 대성집_좋아요_개수 = restaurantLikeQueryDaoSupport.countByRestaurant(대성집);
+        assertThat(대성집_좋아요_개수).isEqualTo(0);
+        Optional<OauthMember> 말랑_조회_결과 = oauthMemberRepository.findById(말랑.id());
+        assertThat(말랑_조회_결과).isEmpty();
     }
 }
