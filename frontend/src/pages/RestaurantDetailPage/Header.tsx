3e976237958645cb41b3ad71a4eaee7a8f48f0c3
Jeremy <102432453+shackstack@users.noreply.github.com>
2023-10-06 16:51:03 +0900

    feat: 음식점 상세페이지 공유하기 기능 구현 (#569) (#570)
    * feat: 음식점 상세페이지 공유하기 기능 구현 (#569)
    
    * fix: lint error 해결

--- frontend/src/pages/RestaurantDetailPage/Header.tsx
+++ frontend/src/pages/RestaurantDetailPage/Header.tsx
@@ -1,7 +1,9 @@
+/* eslint-disable no-useless-return */
 import styled from 'styled-components';
 import View from '~/assets/icons/view.svg';
 import Love from '~/assets/icons/black-love.svg';
 import { FONT_SIZE } from '~/styles/common';
+import ShareIcon from '~/assets/icons/share.svg';
 
 interface HeaderProps {
   name: string;
@@ -10,9 +12,35 @@ interface HeaderProps {
 }
 
 function Header({ name, viewCount, likeCount }: HeaderProps) {
+  const share = async () => {
+    if (typeof navigator.share === 'undefined') {
+      await copyClipBoard(window.location.href);
+      return;
+    }
+    try {
+      await navigator.share({ url: window.location.href });
+    } catch (e) {
+      if (e.toString().includes('AbortError')) return;
+    }
+  };
+
+  const copyClipBoard = async (text: string) => {
+    try {
+      await navigator.clipboard.writeText(text);
+      alert('링크가 복사되었어요.');
+    } catch (err) {
+      alert('클립보드에 저장하는데 문제가 생겼어요.');
+    }
+  };
+
   return (
     <StyledDetailHeader tabIndex={0}>
-      <h3>{name}</h3>
+      <StyledTitleSection>
+        <h3>{name}</h3>
+        <StyledShareButton type="button" onClick={share}>
+          <ShareIcon />
+        </StyledShareButton>
+      </StyledTitleSection>
       <div role="group">
         <div aria-label={`조회수 ${viewCount}`}>
           <View width={18} aria-hidden /> <span aria-hidden>{viewCount}</span>
@@ -47,4 +75,16 @@ const StyledDetailHeader = styled.section`
       gap: 0 0.8rem;
     }
   }
+`;
+
+const StyledShareButton = styled.button`
+  border: none;
+  background-color: transparent;
+`;
+
+const StyledTitleSection = styled.div`
+  display: flex;
+  justify-content: space-between;
+
+  width: 100%;
 `;
