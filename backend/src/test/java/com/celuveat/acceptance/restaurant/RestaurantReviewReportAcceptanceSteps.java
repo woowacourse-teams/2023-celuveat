973802a0b7bf6eda376811281c813a14fd9d9406
shin-mallang <huipulci1@naver.com>
2023-10-09 11:54:00 +0900

    test: 음식점 리뷰 신고 관련 인수테스트 재작성 (#583)

--- backend/src/test/java/com/celuveat/acceptance/restaurant/RestaurantReviewReportAcceptanceSteps.java
+++ backend/src/test/java/com/celuveat/acceptance/restaurant/RestaurantReviewReportAcceptanceSteps.java
@@ -8,11 +8,15 @@ import io.restassured.response.Response;
 
 public class RestaurantReviewReportAcceptanceSteps {
 
-    public static ExtractableResponse<Response> 신고_요청을_보낸다(String 신고내용, Long 리뷰_아이디, String 세션_아이디) {
+    public static ExtractableResponse<Response> 신고_요청을_보낸다(
+            String 세션_아이디,
+            Long 리뷰_아이디,
+            String 신고내용
+    ) {
         ReportReviewRequest reportReviewRequest = new ReportReviewRequest(신고내용);
         return given(세션_아이디)
                 .when().body(reportReviewRequest)
-                .post("/reviews/" + 리뷰_아이디 + "/report")
+                .post("/reviews/{reviewId}/report", 리뷰_아이디)
                 .then()
                 .extract();
     }
--- backend/src/test/java/com/celuveat/acceptance/restaurant/RestaurantReviewReportAcceptanceTest.java
+++ backend/src/test/java/com/celuveat/acceptance/restaurant/RestaurantReviewReportAcceptanceTest.java
@@ -1,33 +1,55 @@
 package com.celuveat.acceptance.restaurant;
 
+import static com.celuveat.acceptance.common.AcceptanceSteps.ID를_추출한다;
 import static com.celuveat.acceptance.common.AcceptanceSteps.응답_상태를_검증한다;
 import static com.celuveat.acceptance.common.AcceptanceSteps.정상_처리;
+import static com.celuveat.acceptance.restaurant.RestaurantReviewAcceptanceSteps.리뷰_요청;
+import static com.celuveat.acceptance.restaurant.RestaurantReviewAcceptanceSteps.리뷰_작성_요청을_보낸다;
 import static com.celuveat.acceptance.restaurant.RestaurantReviewReportAcceptanceSteps.신고_요청을_보낸다;
-import static com.celuveat.auth.fixture.OauthMemberFixture.멤버;
-import static com.celuveat.restaurant.fixture.RestaurantFixture.음식점;
-import static com.celuveat.restaurant.fixture.RestaurantReviewFixture.음식점_리뷰;
+import static com.celuveat.auth.fixture.OauthMemberFixture.도기;
+import static com.celuveat.auth.fixture.OauthMemberFixture.말랑;
+import static com.celuveat.celeb.fixture.CelebFixture.성시경;
+import static com.celuveat.celeb.fixture.CelebFixture.회사랑;
+import static com.celuveat.restaurant.fixture.RestaurantFixture.대성집;
+import static com.celuveat.restaurant.fixture.RestaurantFixture.하늘초밥;
 
 import com.celuveat.acceptance.common.AcceptanceTest;
+import com.celuveat.auth.command.domain.OauthMember;
+import com.celuveat.celeb.command.domain.Celeb;
+import com.celuveat.restaurant.command.domain.Restaurant;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 @DisplayName("음식점 리뷰 신고 인수테스트")
 public class RestaurantReviewReportAcceptanceTest extends AcceptanceTest {
 
+    private final OauthMember 말랑 = 말랑();
+    private final OauthMember 도기 = 도기();
+    private final Restaurant 대성집 = 대성집();
+    private final Restaurant 하늘초밥 = 하늘초밥();
+    private final Celeb 성시경 = 성시경();
+    private final Celeb 회사랑 = 회사랑();
+
+    @BeforeEach
+    protected void setUp() {
+        super.setUp();  // connection refused 에러 해결
+        testData.addMembers(말랑, 도기);
+        testData.addCelebs(성시경, 회사랑);
+        testData.addRestaurants(대성집, 하늘초밥);
+        초기_데이터_저장();
+    }
+
     @Test
     void 음식점_리뷰를_신고한다() {
         // given
-        var 맛집 = 음식점을_저장한다(음식점("맛집"));
-        var 오도 = 멤버("오도");
-        회원가입후_로그인한다(오도);
-        var 리뷰_아이디 = 음식점_리뷰를_저장한다(음식점_리뷰(오도, 맛집));
-
-        var 도기 = 멤버("도기");
-        var 세션_아이디 = 회원가입과_로그인후_세션아이디를_가져온다(도기);
-        var 신고내용 = "부적절한 댓글입니다";
+        var 말랑_세션_아이디 = 로그인후_세션아이디를_가져온다(말랑);
+        var 도기_세션_아이디 = 로그인후_세션아이디를_가져온다(도기);
+        var 요청 = 리뷰_요청("맛집이네요 또 올 것 같습니다", 대성집.id(), 5.0);
+        var 말랑_리뷰_ID = ID를_추출한다(리뷰_작성_요청을_보낸다(말랑_세션_아이디, 요청));
 
         // when
-        var 응답 = 신고_요청을_보낸다(신고내용, 리뷰_아이디, 세션_아이디);
+        var 응답 = 신고_요청을_보낸다(도기_세션_아이디, 말랑_리뷰_ID, "부적절한 댓글입니다");
 
         // then
         응답_상태를_검증한다(응답, 정상_처리);
