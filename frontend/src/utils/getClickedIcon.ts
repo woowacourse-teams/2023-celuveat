78de1002ca55b465c379765e5f40e98bfa76ad9e
Jeremy <102432453+shackstack@users.noreply.github.com>
2023-10-10 10:44:26 +0900

    refactor: 구조분해할당 적용 (#557)

--- frontend/src/utils/getClickedIcon.ts
+++ frontend/src/utils/getClickedIcon.ts
@@ -1,7 +1,7 @@
 export const getClickedIcon = (pathname: string) => {
-  const splitPathname = pathname.split('/');
-  if (splitPathname[1] === '') return 'home';
-  if (splitPathname[1] === 'map') return 'map';
-  if (splitPathname[1] === 'signup') return 'user';
+  const [, splitPathname] = pathname.split('/');
+  if (splitPathname === '') return 'home';
+  if (splitPathname === 'map') return 'map';
+  if (splitPathname === 'signup') return 'user';
   return null;
 };
