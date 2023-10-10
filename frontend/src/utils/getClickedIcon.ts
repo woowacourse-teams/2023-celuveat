4e1096d89700b6ea907f8aa5cd0619bc1c304732
Jeremy <102432453+shackstack@users.noreply.github.com>
2023-10-10 14:29:37 +0900

    refactor: getClickedIcon 유틸 함수 제거 (#594)

--- frontend/src/utils/getClickedIcon.ts
+++
@@ -1,7 +0,0 @@
-export const getClickedIcon = (pathname: string) => {
-  const [, splitPathname] = pathname.split('/');
-  if (splitPathname === '') return 'home';
-  if (splitPathname === 'map') return 'map';
-  if (splitPathname === 'signup') return 'user';
-  return null;
-};
