7bfd4857a522402b96ca2f614cc780caab5c4650
Jeremy <102432453+shackstack@users.noreply.github.com>
2023-10-12 20:59:23 +0900

    hotfix: 지역 법정동 코드 수정

--- frontend/src/constants/recommendedRegion.ts
+++ frontend/src/constants/recommendedRegion.ts
@@ -1,7 +1,11 @@
 export const RECOMMENDED_REGION = {
-  jamsil: {
-    name: ['잠실'],
-    code: [1171010100],
+  apgujeong: {
+    name: ['압구정', '청담'],
+    code: [1168011000, 1168010400],
+  },
+  eulji: {
+    name: ['을지로', '종로'],
+    code: [1111000000, 1114000000],
   },
   seongsu: {
     name: ['성수'],
@@ -11,25 +15,17 @@ export const RECOMMENDED_REGION = {
     name: ['홍대', '합정'],
     code: [1144012100, 1144012000, 1144011500, 1144012400, 1144012200],
   },
-  eulji: {
-    name: ['을지로', '종로'],
-    code: [1111000000, 1114000000],
-  },
-  apgujeong: {
-    name: ['압구정', '청담'],
-    code: [1168011000, 1168010400],
-  },
   yeoui: {
     name: ['여의도'],
     code: [1156011000],
   },
+  jamsil: {
+    name: ['잠실'],
+    code: [1171000000],
+  },
   itaewon: {
     name: ['이태원'],
-    code: [1117013000],
-  },
-  sokcho: {
-    name: ['속초', '강릉', '양양'],
-    code: [5121000000, 5121000000, 5183000000],
+    code: [1117013000, 1117013100, 1117013600],
   },
   busan: {
     name: ['부산'],
@@ -38,5 +34,9 @@ export const RECOMMENDED_REGION = {
   jeju: {
     name: ['제주'],
     code: [5000000000],
+  },
+  sokcho: {
+    name: ['속초', '강릉', '양양'],
+    code: [5121000000, 5121000000, 5183000000],
   },
 };
