92ced8bae9792bb56e9e3b322824ebae41943ea3
turtle601 <turtle601@naver.com>
2023-10-17 14:50:16 +0900

    test: 로그인 관련 기능 테스트 작성 (#639)

--- frontend/cypress/support/commands.ts
+++ frontend/cypress/support/commands.ts
@@ -1,3 +1,5 @@
+// cypress/support/commands.ts
+
 Cypress.Commands.add('getByCy', (selector, ...args) => cy.get(`[data-cy='${selector}']`, ...args));
 Cypress.Commands.add('getByAriaLabel', (selector, ...args) => cy.get(`[aria-label='${selector}']`, ...args));
 
@@ -5,4 +7,32 @@ Cypress.Commands.add('shouldBeList', restaurantNames => {
   restaurantNames.forEach(name => {
     cy.getByAriaLabel('음식점 리스트').find(`[data-cy="${name} 카드"]`).should('exist');
   });
+});
+
+Cypress.Commands.add('loginGoogle', () => {
+  cy.origin('https://accounts.google.com', () => {
+    Cypress.on(
+      'uncaught:exception',
+      err => !err.message.includes('ResizeObserver loop') && !err.message.includes('Error in protected function'),
+    );
+
+    cy.get('input[type="email"]').type(Cypress.env('GOOGLE_EMAIL_FOR_TESTING'));
+    cy.contains('다음').click().wait(20000);
+
+    cy.get('[type="password"]').type(Cypress.env('GOOGLE_PASSWORD_FOR_TESTING'));
+    cy.contains('다음').click().wait(20000);
+  });
+});
+
+Cypress.Commands.add('loginGoogleForDesktop', () => {
+  cy.get('button[type="google"]').click(); // 구글 로그인 하기 버튼을 누른다.
+
+  cy.loginGoogle();
+});
+
+Cypress.Commands.add('loginGoogleForMobile', () => {
+  cy.get('nav').find('button').last().click(); // 모바일 nav 하단의 마이 페이지 버튼을 누른다.
+  cy.get('button[type="google"]').click(); // 구글 로그인 하기 버튼을 누른다.
+
+  cy.loginGoogle();
 });
--- frontend/cypress/support/index.d.ts
+++ frontend/cypress/support/index.d.ts
@@ -3,5 +3,15 @@ declare namespace Cypress {
     getByCy(selector: string, ...args): Chainable;
     getByAriaLabel(selector: string, ...args): Chainable;
     shouldBeList(restaurantNames: string[]): Chainable;
+
+    dataCy(value: string): Chainable<JQuery<HTMLElement>>;
+
+    /**
+     * Custom command that adds two given numbers
+     */
+    asyncAdd(a: number, b: number): Chainable<number>;
+    loginGoogle(): Chainable;
+    loginGoogleForDesktop(): Chainable;
+    loginGoogleForMobile(): Chainable;
   }
 }
