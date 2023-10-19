d1293e9209d7383d27f10ef8f28ab621d31d338f
turtle601 <turtle601@naver.com>
2023-10-20 08:50:41 +0900

    hotfix: e2e.yml timeout 시간 오류 에러 해결

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -5,7 +5,7 @@ module.exports = defineConfig({
     viewportWidth: 1920,
     viewportHeight: 1080,
     baseUrl: 'https://dev.celuveat.com',
-    defaultCommandTimeout: 100000,
+    defaultCommandTimeout: 4000000,
     video: false,
     experimentalStudio: true,
     experimentalModifyObstructiveThirdPartyCode: true,
