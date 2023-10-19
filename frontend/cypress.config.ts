a4e405f6bfc5eee0e012122806ca147a2fb8f3a4
황준승 <78203399+turtle601@users.noreply.github.com>
2023-10-19 11:09:08 +0900

    test: 메인 페이지 관련 msw 코드 작성 및 e2e 테스트 코드 작성 (#640)
    * test: e2e테스트를 위한 cypress 셋팅 수정 (#639)
    
    * test: 메인 페이지 관련 msw 코드 작성 (#639)
    
    * test: 로그인 관련 기능 테스트 작성 (#639)
    
    * test: 좋아요 관련 기능 테스트 작성 (#639)
    
    * test: api 관련 기능 테스트 작성 (#639)
    
    * fix: yarn install lint 에러 수정
    
    * fix: yarn install lint 에러 수정 (#639)
    
    * fix: yarn install lint 에러 수정 (#639)
    
    * refactor: 좋아요 확인 테스트 코드 수정 (#639)

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -4,8 +4,11 @@ export default defineConfig({
   e2e: {
     viewportWidth: 1920,
     viewportHeight: 1080,
-    baseUrl: 'http://localhost:3000',
-    defaultCommandTimeout: 10000,
+    baseUrl: 'https://dev.celuveat.com',
+    defaultCommandTimeout: 40000,
     video: false,
+    experimentalStudio: true,
+    experimentalModifyObstructiveThirdPartyCode: true,
+    experimentalWebKitSupport: true,
   },
 });
--- frontend/cypress/e2e/restaurantListInteraction.cy.ts
+++
@@ -1,3 +0,0 @@
-describe('no test', () => {
-  it('no test', () => {});
-});
