52b4fd7bc1582e286ad8d4a8ae4010b9bbd8ab63
turtle601 <turtle601@naver.com>
2023-10-20 16:26:31 +0900

    hotfix: cypress 오류 수정

--- frontend/cypress/e2e/mobile/login.cy.ts
+++ frontend/cypress/e2e/mobile/login.cy.ts
@@ -8,13 +8,13 @@ describe('로그인 관련 로직을 테스트 한다.', () => {
       },
     });
 
-    // cy.get('nav').find('button').last().click(); // 모바일 nav 하단의 마이 페이지 버튼을 누른다.
-    // cy.get('button[type="google"]').click(); // 구글 로그인 하기 버튼을 누른다.
+    cy.get('nav').find('button').last().click(); // 모바일 nav 하단의 마이 페이지 버튼을 누른다.
+    cy.get('button[type="google"]').click(); // 구글 로그인 하기 버튼을 누른다.
   });
 
   it('모바일에서 성시경, 소문난성수감자탕 페이지에서 로그인을 한 후 다시 성시경, 소문난성수감자탕 페이지로 돌아 간다.', () => {
     cy.location().should(loc => {
-      expect(loc.href).to.eq('http://localhost:3000');
+      expect(loc.href).to.eq('http://localhost:3000/restaurants/311?celebId=7');
     });
   });
 
