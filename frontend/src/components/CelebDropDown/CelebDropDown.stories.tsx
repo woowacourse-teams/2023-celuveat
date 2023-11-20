a25c4f102c433694e5c9fce8e678968be75cd2b0
황준승 <78203399+turtle601@users.noreply.github.com>
2023-11-20 14:59:36 +0900

    refactor: 범용적인 DropDown 컴포넌트를 celuveat-ui-library 배포 후 적용 (#652)
    * chore: celuveat-ui-library 설치
    
    * refactor: celuveat-library를 적용,  InfoDropDown 코드 수정
    
    * fix: svg 경로로 인한 storybook 에러 해결
    
    * refactor: celuveat-library를 적용,  CelebDropDown 코드 수정
    
    * fix: github action ci 오류 수정
    
    * fix: 사용하지 않은 memo 제거 (#650)

--- frontend/src/components/CelebDropDown/CelebDropDown.stories.tsx
+++ frontend/src/components/CelebDropDown/CelebDropDown.stories.tsx
@@ -10,44 +10,4 @@ export default meta;
 
 type Story = StoryObj<typeof CelebDropDown>;
 
-export const Default: Story = {
-  args: {
-    celebs: [
-      {
-        id: 1,
-        name: '히밥',
-        youtubeChannelName: '@heebab',
-        profileImageUrl:
-          'https://yt3.googleusercontent.com/sL5ugPfl9vvwRwhf6l5APY__BZBw8qWiwgHs-uVsMPFoD5-a4opTJIcRSyrY8aY5LEESOMWJ=s176-c-k-c0x00ffffff-no-rj',
-      },
-      {
-        id: 2,
-        name: '정찬성',
-        youtubeChannelName: '@Korean_zzombi',
-        profileImageUrl:
-          'https://yt3.googleusercontent.com/sL5ugPfl9vvwRwhf6l5APY__BZBw8qWiwgHs-uVsMPFoD5-a4opTJIcRSyrY8aY5LEESOMWJ=s176-c-k-c0x00ffffff-no-rj',
-      },
-      {
-        id: 3,
-        name: '정찬',
-        youtubeChannelName: '@Korean_zzombi',
-        profileImageUrl:
-          'https://yt3.googleusercontent.com/sL5ugPfl9vvwRwhf6l5APY__BZBw8qWiwgHs-uVsMPFoD5-a4opTJIcRSyrY8aY5LEESOMWJ=s176-c-k-c0x00ffffff-no-rj',
-      },
-      {
-        id: 4,
-        name: '정성',
-        youtubeChannelName: '@Korean_zzombi',
-        profileImageUrl:
-          'https://yt3.googleusercontent.com/sL5ugPfl9vvwRwhf6l5APY__BZBw8qWiwgHs-uVsMPFoD5-a4opTJIcRSyrY8aY5LEESOMWJ=s176-c-k-c0x00ffffff-no-rj',
-      },
-      {
-        id: 5,
-        name: '정찬성1',
-        youtubeChannelName: '@Korean_zzombi',
-        profileImageUrl:
-          'https://yt3.googleusercontent.com/sL5ugPfl9vvwRwhf6l5APY__BZBw8qWiwgHs-uVsMPFoD5-a4opTJIcRSyrY8aY5LEESOMWJ=s176-c-k-c0x00ffffff-no-rj',
-      },
-    ],
-  },
-};
+export const Default: Story = {};
