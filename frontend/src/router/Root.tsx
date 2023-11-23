5d62ddcc12225ce3cb3867496126288f4468328b
Jeremy <102432453+shackstack@users.noreply.github.com>
2023-11-23 23:24:49 +0900

    chore: 리액트 헬멧 라이브러리 제거 (#658)

--- frontend/src/router/Root.tsx
+++ frontend/src/router/Root.tsx
@@ -2,7 +2,6 @@
 import { Suspense, lazy } from 'react';
 import { styled } from 'styled-components';
 import { Outlet, ScrollRestoration } from 'react-router-dom';
-import { HelmetProvider } from 'react-helmet-async';
 import Footer from '~/components/@common/Footer';
 import { Header, MobileHeader } from '~/components/@common/Header';
 import LoadingIndicator from '~/components/@common/LoadingIndicator';
@@ -18,10 +17,9 @@ function Root() {
   const { value: isListShowed } = useBooleanState(false);
 
   const { isMobile } = useMediaQuery();
-  const helmetContext = {};
 
   return (
-    <HelmetProvider context={helmetContext}>
+    <>
       <Suspense
         fallback={
           <StyledProcessing>
@@ -38,7 +36,7 @@ function Root() {
       </Suspense>
       <ScrollRestoration />
       <Toast />
-    </HelmetProvider>
+    </>
   );
 }
 
