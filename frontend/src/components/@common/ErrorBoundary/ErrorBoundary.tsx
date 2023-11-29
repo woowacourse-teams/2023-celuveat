3a5b45ca5eddac9d741580f7e0f09f356799316e
Jeremy <102432453+shackstack@users.noreply.github.com>
2023-11-29 12:18:32 +0900

    feat: 에러바운더리 구현 (#661)

---
+++ frontend/src/components/@common/ErrorBoundary/ErrorBoundary.tsx
@@ -0,0 +1,46 @@
+import { Component, ReactElement, ReactNode } from 'react';
+
+interface FallbackRenderProps {
+  resetErrorBoundary: () => void;
+}
+interface Props {
+  children: ReactNode;
+  fallbackRender: ({ resetErrorBoundary }: FallbackRenderProps) => ReactElement;
+  reset: () => void;
+}
+
+interface State {
+  hasError: boolean;
+}
+
+class ErrorBoundary extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(): State {
+    return {
+      hasError: true,
+    };
+  }
+
+  resetErrorBoundary() {
+    const { reset } = this.props;
+    reset();
+    this.setState({ hasError: false });
+  }
+
+  render() {
+    const { hasError } = this.state;
+    const { children, fallbackRender } = this.props;
+
+    if (hasError) {
+      return fallbackRender({ resetErrorBoundary: () => this.resetErrorBoundary() });
+    }
+
+    return children;
+  }
+}
+
+export default ErrorBoundary;
