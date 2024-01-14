1b25178ebfb8c98a5d49b488885b50f6d0d0ec7a
turtle601 <turtle601@naver.com>
2024-01-14 13:19:30 +0900

    fix: 페이지 code 스플리팅으로 인한 suspense 오류 해결

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,29 @@
+import { Suspense } from 'react';
+import { styled } from 'styled-components';
+import LoadingIndicator from '~/components/@common/LoadingIndicator';
+
 import Router from './router/Router';
 
 function App() {
-  return <Router />;
+  return (
+    <Suspense
+      fallback={
+        <StyledProcessing>
+          <LoadingIndicator size={64} />
+        </StyledProcessing>
+      }
+    >
+      <Router />
+    </Suspense>
+  );
 }
 
 export default App;
+
+const StyledProcessing = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  height: 100vh;
+`;
