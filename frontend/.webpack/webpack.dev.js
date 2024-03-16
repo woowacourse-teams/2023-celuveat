20e57159882ca02f3f54278b22590600b6bd7b65
황준승 <78203399+turtle601@users.noreply.github.com>
2024-03-16 18:23:39 +0900

    refactor: webpack 캐시를 활용한 빌드 속도 개선 (#672)
    * chore: dev환경 webpack 설정 구현
    
    * chore: common환경 webpack 설정 구현
    
    * chore: prod환경 webpack 설정 구현
    
    * chore: 기존 웹팩 환경 파일 제거
    
    * chore: 불필요한 babelrc 파일 제거
    
    * chore: 빌드 명령어 변경 및 사용하지 않는 라이브러리 제거
    
    * chore: 타입체킹에 실패하면 빌드가 되지 않도록 설정

---
+++ frontend/.webpack/webpack.dev.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const Dotenv = require('dotenv-webpack');
+const { merge } = require('webpack-merge');
+
+const common = require('./webpack.common');
+
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'eval-cheap-module-source-map',
+  cache: {
+    type: 'filesystem',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|ts|tsx)$/i,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          cacheCompression: false,
+          cacheDirectory: true,
+          presets: ['@babel/preset-env', ['@babel/preset-react', { runtime: 'automatic' }], '@babel/preset-typescript'],
+          plugins: [
+            ['babel-plugin-styled-components'],
+            [
+              'babel-plugin-root-import',
+              {
+                rootPathPrefix: '~',
+                rootPathSuffix: 'src',
+              },
+            ],
+          ],
+        },
+      },
+    ],
+  },
+  plugins: [
+    new Dotenv({
+      path: path.resolve(__dirname, `../.msw.env`),
+    }),
+  ],
+});
