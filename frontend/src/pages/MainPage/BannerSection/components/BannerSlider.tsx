38b52a6ea811ac9e1406299cbbee4be062728062
Jeremy <102432453+shackstack@users.noreply.github.com>
2024-04-05 11:44:07 +0900

    refactor: BannerSlider 컴포넌트 useOnClickBlock 적용

--- frontend/src/pages/MainPage/BannerSection/components/BannerSlider.tsx
+++ frontend/src/pages/MainPage/BannerSection/components/BannerSlider.tsx
@@ -1,26 +1,41 @@
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import Slider from 'react-slick';
 import styled, { css } from 'styled-components';
 import { SERVER_IMG_URL } from '~/constants/url';
 import useMediaQuery from '~/hooks/useMediaQuery';
 import { BannerCarouselSettings } from '~/constants/carouselSettings';
 import { paintSkeleton } from '~/styles/common';
+import useOnClickBlock from '~/hooks/useOnClickBlock';
 
 function BannerSlider() {
   const { isMobile } = useMediaQuery();
+  const navigate = useNavigate();
+  const register1 = useOnClickBlock({
+    callback: () => {
+      navigate('/updated-recent');
+    },
+  });
+  const register2 = useOnClickBlock({
+    callback: () => {
+      navigate('/event');
+    },
+  });
 
   return (
     <Slider {...BannerCarouselSettings} arrows={!isMobile}>
-      <Link to="/updated-recent">
-        <StyledBanner
-          alt="최근 업데이트된 맛집"
-          src={`${SERVER_IMG_URL}banner/recent-updated.jpg`}
-          isMobile={isMobile}
-        />
-      </Link>
-      <Link to="/event">
-        <StyledBanner alt="사진 등록 이벤트" src={`${SERVER_IMG_URL}banner/event-banner.jpeg`} isMobile={isMobile} />
-      </Link>
+      <StyledBanner
+        alt="최근 업데이트된 맛집"
+        src={`${SERVER_IMG_URL}banner/recent-updated.jpg`}
+        isMobile={isMobile}
+        {...register1}
+      />
+
+      <StyledBanner
+        alt="사진 등록 이벤트"
+        src={`${SERVER_IMG_URL}banner/event-banner.jpeg`}
+        isMobile={isMobile}
+        {...register2}
+      />
     </Slider>
   );
 }
@@ -33,6 +48,8 @@ const StyledBanner = styled.img<{ isMobile: boolean }>`
   object-fit: cover;
 
   overflow: hidden;
+
+  cursor: pointer;
 
   ${({ isMobile }) =>
     isMobile
