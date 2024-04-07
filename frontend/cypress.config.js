c7e59391a9d6563eb9d97d1a64b7ad6afcd7b0ff
Jeremy <102432453+shackstack@users.noreply.github.com>
2024-04-08 08:31:14 +0900

    fix: defaultCommandTimeout 1분으로 조정

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -5,7 +5,7 @@ module.exports = defineConfig({
     viewportWidth: 1920,
     viewportHeight: 1080,
     baseUrl: 'http://localhost:3000',
-    defaultCommandTimeout: 10000,
+    defaultCommandTimeout: 60000,
     video: true,
     experimentalStudio: true,
     experimentalModifyObstructiveThirdPartyCode: true,
