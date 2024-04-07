4db01b89ff8011a8222895bdc8a0439cf4a508be
Jeremy <102432453+shackstack@users.noreply.github.com>
2024-04-07 20:38:02 +0900

    refactor: getProfile 공통로직 훅 적용

--- frontend/src/hooks/server/useCheckLogin.ts
+++ frontend/src/hooks/server/useCheckLogin.ts
@@ -3,15 +3,10 @@ import { getProfile } from '~/api/user';
 
 import type { ProfileData } from '~/@types/api.types';
 
-const useCheckLogin = () => {
-  const { data } = useQuery<ProfileData>({
+const useCheckLogin = () =>
+  useQuery<ProfileData>({
     queryKey: ['profile'],
     queryFn: getProfile,
   });
-
-  return {
-    isLogin: Boolean(data),
-  };
-};
 
 export default useCheckLogin;
