5c166c01cff740dc5d4d7411bb9b9fc987bed6fd
Jeremy <102432453+shackstack@users.noreply.github.com>
2024-04-07 23:56:47 +0900

    infra: e2e 테스트 도중 생성된 비디오 s3에 업로드
    - cypress config 수정

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -6,7 +6,7 @@ module.exports = defineConfig({
     viewportHeight: 1080,
     baseUrl: 'http://localhost:3000',
     defaultCommandTimeout: 20000,
-    video: false,
+    video: true,
     experimentalStudio: true,
     experimentalModifyObstructiveThirdPartyCode: true,
     experimentalWebKitSupport: true,
