488410c8893706b046199c0c531d8ad0856d80a8
Jeremy <102432453+shackstack@users.noreply.github.com>
2024-04-07 19:25:16 +0900

    test(e2e): 좋아요를 누른 음식점을 위시리스트에서 좋아요를 해제해도 바로 제거되지 않는다.

--- frontend/cypress/e2e/desktop/like.cy.ts
+++ frontend/cypress/e2e/desktop/like.cy.ts
@@ -23,4 +23,16 @@ describe('좋아요 관련 기능을 테스트 한다.', () => {
 
     cy.get('ul').should('not.contain.text', targetName);
   });
+
+  it('좋아요를 누른 음식점을 위시리스트에서 좋아요를 해제해도 바로 제거되지 않는다. 즉 새로고침을 해야 제거된다.', () => {
+    cy.getByAriaLabel('프로필').click();
+    cy.getByCy('dropdown').contains('위시리스트').click();
+
+    cy.getByAriaLabel(`${targetName} 카드`).find('[aria-label="좋아요"]').click();
+    cy.get('ul').should('contain.text', targetName);
+
+    cy.reload();
+
+    cy.get('ul').should('not.contain.text', targetName);
+  });
 });
